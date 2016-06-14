72632bde976d1fd759741dde77b8ddc2b9f10e92
Jean Aurambault <jaurambault@box.com>
2016-06-14 14:15:14 -0700

    fix import

--- cli/src/test/java/com/box/l10n/mojito/cli/command/RepoCreateCommandTest.java
+++ cli/src/test/java/com/box/l10n/mojito/cli/command/RepoCreateCommandTest.java
@@ -13,7 +13,6 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
