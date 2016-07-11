b1ebe5b0609600538ffb765aaa2191af2ec97210
Jee Yi <jyi@box.com>
2016-07-11 14:00:00 -0700

    MOJI-149 HTML tag integrity checker

---
+++ webapp/src/main/java/com/box/l10n/mojito/service/assetintegritychecker/integritychecker/HtmlTagIntegrityCheckerException.java
@@ -0,0 +1,17 @@
+package com.box.l10n.mojito.service.assetintegritychecker.integritychecker;
+
+/**
+ *
+ * @author jyi
+ */
+public class HtmlTagIntegrityCheckerException extends IntegrityCheckException {
+
+    public HtmlTagIntegrityCheckerException(String message) {
+        super(message);
+    }
+
+    public HtmlTagIntegrityCheckerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+}
