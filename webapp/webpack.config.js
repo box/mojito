0d0b74dbc6c7c2bfee07c65d6acf61a276517875
aurambaj@users.noreply.github.com <jean.aurambault@wanadoo.fr>
2017-08-13 10:34:24 -0700

    Use Webpack to handle sass/css
    - remove fonts from project
    - remove bootstrap-multiselect.css and animate.min.css (don't find any usage)
    - add url-loader and inline fonts (not inline doesn't work with different deploy path)
    - remove grunt task for sass

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -7,7 +7,8 @@ module.exports = function (env) {
 
     var config = {
         entry: {
-            'app' : path.resolve(__dirname, './src/main/resources/public/js/app.js'),
+            'app': path.resolve(__dirname, './src/main/resources/public/js/app.js'),
+            'css': path.resolve(__dirname, './src/main/resources/sass/mojito.scss')
         },
         output: {
             path: path.resolve(__dirname, './target/classes/public'),
@@ -67,8 +68,37 @@ module.exports = function (env) {
                             loader: 'java-properties-flat-loader'
                         }
                     ]
+
+                },
+                {
+                    // __webpack_public_path__ is not supported by ExtractTextPlugin
+                    // so we inline all the fonts here. If not inlined, references 
+                    // to the font are invalid if mojito is deployed with a 
+                    // specific deploy path. 
+                    // hardcoded for deploy path for test -->
+                    //    name: '{deployPath}/fonts/[name]-[hash].[ext]'
                     
-                }   
+                    test: /\.(eot|ttf|woff|woff2)$/,
+                    loader: 'url-loader',
+                    options: {
+                        name: 'fonts/[name]-[hash].[ext]'
+                    }
+                },
+
+                {
+                    test: /\.scss$/,
+                    use: [{
+                            loader: "style-loader" // creates style nodes from JS strings
+                        }, {
+                            loader: "css-loader" // translates CSS into CommonJS
+                        }, {
+                            loader: "sass-loader",
+                            options: {
+                                precision: 8,
+                                sourceMap: true
+                            }
+                        }]
+                },
             ]
         },
         plugins: []
