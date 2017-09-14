02270eb3d9be677f8e8230e41ea56db0dc43c2cf
Jee Yi <completedinyou@gmail.com>
2017-09-14 16:10:26 -0700

    Added CLI comand to view existing repo properties (#200)
    * Added CLI comand to view existing repo properties
    
    * refactored the comparators

---
+++ cli/src/test/java/com/box/l10n/mojito/cli/command/RepoViewCommandTest.java
@@ -0,0 +1,65 @@
+package com.box.l10n.mojito.cli.command;
+
+import com.box.l10n.mojito.cli.CLITestBase;
+import com.box.l10n.mojito.cli.command.param.Param;
+import com.box.l10n.mojito.entity.Repository;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author jyi
+ */
+public class RepoViewCommandTest extends CLITestBase {
+
+    /**
+     * logger
+     */
+    static Logger logger = LoggerFactory.getLogger(RepoViewCommandTest.class);
+
+    @Test
+    public void testViewTestRepo() throws Exception {
+        Repository repository = createTestRepoUsingRepoService();
+        String testRepoName = repository.getName();
+
+        getL10nJCommander().run(
+                "repo-view",
+                Param.REPOSITORY_NAME_SHORT, testRepoName
+        );
+
+        assertTrue("Repository id is missing or incorrect from output", outputCapture.toString().contains("Repository id --> " + repository.getId()));
+        assertFalse("Repository integrity checker is incorrect", outputCapture.toString().contains("Integrity checkers -->"));
+        assertTrue("Repository integrity checker is incorrect", outputCapture.toString().contains("Repository locales --> \"(fr-CA)->fr-FR\" fr-FR ja-JP"));
+
+        getL10nJCommander().run(
+                "repo-update",
+                Param.REPOSITORY_NAME_SHORT, testRepoName,
+                RepoCommand.INTEGRITY_CHECK_SHORT_PARAM, "xliff:SIMPLE_PRINTF_LIKE,properties:MESSAGE_FORMAT"
+        );
+        getL10nJCommander().run(
+                "repo-view",
+                Param.REPOSITORY_NAME_SHORT, testRepoName
+        );
+
+        assertTrue("Repository id is missing or incorrect from output", outputCapture.toString().contains("Repository id --> " + repository.getId()));
+        assertTrue("Repository integrity checker is incorrect", outputCapture.toString().contains("Integrity checkers --> properties:MESSAGE_FORMAT,xliff:SIMPLE_PRINTF_LIKE"));
+        assertTrue("Repository integrity checker is incorrect", outputCapture.toString().contains("Repository locales --> \"(fr-CA)->fr-FR\" fr-FR ja-JP"));
+
+    }
+
+    @Test
+    public void testViewNonExistingRepo() throws Exception {
+        String testRepoName = testIdWatcher.getEntityName("repository");
+
+        getL10nJCommander().run(
+                "repo-view",
+                Param.REPOSITORY_NAME_SHORT, testRepoName
+        );
+
+        assertTrue("Expecting error from viewing non-existing repository", outputCapture.toString().contains("Repository with name [" + testRepoName + "] is not found"));
+    }
+
+}
--- restclient/src/main/java/com/box/l10n/mojito/rest/entity/IntegrityChecker.java
+++ restclient/src/main/java/com/box/l10n/mojito/rest/entity/IntegrityChecker.java
@@ -1,6 +1,7 @@
 package com.box.l10n.mojito.rest.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import java.util.Comparator;
 
 /**
  * Entity that assign an integrity check for a type of asset.
@@ -47,5 +48,16 @@ public class IntegrityChecker {
 
     public void setIntegrityCheckerType(IntegrityCheckerType integrityCheckerType) {
         this.integrityCheckerType = integrityCheckerType;
+    }
+
+    public static Comparator<IntegrityChecker> getComparator() {
+        return new Comparator<IntegrityChecker>() {
+            @Override
+            public int compare(IntegrityChecker integrityChecker1, IntegrityChecker integrityChecker2) {
+                String extension1 = integrityChecker1.getAssetExtension();
+                String extension2 = integrityChecker2.getAssetExtension();
+                return extension1.compareTo(extension2);
+            }
+        };
     }
 }
