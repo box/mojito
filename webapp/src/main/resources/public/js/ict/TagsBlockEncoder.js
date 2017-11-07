c783d1bd64f2f0464fa308fdd23fc6da6fb5d89d
Jean Aurambault <aurambaj@users.noreply.github.com>
2017-11-07 08:21:46 -0800

    Add library to encode Unicode string into base64 string remapped into the tags block

---
+++ webapp/src/main/resources/public/js/ict/TagsBlockEncoder.js
@@ -0,0 +1,34 @@
+class TagsBlockEncoder {
+    
+    unicodeToTagsBlock(id) {
+        var base64 = this.b64EncodeUnicode(id);
+        return this.asciiToTagsBlock(base64);
+    }
+
+    asciiToTagsBlock(string) {
+        var res = '';
+
+        [...string].forEach(c => {
+            if (c.charCodeAt(0) >= 32 && c.charCodeAt(0) <= 126) {
+                res += String.fromCharCode(56128, 56352 - 32 + c.charCodeAt(0));
+            } else {
+                throw "Unsupported character to encode in Tags block.";
+            }
+        });
+
+        return res;
+    }
+    
+    b64EncodeUnicode(str) {
+        // from MDN: https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding
+        // first we use encodeURIComponent to get percent-encoded UTF-8,
+        // then we convert the percent encodings into raw bytes which
+        // can be fed into btoa.
+        return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
+                function toSolidBytes(match, p1) {
+                    return String.fromCharCode('0x' + p1);
+                }));
+    }
+}
+
+export default new TagsBlockEncoder();
