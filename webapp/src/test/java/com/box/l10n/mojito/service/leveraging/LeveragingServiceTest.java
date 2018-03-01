2d94bc8ae7b84f51dc70a76ba7063394971d487f
Jean Aurambault <aurambaj@users.noreply.github.com>
2018-02-28 17:38:20 -0800

    Provide an option to perform leveraging only for target text unit with name matching a given regex

--- webapp/src/test/java/com/box/l10n/mojito/service/leveraging/LeveragingServiceTest.java
+++ webapp/src/test/java/com/box/l10n/mojito/service/leveraging/LeveragingServiceTest.java
@@ -12,6 +12,8 @@ import com.box.l10n.mojito.service.tm.TMService;
 import com.box.l10n.mojito.service.tm.TMTestData;
 import com.box.l10n.mojito.service.tm.TMTextUnitVariantRepository;
 import com.box.l10n.mojito.test.TestIdWatcher;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
 import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
@@ -70,7 +72,44 @@ public class LeveragingServiceTest extends ServiceTestBase {
         tmService.addTMTextUnit(tm.getId(), assetId, "TEST2", "Content2", "Comment2");
         tmService.addTMTextUnit(tm.getId(), assetId, "TEST3", "Content3", "Comment3");
 
-        leveragingService.copyAllTranslationsWithMD5MatchBetweenRepositories(sourceRepository, targetRepository).get();
+        leveragingService.copyAllTranslationsWithMD5MatchBetweenRepositories(sourceRepository, targetRepository, null).get();
+
+        List<TMTextUnitVariant> sourceTranslations = tmTextUnitVariantRepository.findByTmTextUnitTmRepositoriesOrderByContent(sourceRepository);
+        List<TMTextUnitVariant> targetTranslations = tmTextUnitVariantRepository.findByTmTextUnitTmRepositoriesOrderByContent(targetRepository);
+
+        Iterator<TMTextUnitVariant> itSource = sourceTranslations.iterator();
+        Iterator<TMTextUnitVariant> itTarget = targetTranslations.iterator();
+
+        while (itTarget.hasNext()) {
+            TMTextUnitVariant next = itTarget.next();
+            Assert.assertEquals("translation in source and target must be the same", itSource.next().getContent(), next.getContent());
+        }
+
+        Assert.assertFalse(itTarget.hasNext());
+        Assert.assertFalse(itSource.hasNext());
+
+    }
+
+    @Test
+    public void copyAllTranslationsWithMD5MatchBetweenRepositoriesNameRegex() throws InterruptedException, ExecutionException, RepositoryNameAlreadyUsedException {
+
+        TMTestData tmTestDataSource = new TMTestData(testIdWatcher);
+
+        Repository sourceRepository = tmTestDataSource.repository;
+
+        logger.debug("Create the target repository");
+        Repository targetRepository = repositoryService.createRepository(testIdWatcher.getEntityName("targetRepository"));
+
+        TM tm = targetRepository.getTm();
+
+        Asset asset = assetService.createAsset(targetRepository.getId(), "fake for test", "fake_for_test");
+        Long assetId = asset.getId();
+
+        tmService.addTMTextUnit(tm.getId(), assetId, "zuora_error_message_verify_state_province", "Please enter a valid state, region or province", "Comment1");
+        tmService.addTMTextUnit(tm.getId(), assetId, "TEST2", "Content2", "Comment2");
+        tmService.addTMTextUnit(tm.getId(), assetId, "TEST3", "Content3", "Comment3");
+
+        leveragingService.copyAllTranslationsWithMD5MatchBetweenRepositories(sourceRepository, targetRepository, null).get();
 
         List<TMTextUnitVariant> sourceTranslations = tmTextUnitVariantRepository.findByTmTextUnitTmRepositoriesOrderByContent(sourceRepository);
         List<TMTextUnitVariant> targetTranslations = tmTextUnitVariantRepository.findByTmTextUnitTmRepositoriesOrderByContent(targetRepository);
@@ -107,13 +146,21 @@ public class LeveragingServiceTest extends ServiceTestBase {
         tmService.addTMTextUnit(tm.getId(), assetId, "TEST2", "Content2", "Comment2");
         tmService.addTMTextUnit(tm.getId(), assetId, "TEST3", "Content3", "Comment3");
 
-        leveragingService.copyAllTranslationsWithExactMatchBetweenRepositories(sourceRepository, targetRepository).get();
+        leveragingService.copyAllTranslationsWithExactMatchBetweenRepositories(sourceRepository, targetRepository, "TEST.*").get();
 
         List<TMTextUnitVariant> sourceTranslations = tmTextUnitVariantRepository.findByTmTextUnitTmRepositoriesOrderByContent(sourceRepository);
         List<TMTextUnitVariant> targetTranslations = tmTextUnitVariantRepository.findByTmTextUnitTmRepositoriesOrderByContent(targetRepository);
 
-        Iterator<TMTextUnitVariant> itSource = sourceTranslations.iterator();
-        Iterator<TMTextUnitVariant> itTarget = targetTranslations.iterator();
+        Predicate<TMTextUnitVariant> filterZuora = new Predicate<TMTextUnitVariant>() {
+            @Override
+            public boolean apply(TMTextUnitVariant tmtuv) {
+                return "zuora_error_message_verify_state_province_update".equals(tmtuv.getTmTextUnit().getName());
+            }
+
+        };
+        
+        Iterator<TMTextUnitVariant> itSource = Iterables.filter(sourceTranslations, filterZuora).iterator();
+        Iterator<TMTextUnitVariant> itTarget = Iterables.filter(sourceTranslations, filterZuora).iterator();
 
         while (itTarget.hasNext()) {
             TMTextUnitVariant next = itTarget.next();
@@ -124,7 +171,7 @@ public class LeveragingServiceTest extends ServiceTestBase {
         Assert.assertFalse(itSource.hasNext());
 
     }
-    
+
     @Test
     public void checkCommentsAreNotCopiedIfTmTextUnitCurrentVariantNotChanged() throws InterruptedException, ExecutionException, RepositoryNameAlreadyUsedException {
 
@@ -144,8 +191,8 @@ public class LeveragingServiceTest extends ServiceTestBase {
         tmService.addTMTextUnit(tm.getId(), assetId, "TEST2", "Content2", "Comment2");
         tmService.addTMTextUnit(tm.getId(), assetId, "TEST3", "Content3", "Comment3");
 
-        leveragingService.copyAllTranslationsWithExactMatchBetweenRepositories(sourceRepository, targetRepository).get();
-        leveragingService.copyAllTranslationsWithExactMatchBetweenRepositories(sourceRepository, targetRepository).get();
+        leveragingService.copyAllTranslationsWithExactMatchBetweenRepositories(sourceRepository, targetRepository, null).get();
+        leveragingService.copyAllTranslationsWithExactMatchBetweenRepositories(sourceRepository, targetRepository, null).get();
 
         List<TMTextUnitVariant> targetTranslations = tmTextUnitVariantRepository.findByTmTextUnitTmRepositoriesOrderByContent(targetRepository);
         for (TMTextUnitVariant targetTranslation : targetTranslations) {
