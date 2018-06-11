43d79e3fed5b1e3d12d2d895d77121fd66e9c89e
Jean Aurambault <aurambaj@users.noreply.github.com>
2018-06-11 12:13:10 -0700

    Add simple Mustache template engine.

---
+++ webapp/src/main/java/com/box/l10n/mojito/mustache/MustacheLambdaOdd.java
@@ -0,0 +1,38 @@
+package com.box.l10n.mojito.mustache;
+
+import com.samskivert.mustache.Mustache;
+import com.samskivert.mustache.Template;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Prints "odd" for every odd invocation of the lambda
+ * 
+ * @author jeanaurambault
+ */
+public class MustacheLambdaOdd implements Mustache.Lambda {
+
+    Map<String, AtomicInteger> counters = new HashMap<>();
+    
+    @Override
+    public void execute(Template.Fragment frag, Writer out) throws IOException {
+        String key = frag.execute();
+        execute(key, out);
+    }
+
+    void execute(String key, Writer out) throws IOException {
+        AtomicInteger counter = counters.get(key);
+        
+        if (counter == null) {
+            counter = new AtomicInteger();
+            counters.put(key, counter);
+        }
+        
+        if (counter.getAndIncrement() % 2 != 0) {
+            out.write("odd");
+        }
+    }
+}
