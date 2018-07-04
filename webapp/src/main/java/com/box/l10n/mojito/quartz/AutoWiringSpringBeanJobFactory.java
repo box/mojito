416f06e9135712c4db3cfb2ac6172751ae4e580e
Jean Aurambault <aurambaj@users.noreply.github.com>
2018-07-04 09:54:29 -0700

    Add Quartz Scheduler
    - use the jdbc store by providing the spring data source to the scheduler
    - default properties are defined in the global application.properties
    - migration script contains table creation for mysql
    - db/hsql/quartz.sql script contains table creation for testing/hsql
    - QuartzConfig reads config on the l10n namespace and forward them to quartz (the properties are exaclty the same, just prefixed with l10n.)
    - QuartzConfig contains logic to add jobs and triggers defined with spring beans and remove the old ones if the spring beans were removed. Those job must use the DEFAULT group
    - Some jobs are created dynamically like updating the repository statistics use the DYNAMIC group name (to distinguish them from spring beans DEFAULT group)

---
+++ webapp/src/main/java/com/box/l10n/mojito/quartz/AutoWiringSpringBeanJobFactory.java
@@ -0,0 +1,25 @@
+package com.box.l10n.mojito.quartz;
+
+import org.quartz.spi.TriggerFiredBundle;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.scheduling.quartz.SpringBeanJobFactory;
+
+public final class AutoWiringSpringBeanJobFactory extends SpringBeanJobFactory implements ApplicationContextAware {
+
+    private transient AutowireCapableBeanFactory beanFactory;
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        beanFactory = applicationContext.getAutowireCapableBeanFactory();
+    }
+
+    @Override
+    protected Object createJobInstance(final TriggerFiredBundle bundle) throws Exception {
+        final Object job = super.createJobInstance(bundle);
+        beanFactory.autowireBean(job);
+        return job;
+    }
+
+}
