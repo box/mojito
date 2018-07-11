f66f363404ba1daa11520ae88eff84100deea8e9
Edwin Hoogerbeets <ehoogerbeets+github@gmail.com>
2018-07-11 15:01:53 -0700

    MOJI-677: add a confirmation dialog box while cancelling a drop (#293)
    * MOJI-677: add a confirmation dialog box while cancelling a request
    
    * Clean up the comments a bit
    
    * "Really Cancel?" -> "Confirm Cancel"
    
    * Remove the unnecessary "cancelRepoId" property in the state

--- webapp/src/main/resources/public/js/components/drops/Drops.js
+++ webapp/src/main/resources/public/js/components/drops/Drops.js
@@ -24,6 +24,7 @@ import DropStore from "../../stores/drop/DropStore";
 import ImportDropConfig from "../../sdk/drop/ImportDropConfig";
 import NewDropModal from "./NewDropModal";
 import RepositoryActions from "../../actions/RepositoryActions";
+import ConfirmationModal from "../widgets/ConfirmationModal";
 
 let Drops = React.createClass({
     mixins: [FluxyMixin],
@@ -64,6 +65,11 @@ let Drops = React.createClass({
 
             /** @type {Drop} */
             "selectedDrop": null,
+
+            /** @type {Boolean} */
+            "showCancelModal": false,
+            /** @type {Number} */
+            "cancelDropId": null
         };
     },
 
@@ -157,7 +163,7 @@ let Drops = React.createClass({
     },
 
     /**
-     * Hande import onclick event
+     * Handle import onclick event
      */
     onClickImport(dropId, repoId) {
         this.showAlert(this.props.intl.formatMessage({id: "drops.beingImported.alert"}));
@@ -168,16 +174,43 @@ let Drops = React.createClass({
     },
 
     /**
-     * handle cancel Drop onclick event
+     * handle cancel drop onclick event
      * @param {Number} dropId
      * @param {Number} repoId
      */
     onClickCancel(dropId, repoId) {
+        this.setState({
+            "showCancelModal": true,
+            "cancelDropId": dropId,
+        });
+    },
+
+    /**
+     * handle cancel drop confirmation
+     */
+    onConfirmCancel() {
+        let dropId = this.state.cancelDropId;
+
         this.showAlert(this.props.intl.formatMessage({id: "drops.beingCanceled.alert"}));
         let cancelDropConfig = new CancelDropConfig(dropId, null);
         DropActions.cancelRequest(cancelDropConfig);
 
         this.delayGetNewRequests(500);
+
+        this.setState({
+            "showCancelModal": false,
+            "cancelDropId": null
+        })
+    },
+
+    /**
+     * handle cancel drop not confirmed
+     */
+    onCancelCancel() {
+        this.setState({
+            "showCancelModal": false,
+            "cancelDropId": null
+        });
     },
 
     /**
@@ -535,6 +568,13 @@ let Drops = React.createClass({
     render() {
         return (
             <div>
+                <ConfirmationModal showModal={this.state.showCancelModal}
+                    modalTitleMessage="drops.confirm.cancel.model.title"
+                    modalBodyMessage="drops.confirm.cancel.model.body"
+                    confirmButtonLabel="drops.confirm.cancel.model.confirm"
+                    cancelButtonLabel="drops.confirm.cancel.model.cancel"
+                    onCancelledCallback={this.onCancelCancel}
+                    onConfirmedCallback={this.onConfirmCancel}/>
                 <ReactSidebarResponsive ref="sideBar" sidebar={this.getSideBarContent()}
                          rootClassName="side-bar-root-container"
                          sidebarClassName="side-bar-container"
