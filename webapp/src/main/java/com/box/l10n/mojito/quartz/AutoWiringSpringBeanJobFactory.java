bb3ee6185bd584f03428aed4a0480a173bf0f9cd
Jean Aurambault <aurambaj@users.noreply.github.com>
2018-08-20 15:43:36 -0700

    Add logging when creating the job is failing

--- webapp/src/main/java/com/box/l10n/mojito/quartz/AutoWiringSpringBeanJobFactory.java
+++ webapp/src/main/java/com/box/l10n/mojito/quartz/AutoWiringSpringBeanJobFactory.java
@@ -1,6 +1,8 @@
 package com.box.l10n.mojito.quartz;
 
 import org.quartz.spi.TriggerFiredBundle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.context.ApplicationContext;
@@ -8,6 +10,11 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.scheduling.quartz.SpringBeanJobFactory;
 
 public final class AutoWiringSpringBeanJobFactory extends SpringBeanJobFactory implements ApplicationContextAware {
+
+    /**
+     * logger
+     */
+    static Logger logger = LoggerFactory.getLogger(AutoWiringSpringBeanJobFactory.class);
 
     private transient AutowireCapableBeanFactory beanFactory;
 
@@ -17,9 +24,14 @@ public final class AutoWiringSpringBeanJobFactory extends SpringBeanJobFactory i
 
     @Override
     protected Object createJobInstance(final TriggerFiredBundle bundle) throws Exception {
-        final Object job = super.createJobInstance(bundle);
-        beanFactory.autowireBean(job);
-        return job;
+        try {
+            final Object job = super.createJobInstance(bundle);
+            beanFactory.autowireBean(job);
+            return job;
+        } catch (Throwable t) {
+            logger.error("Can't create a Quartz job instance, this is a critical error", t);
+            throw t;
+        }
     }
 
 }
