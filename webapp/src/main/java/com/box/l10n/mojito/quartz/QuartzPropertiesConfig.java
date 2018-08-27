79743cdfe8baa38c5754a735d7b8959446323d39
Jean Aurambault <aurambaj@users.noreply.github.com>
2018-08-27 12:50:04 -0700

    Allow configuration of the Quartz datasource using quartz setting (before it was reusing the Spring datasource)
    If a datasource is define in the Quartz settings, use the settings to configure the database connection.
    
    If there is no setting it will use the Spring datasource.

--- webapp/src/main/java/com/box/l10n/mojito/quartz/QuartzPropertiesConfig.java
+++ webapp/src/main/java/com/box/l10n/mojito/quartz/QuartzPropertiesConfig.java
@@ -1,5 +1,7 @@
 package com.box.l10n.mojito.quartz;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +13,8 @@ import java.util.Properties;
 @Configuration
 @ConfigurationProperties("l10n.org")
 public class QuartzPropertiesConfig {
+
+    static Logger logger = LoggerFactory.getLogger(QuartzPropertiesConfig.class);
 
     Map<String, String> quartz = new HashMap<>();
 
@@ -25,6 +29,7 @@ public class QuartzPropertiesConfig {
 
         for (Map.Entry<String, String> entry : quartz.entrySet()) {
             properties.put("org.quartz." + entry.getKey(), entry.getValue());
+            logger.debug("org.quartz.{}={}" , entry.getKey(), entry.getValue());
         }
 
         return properties;
