0aa68cc1311aaafbcce51d1cd2fa553239cd91e6
Jean Aurambault <aurambaj@users.noreply.github.com>
2018-08-27 12:40:11 -0700

    Modify Quartz table to use longblob. This need to run async task that have big paylaod.
    Also refactor the exception handling to share the logic between quartz and the old pollable framework

--- webapp/src/main/java/com/box/l10n/mojito/quartz/QuartzPollableJob.java
+++ webapp/src/main/java/com/box/l10n/mojito/quartz/QuartzPollableJob.java
@@ -1,10 +1,17 @@
 package com.box.l10n.mojito.quartz;
 
+import com.box.l10n.mojito.entity.PollableTask;
 import com.box.l10n.mojito.json.ObjectMapper;
+import com.box.l10n.mojito.service.pollableTask.ExceptionHolder;
+import com.box.l10n.mojito.service.pollableTask.PollableFuture;
+import com.box.l10n.mojito.service.pollableTask.PollableFutureTaskResult;
+import com.box.l10n.mojito.service.pollableTask.PollableTaskException;
+import com.box.l10n.mojito.service.pollableTask.PollableTaskExceptionUtils;
 import com.box.l10n.mojito.service.pollableTask.PollableTaskService;
 import com.box.l10n.mojito.service.tm.importer.TextUnitBatchImporterService;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.google.common.reflect.TypeToken;
+import nu.validator.htmlparser.annotation.Auto;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
@@ -38,18 +45,30 @@ public abstract class QuartzPollableJob<I, O> implements Job {
     @Autowired
     PollableTaskService pollableTaskService;
 
+    @Autowired
+    PollableTaskExceptionUtils pollableTaskExceptionUtils;
+
     public abstract O call(I input) throws JobExecutionException;
 
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
         Long pollableTaskId = context.getMergedJobDataMap().getLong(POLLABLE_TASK_ID);
+        PollableTask pollableTask = pollableTaskService.getPollableTask(pollableTaskId);
 
         Object output = null;
+        ExceptionHolder exceptionHolder = new ExceptionHolder(pollableTask);
+
         try {
             I input = (I) jsonStringToObject(context.getMergedJobDataMap().getString(INPUT), typeTokenInput.getRawType());
             output = call(input);
+        } catch (Throwable t) {
+            pollableTaskExceptionUtils.processException(t, exceptionHolder);
         } finally {
-            pollableTaskService.finishTask(pollableTaskId, null, null, null);
+            pollableTask = pollableTaskService.finishTask(
+                    pollableTask.getId(),
+                    null,
+                    exceptionHolder,
+                    null);
         }
     }
 
