87248255bf922e4e04ff9237a4ccd5370a8a7f2c
Jean Aurambault <aurambaj@users.noreply.github.com>
2018-08-27 13:07:49 -0700

    Create a WS to add or remove an host from rotation.
    When put of the rotation the health check will return 503. Make the entry point accessible without authentication from 127.0.0.1 for local invocation only

---
+++ webapp/src/main/java/com/box/l10n/mojito/rest/rotation/HealthRotation.java
@@ -0,0 +1,70 @@
+package com.box.l10n.mojito.rest.rotation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author jaurambault
+ */
+@Component
+public class HealthRotation implements HealthIndicator {
+
+    /**
+     * logger
+     */
+    static Logger logger = LoggerFactory.getLogger(HealthRotation.class);
+
+    Boolean inRotation = true;
+
+    String host;
+
+    @PostConstruct
+    public void init() {
+        host = getHost();
+    }
+
+    @Override
+    public Health health() {
+        Health.Builder builder;
+
+        if (inRotation) {
+            builder = Health.up();
+        } else {
+            builder = Health.down();
+        }
+        return builder.withDetail("host", host).build();
+    }
+
+    public void setInRotation(Boolean inRotation) {
+        this.inRotation = inRotation;
+    }
+
+
+    String getHost() {
+        String host;
+        try {
+            host = getHostAsync().get(1, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            host = "Can't get host";
+        }
+        return host;
+    }
+
+    @Async
+    Future<String> getHostAsync() throws UnknownHostException {
+        String host = InetAddress.getLocalHost().getHostName();
+        return new AsyncResult<>(host);
+    }
+
+}
