94b79a3976c321532ee655338f85677ec4eb810a
Jean Aurambault <aurambaj@users.noreply.github.com>
2018-10-11 17:37:41 -0700

    Fix OAuth redirect when session expires
    We want to prompt for login again and reload the app in the proper state (that doesn't fully work but nothing new..)
    
    - It was going to the old login page
    - We need to let the frontend know that oauth is enabled (hence adding those config)
    - In the frontend for oauth we redirect to the current page and for default login we redirect to spring url with show page.
    - Note that the redirect don't work for links from repository to workbench (relatively annoying) and within the workbench (not to bad)

--- webapp/src/main/java/com/box/l10n/mojito/react/ReactAppConfig.java
+++ webapp/src/main/java/com/box/l10n/mojito/react/ReactAppConfig.java
@@ -1,6 +1,8 @@
 package com.box.l10n.mojito.react;
 
+import nu.validator.htmlparser.annotation.Auto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
@@ -10,11 +12,22 @@ public class ReactAppConfig {
     @Autowired
     OpengrokConfig opengrok;
 
+    @Autowired
+    LoginConfig login;
+
     public OpengrokConfig getOpengrok() {
         return opengrok;
     }
 
     public void setOpengrok(OpengrokConfig opengrok) {
         this.opengrok = opengrok;
+    }
+
+    public LoginConfig getLogin() {
+        return login;
+    }
+
+    public void setLogin(LoginConfig login) {
+        this.login = login;
     }
 }
