aeac23acfc9f2177327c4620bccc1fba71271218
Jean Aurambault <aurambaj@users.noreply.github.com>
2018-10-15 12:58:46 -0700

    Use POST request to check content validity when saving a text unit in the workbench
    Fix, #348: GET fails if the text is too long and when proxied by NGINX.

---
+++ webapp/src/main/java/com/box/l10n/mojito/rest/textunit/TextUnitCheckBody.java
@@ -0,0 +1,23 @@
+package com.box.l10n.mojito.rest.textunit;
+
+public class TextUnitCheckBody {
+
+    Long tmTextUnitId;
+    String content;
+
+    public Long getTmTextUnitId() {
+        return tmTextUnitId;
+    }
+
+    public void setTmTextUnitId(Long tmTextUnitId) {
+        this.tmTextUnitId = tmTextUnitId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
