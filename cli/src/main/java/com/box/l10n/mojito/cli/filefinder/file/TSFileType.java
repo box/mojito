ab3c2ac5e2ada8e2cfe20de2c9dcd3d07cdd4c3f
Jee Yi <jeeyi@users.noreply.github.com>
2018-11-14 11:54:55 -0800

    client-side changes to support JS/TS file format

---
+++ cli/src/main/java/com/box/l10n/mojito/cli/filefinder/file/TSFileType.java
@@ -0,0 +1,12 @@
+package com.box.l10n.mojito.cli.filefinder.file;
+
+/**
+ *
+ * @author jyi
+ */
+public class TSFileType extends LocaleAsFileNameType {
+
+    public TSFileType() {
+        this.sourceFileExtension = "ts";
+    }
+}
