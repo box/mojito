f9bcc2a6049f76bdd2f0aff66bd260c4f9b0f72f
Jean Aurambault <aurambaj@users.noreply.github.com>
2018-12-05 22:34:19 -0800

    Need to initialize the source locale to "en" (id: 21)

--- webapp/src/main/resources/db/migration/V33__Add_Source_locale.sql
+++ webapp/src/main/resources/db/migration/V33__Add_Source_locale.sql
@@ -1,3 +1,4 @@
-alter table repository add column source_locale_id bigint;
+-- 21 is "en"
+alter table repository add column source_locale_id bigint not null default 21;
 alter table repository_aud add column source_locale_id bigint;
 alter table repository add constraint FK__REPOSITORY__LOCALE__ID foreign key (source_locale_id) references locale (id);
