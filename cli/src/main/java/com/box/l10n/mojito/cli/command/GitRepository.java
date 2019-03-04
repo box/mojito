7bf864ca8b8d33a1e1c3cdc537e98aed99f801f6
xiayewang <xiayewang@pinterest.com>
2019-03-04 14:20:26 -0800

    add git-diff param to push command to enable processing source asset contains git changes

--- cli/src/main/java/com/box/l10n/mojito/cli/command/GitRepository.java
+++ cli/src/main/java/com/box/l10n/mojito/cli/command/GitRepository.java
@@ -5,8 +5,10 @@ import org.eclipse.jgit.api.BlameCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.blame.BlameResult;
 import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.treewalk.CanonicalTreeParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -97,6 +99,16 @@ public class GitRepository {
 
     public File getDirectory() {
         return jgitRepository.getDirectory();
+    }
+
+
+    public CanonicalTreeParser getHeadTree() throws IOException {
+        ObjectId headCommit = jgitRepository.resolve("HEAD^{tree}");
+        CanonicalTreeParser treeParser = new CanonicalTreeParser();
+        try (ObjectReader reader = jgitRepository.newObjectReader()) {
+            treeParser.reset(reader, headCommit);
+        }
+        return treeParser;
     }
 
 }
--- cli/src/main/java/com/box/l10n/mojito/cli/command/PushCommand.java
+++ cli/src/main/java/com/box/l10n/mojito/cli/command/PushCommand.java
@@ -12,11 +12,17 @@ import com.box.l10n.mojito.rest.client.RepositoryClient;
 import com.box.l10n.mojito.rest.client.exception.PollableTaskException;
 import com.box.l10n.mojito.rest.entity.*;
 import com.google.common.collect.Sets;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.diff.DiffEntry;
 import org.fusesource.jansi.Ansi;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -65,6 +71,9 @@ public class PushCommand extends Command {
     @Parameter(names = {"--branch-createdby", "-bc"}, arity = 1, required = false, description = "username of text unit author")
     String branchCreatedBy;
 
+    @Parameter(names = {"--git-diff", "-df"}, arity = 1, required = false, description = "only processing git diff files")
+    Boolean gitDiff = false;
+
     @Autowired
     AssetClient assetClient;
 
@@ -87,6 +96,16 @@ public class PushCommand extends Command {
         List<PollableTask> pollableTasks = new ArrayList<>();
 
         ArrayList<FileMatch> sourceFileMatches = commandHelper.getSourceFileMatches(commandDirectories, fileType, sourceLocale, sourcePathFilterRegex);
+
+        // filter out source files which do not contain diff changes if git diff is enabled.
+        if (gitDiff) {
+            sourceFileMatches = getGitChangeFiles(sourceFileMatches);
+            if (sourceFileMatches.isEmpty()) {
+                logger.debug("No new string has been created. No need to create branch and process asset.");
+                return;
+            }
+        }
+
         Set<Long> usedAssetIds = new HashSet<>();
 
         for (FileMatch sourceFileMatch : sourceFileMatches) {
@@ -110,7 +129,6 @@ public class PushCommand extends Command {
             sourceAsset.setRepositoryId(repository.getId());
             sourceAsset.setFilterConfigIdOverride(sourceFileMatch.getFileType().getFilterConfigIdOverride());
             sourceAsset.setFilterOptions(filterOptions);
-           
             consoleWriter.a(" - Uploading: ").fg(Ansi.Color.CYAN).a(sourcePath).println();
 
             SourceAsset assetAfterSend = assetClient.sendSourceAsset(sourceAsset);
@@ -145,5 +163,29 @@ public class PushCommand extends Command {
         }
 
         consoleWriter.fg(Ansi.Color.GREEN).newLine().a("Finished").println(2);
+    }
+
+    /**
+     * Get fileMatch list contains git changes against git tree HEAD
+     * @param sourceFileMatches
+     * @return
+     * @throws CommandException
+     */
+    private ArrayList<FileMatch> getGitChangeFiles(ArrayList<FileMatch> sourceFileMatches) throws CommandException {
+        GitRepository gitRepository = new GitRepository();
+        gitRepository.init(commandDirectories.getSourceDirectoryPath().toString());
+        Git git = new Git(gitRepository.jgitRepository);
+        String gitPath = gitRepository.getDirectory().getPath().replace(".git", "");
+        Set<String> gitDiffFiles = new HashSet<>();
+        try {
+            List<DiffEntry> diffEntries = git.diff().setOldTree(gitRepository.getHeadTree()).call();
+            gitDiffFiles.addAll(diffEntries.stream().map(DiffEntry::getNewPath).collect(Collectors.toSet()));
+        } catch (IOException | GitAPIException e) {
+            logger.debug(e.getMessage());
+        }
+        return sourceFileMatches.
+                stream().
+                filter(fileMatch -> fileMatch.getPath().toString().startsWith(gitPath) ? gitDiffFiles.contains(fileMatch.getPath().toString().substring(gitPath.length())) : false).
+                collect(Collectors.toCollection(ArrayList::new));
     }
 }
