df71a379bb544d7a2878e54dc23784872994b763
xiayewang <xiayewang@pinterest.com>
2019-03-06 19:11:55 -0800

    add git param to process asset with git change

--- cli/src/main/java/com/box/l10n/mojito/cli/command/PushCommand.java
+++ cli/src/main/java/com/box/l10n/mojito/cli/command/PushCommand.java
@@ -11,12 +11,21 @@ import com.box.l10n.mojito.rest.client.AssetClient;
 import com.box.l10n.mojito.rest.client.RepositoryClient;
 import com.box.l10n.mojito.rest.client.exception.PollableTaskException;
 import com.box.l10n.mojito.rest.entity.*;
+import com.box.l10n.mojito.rest.entity.Repository;
 import com.google.common.collect.Sets;
+
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.treewalk.filter.PathFilter;
 import org.fusesource.jansi.Ansi;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -65,6 +74,12 @@ public class PushCommand extends Command {
     @Parameter(names = {"--branch-createdby", "-bc"}, arity = 1, required = false, description = "username of text unit author")
     String branchCreatedBy;
 
+    @Parameter(names = {"--git-diff", "-df"}, arity = 1, required = false, description = "only processing git diff files")
+    Boolean gitDiff = false;
+
+    @Parameter(names = {"--git-regex", "-gr"}, arity = 1, required = false, description = "only processing git diff files if diff change matches regex")
+    String gitRegex;
+
     @Autowired
     AssetClient assetClient;
 
@@ -87,6 +102,15 @@ public class PushCommand extends Command {
         List<PollableTask> pollableTasks = new ArrayList<>();
 
         ArrayList<FileMatch> sourceFileMatches = commandHelper.getSourceFileMatches(commandDirectories, fileType, sourceLocale, sourcePathFilterRegex);
+
+        if (gitDiff) {
+            try {
+                sourceFileMatches = getGitChangeFiles(sourceFileMatches);
+            } catch (GitAPIException e) {
+                throw new CommandException(e.getMessage());
+            }
+        }
+
         Set<Long> usedAssetIds = new HashSet<>();
 
         for (FileMatch sourceFileMatch : sourceFileMatches) {
@@ -110,7 +134,7 @@ public class PushCommand extends Command {
             sourceAsset.setRepositoryId(repository.getId());
             sourceAsset.setFilterConfigIdOverride(sourceFileMatch.getFileType().getFilterConfigIdOverride());
             sourceAsset.setFilterOptions(filterOptions);
-           
+
             consoleWriter.a(" - Uploading: ").fg(Ansi.Color.CYAN).a(sourcePath).println();
 
             SourceAsset assetAfterSend = assetClient.sendSourceAsset(sourceAsset);
@@ -145,5 +169,38 @@ public class PushCommand extends Command {
         }
 
         consoleWriter.fg(Ansi.Color.GREEN).newLine().a("Finished").println(2);
+    }
+
+    /**
+     * Get fileMatch list contains git changes against git tree HEAD
+     * @param sourceFileMatches
+     * @return
+     * @throws CommandException
+     */
+    private ArrayList<FileMatch> getGitChangeFiles(ArrayList<FileMatch> sourceFileMatches) throws CommandException, GitAPIException {
+        logger.debug("filter out source files which do not contain diff changes.");
+        GitRepository gitRepository = new GitRepository();
+        gitRepository.init(commandDirectories.getSourceDirectoryPath().toString());
+        Git git = new Git(gitRepository.jgitRepository);
+        String gitPath = gitRepository.getDirectory().getPath().replace(".git", "");
+        Set<String> gitDiffFiles = new HashSet<>();
+
+        Pattern pattern = Pattern.compile(gitRegex);
+        for(FileMatch fileMatch: sourceFileMatches) {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            List<DiffEntry> diff = git.
+                    diff().
+                    setCached(true).
+                    setOutputStream(byteArrayOutputStream).
+                    setPathFilter(PathFilter.create(fileMatch.getPath().toString().substring(gitPath.length()))).
+                    call();
+            if(gitRegex == null || pattern.matcher(byteArrayOutputStream.toString()).find()) {
+                gitDiffFiles.add(gitPath + diff.get(0).getNewPath());
+            }
+        }
+        return sourceFileMatches.
+                stream().
+                filter(fileMatch -> gitDiffFiles.contains(fileMatch.getPath().toString())).
+                collect(Collectors.toCollection(ArrayList::new));
     }
 }
