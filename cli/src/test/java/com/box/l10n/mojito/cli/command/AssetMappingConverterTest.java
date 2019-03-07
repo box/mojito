d1003c0845aeb2dd4bead6fbafcc018fd640e868
Jean Aurambault <aurambaj@users.noreply.github.com>
2019-03-07 13:47:17 -0800

    Add asset mapping option to the pull command (#416)
    This can be used to generate localized file for asset that are subset of a main asset. It can also be used for one off maintenance task to deal with renamed assets

---
+++ cli/src/test/java/com/box/l10n/mojito/cli/command/AssetMappingConverterTest.java
@@ -0,0 +1,34 @@
+package com.box.l10n.mojito.cli.command;
+
+import com.beust.jcommander.ParameterException;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class AssetMappingConverterTest {
+
+    @Test
+    public void testConvertNull() {
+        AssetMappingConverter assetMappingConverter = new AssetMappingConverter();
+        Map<String, String> convert = assetMappingConverter.convert(null);
+        assertNull(convert);
+    }
+
+    @Test
+    public void testConvertValid() {
+        AssetMappingConverter assetMappingConverter = new AssetMappingConverter();
+        Map<String, String> convert = assetMappingConverter.convert("1a:1b;2a:2b");
+        assertEquals("1b", convert.get("1a"));
+        assertEquals("2b", convert.get("2a"));
+        assertEquals(2, convert.size());
+    }
+
+    @Test(expected = ParameterException.class)
+    public void testConvertInvalid() {
+        AssetMappingConverter assetMappingConverter = new AssetMappingConverter();
+        Map<String, String> convert = assetMappingConverter.convert("dsafdsf");
+        assertEquals(0, convert.size());
+    }
+}
