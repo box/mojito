b8a8a0e829fc090b7c2c06bdc9b1e0fe6c25bd6a
Jean Aurambault <aurambaj@users.noreply.github.com>
2019-03-16 14:34:15 -0700

    Don't format asset id

--- webapp/src/main/java/com/box/l10n/mojito/service/assetExtraction/AssetExtractionService.java
+++ webapp/src/main/java/com/box/l10n/mojito/service/assetExtraction/AssetExtractionService.java
@@ -317,7 +317,7 @@ public class AssetExtractionService {
         processAssetJobInput.setFilterConfigIdOverride(filterConfigIdOverride);
         processAssetJobInput.setFilterOptions(filterOptions);
 
-        String pollableMessage = MessageFormat.format("Process asset: {0}", assetContentId);
+        String pollableMessage = MessageFormat.format("Process asset: {0}", assetContentId.toString());
 
         return quartzPollableTaskScheduler.scheduleJob(ProcessAssetJob.class, processAssetJobInput, parentTaskId, pollableMessage, 2);
     }
