1a8fd3edf4cd5dd93df1a21d39f53897d073b7c1
Jean Aurambault <aurambaj@users.noreply.github.com>
2019-04-24 14:30:38 -0700

    Use compile patterns

--- webapp/src/main/java/com/box/l10n/mojito/okapi/filters/UnescapeUtils.java
+++ webapp/src/main/java/com/box/l10n/mojito/okapi/filters/UnescapeUtils.java
@@ -4,8 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.util.regex.Pattern;
+
 /**
- *
  * @author emagalindan
  */
 @Component
@@ -15,6 +16,13 @@ public class UnescapeUtils {
      * Logger
      */
     static Logger logger = LoggerFactory.getLogger(UnescapeUtils.class);
+
+    private static final Pattern ESCAPED_CARIAGE_RETURN = Pattern.compile("\\\\r");
+    private static final Pattern ESCAPED_LINE_FEED = Pattern.compile("\\\\n");
+    private static final Pattern ESCAPED_QUOTES = Pattern.compile("\\\\(\"|')");
+    private static final Pattern ESCAPED_CHARACTERS = Pattern.compile("\\\\(.)?");
+    private static final Pattern SPACES = Pattern.compile("\\s+");
+    private static final Pattern LINE_FEED = Pattern.compile("\n");
 
     /**
      * Unescapes line feed, cariage return, single quote and double quote
@@ -30,11 +38,11 @@ public class UnescapeUtils {
     }
 
     String replaceEscapedCarriageReturn(String text) {
-        return text.replaceAll("\\\\r", "\r");
+        return ESCAPED_CARIAGE_RETURN.matcher(text).replaceAll("\r");
     }
 
     String replaceEscapedLineFeed(String text) {
-        return text.replaceAll("\\\\n", "\n");
+        return ESCAPED_LINE_FEED.matcher(text).replaceAll("\n");
     }
 
     /**
@@ -44,19 +52,19 @@ public class UnescapeUtils {
      * @return
      */
     String replaceEscapedQuotes(String text) {
-        return text.replaceAll("\\\\(\"|')", "$1");
+       return ESCAPED_QUOTES.matcher(text).replaceAll("$1");
     }
 
     /**
      * Replace other escape character with the character itself.
-     *
+     * <p>
      * Must be call after replacing espace sequence that need a different treatment like {@link #replaceEscapedLineFeed(String)}
      *
      * @param text
      * @return
      */
     String replaceEscapedCharacters(String text) {
-        return text.replaceAll("\\\\(.)?", "$1");
+        return ESCAPED_CHARACTERS.matcher(text).replaceAll("$1");
     }
 
     /**
@@ -66,10 +74,10 @@ public class UnescapeUtils {
      * @return
      */
     String collapseSpaces(String text) {
-        return text.replaceAll("\\s+", " ");
+        return SPACES.matcher(text).replaceAll(" ");
     }
 
     String replaceLineFeedWithSpace(String text) {
-        return text.replaceAll("\n", " ");
+        return LINE_FEED.matcher(text).replaceAll(" ");
     }
 }
