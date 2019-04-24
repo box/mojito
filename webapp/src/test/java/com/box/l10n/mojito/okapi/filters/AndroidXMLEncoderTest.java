1c0b3b57f93c899d33fcc2a5711314494269bcf9
Jean Aurambault <aurambaj@users.noreply.github.com>
2019-04-24 14:04:55 -0700

    Don't escape "annotation" tag in android filter
    fix #437

---
+++ webapp/src/test/java/com/box/l10n/mojito/okapi/filters/AndroidXMLEncoderTest.java
@@ -0,0 +1,43 @@
+package com.box.l10n.mojito.okapi.filters;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.*;
+
+public class AndroidXMLEncoderTest {
+
+    /**
+     * logger
+     */
+    static Logger logger = LoggerFactory.getLogger(AndroidXMLEncoderTest.class);
+
+    AndroidXMLEncoder androidXMLEncoder;
+
+    @Before
+    public void before() {
+        androidXMLEncoder = new AndroidXMLEncoder(false);
+        androidXMLEncoder.unescapeFilter = new UnescapeFilter();
+    }
+
+    @Test
+    public void testEscapeCommonAnnotation() {
+        String actual = androidXMLEncoder.escapeCommon("&lt;annotation font=\\\"title_emphasis\\\"&gt;Android&lt;/annotation&gt;!");
+        assertEquals("<annotation font=\"title_emphasis\">Android</annotation>!", actual);
+    }
+
+    @Test
+    public void testEscapeCommonB() {
+        String actual = androidXMLEncoder.escapeCommon("&lt;b&gt;Android&lt;/b&gt;!");
+        assertEquals("<b>Android</b>!", actual);
+    }
+
+    @Test
+    public void testEscapeDoubleQuotes() {
+        assertEquals("a\\\"b" , androidXMLEncoder.escapeDoubleQuotes("a\"b"));
+    }
+}
+
+
