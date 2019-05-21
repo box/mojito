85c1285afa947f530cd3946f0c14c278b17064e3
Jean Aurambault <aurambaj@users.noreply.github.com>
2019-05-21 09:31:14 -0700

    Refactor Smartling client to use standard OAuth2RestTemplate
    - implements a special SmartlingAuthorizationCodeAccessTokenProvider since smartling seems to be non standard
    - Added simple integration test, needs props (client id & secrets, etc) to be set in config file

--- webapp/src/test/java/com/box/l10n/mojito/smartling/SmartlingClientTest.java
+++ webapp/src/test/java/com/box/l10n/mojito/smartling/SmartlingClientTest.java
@@ -1,41 +1,66 @@
 package com.box.l10n.mojito.smartling;
 
 import com.box.l10n.mojito.smartling.response.SourceStringsResponse;
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
 import org.junit.Assume;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = {SmartlingClientTest.class})
+@SpringApplicationConfiguration(classes = {SmartlingClientTest.class, SmartlingClientConfiguration.class, SmartlingClient.class})
 @EnableAutoConfiguration
 @IntegrationTest("spring.datasource.initialize=false")
 public class SmartlingClientTest {
 
-    @Value("${l10n.smartling.userIdentifier:#{null}}")
-    String userIdentifier;
+    static Logger logger = LoggerFactory.getLogger(SmartlingClient.class);
 
-    @Value("${l10n.smartling.userSecret:#{null}}")
-    String userSecret;
+    @Autowired
+    SmartlingClient smartlingClient;
 
-    @Value("${test.l10n.smartling.projectId:abc123}")
-    String projectId;
+    @Value("${l10n.smartling.clientID:#{null}}")
+    String clientID;
 
-    @Value("${test.l10n.smartling.file:demo.properties}")
-    String file;
+    @Value("${l10n.smartling.clientSecret:#{null}")
+    String clientSecret;
 
-    @Value("${test.l10n.smartling.offset:0}")
-    Integer offset;
+    @Value("${test.l10n.smartling.projectId:#{null}}")
+    String projectId = null;
 
-    @Test
-    public void testClient() throws SmartlingClientException {
-        Assume.assumeNotNull(userIdentifier, userSecret);
-        SmartlingClient smartlingClient = new SmartlingClient(userIdentifier, userSecret);
-        SourceStringsResponse sourceStringsResponse = smartlingClient.getSourceStrings(projectId, file, offset);
+    @Value("${test.l10n.smartling.fileUri:#{null}}")
+    String fileUri = null;
+
+    @Before
+    public void init() {
+        Assume.assumeNotNull(clientID);
+        Assume.assumeNotNull(clientSecret);
     }
 
+    @Test
+    public void testGetSourceStrings() throws Exception {
+        Assume.assumeNotNull(projectId);
+        Assume.assumeNotNull(fileUri);
+
+        for (int i = 0; i < 3; i++) {
+            logger.debug("Test getSourceStrings");
+            SourceStringsResponse sourceStrings = smartlingClient.getSourceStrings(
+                    projectId,
+                    fileUri,
+                    0);
+            sourceStrings.getResponse().getData().getItems().stream().map(item -> item.getHashcode())
+                    .forEach(logger::debug);
+            Thread.sleep(4000L);
+        }
+    }
 }
