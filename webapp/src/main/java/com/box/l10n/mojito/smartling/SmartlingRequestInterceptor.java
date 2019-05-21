64a39634d8b2ea60cd6e221ed1185ff00535709e
Chidinma Egbukichi <chidinmae@users.noreply.github.com>
2019-05-20 20:14:00 -0700

    screenshots: push interceptor

--- webapp/src/main/java/com/box/l10n/mojito/smartling/SmartlingRequestInterceptor.java
+++ webapp/src/main/java/com/box/l10n/mojito/smartling/SmartlingRequestInterceptor.java
@@ -1,22 +1,50 @@
 package com.box.l10n.mojito.smartling;
 
 import org.springframework.http.HttpRequest;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.support.HttpRequestWrapper;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Smartling api uses userIdentifier and userSecret rather than OAuth spec
  */
 public class SmartlingRequestInterceptor implements ClientHttpRequestInterceptor {
+
+    private String matchParam(String bodyString, Pattern pattern) {
+        Matcher usernameMatcher = pattern.matcher(bodyString);
+        String returnString = "";
+        if (usernameMatcher.find()) {
+            returnString = usernameMatcher.group(1).split("&")[0];
+        }
+        return returnString;
+    }
+
     @Override
     public ClientHttpResponse intercept(HttpRequest request, byte[] body,
                                         ClientHttpRequestExecution execution) throws IOException {
         String bodyString = new String(body);
-        // pull the userIdentifier and userSecret here, currently in the request body as username & password
 
-        execution.execute(request, bodyString.getBytes());
+        Pattern usernamePattern = Pattern.compile("username=(.*)&");
+        String username = matchParam(bodyString, usernamePattern);
+
+        Pattern passwordPattern = Pattern.compile("&password=(.*)");
+        String password = matchParam(bodyString, passwordPattern);
+
+        String newBody = "{\"userIdentifier\": \"".concat(username).concat("\", \"userSecret\": \"").concat(password).concat("\"}");
+        return execution.execute(request, newBody.getBytes());
+    }
+
+    public class CustomHttpRequestWrapper extends HttpRequestWrapper {
+
+        public CustomHttpRequestWrapper(HttpRequest request) {
+            super(request);
+            request.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+        }
     }
 }
