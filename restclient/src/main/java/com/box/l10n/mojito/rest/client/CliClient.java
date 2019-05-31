980a4c1a1dc91dad650fd1ed2bc7c01a86749dcf
Jean Aurambault <aurambaj@users.noreply.github.com>
2019-05-31 15:53:29 -0700

    Add command to check if the CLI has the same version as the server.
    - if not, exit with error code
    - add WS that returns the server version (maven + git commit)

---
+++ restclient/src/main/java/com/box/l10n/mojito/rest/client/CliClient.java
@@ -0,0 +1,35 @@
+package com.box.l10n.mojito.rest.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+/**
+ * @author jeanaurambault
+ */
+@Component
+public class CliClient extends BaseClient {
+
+    /**
+     * logger
+     */
+    static Logger logger = LoggerFactory.getLogger(CliClient.class);
+
+    @Override
+    public String getBasePath() {
+        return "";
+    }
+
+    @Override
+    public String getEntityName() {
+        return "cli";
+    }
+
+    public String getVersion() {
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromPath(getBasePathForEntity()).pathSegment("version");
+        logger.error(uriComponentsBuilder.toUriString());
+        return authenticatedRestTemplate.getForObject(uriComponentsBuilder.toUriString(), String.class);
+    }
+
+}
