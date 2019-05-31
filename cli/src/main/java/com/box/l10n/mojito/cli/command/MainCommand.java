db80e88c1d10823f738651ae7501c4c48628665a
Jean Aurambault <aurambaj@users.noreply.github.com>
2019-05-31 15:53:29 -0700

    Add command to check if the CLI has the same version as the server.
    - if not, exit with error code
    - add WS that returns the server version (maven + git commit)

--- cli/src/main/java/com/box/l10n/mojito/cli/command/MainCommand.java
+++ cli/src/main/java/com/box/l10n/mojito/cli/command/MainCommand.java
@@ -4,6 +4,7 @@ import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 import com.box.l10n.mojito.cli.ConsoleWriter;
 import com.box.l10n.mojito.cli.GitInfo;
+import com.box.l10n.mojito.rest.client.CliClient;
 import org.fusesource.jansi.Ansi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -25,11 +26,17 @@ public class MainCommand extends Command {
     @Parameter(names = {"--version", "-v"}, description = "Show version")
     private boolean versionParam;
 
+    @Parameter(names = {"--check-server-version"}, description = "Check if this CLI matches the server's version")
+    private boolean checkVersionServerParam;
+
     @Value("${info.build.version}")
     String version;
 
     @Autowired
     GitInfo gitInfo;
+
+    @Autowired
+    CliClient cliClient;
 
     @Override
     void showUsage() {
@@ -39,16 +46,38 @@ public class MainCommand extends Command {
     @Override
     protected void execute() throws CommandException {
         if (versionParam) {
-            consoleWriter.fg(Ansi.Color.CYAN).a(version).reset();
-
-            if (gitInfo.getCommit().getId() != null) {
-                consoleWriter.a(" (git commit id: ").a(gitInfo.getCommit().getId()).a(")");
-            }
-
-            consoleWriter.println();
+            showVersion();
+        } else if (checkVersionServerParam) {
+            checkServerVersion();
         } else {
             showUsage();
         }
+    }
+
+    void checkServerVersion() throws CommandException {
+        String serverVersion = cliClient.getVersion();
+        String cliVersion = getCliVersion();
+        if (!cliVersion.equals(serverVersion)) {
+            throw new CommandException("CLI version: " + cliVersion + " not the same as the server version: " + serverVersion);
+        }
+    }
+
+    void showVersion() {
+        consoleWriter.fg(Ansi.Color.CYAN).a(version).reset();
+
+        if (gitInfo.getCommit().getId() != null) {
+            consoleWriter.a(" (git commit id: ").a(gitInfo.getCommit().getId()).a(")");
+        }
+
+        consoleWriter.println();
+    }
+
+    String getCliVersion() {
+        String fullVersion = version;
+        if (gitInfo.getCommit() != null) {
+            fullVersion += " (git commit id: " + gitInfo.getCommit().getId() + ")";
+        }
+        return fullVersion;
     }
 
 }
