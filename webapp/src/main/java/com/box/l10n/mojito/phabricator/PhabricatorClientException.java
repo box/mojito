49f4187addf9832a0d0a5076bfc83a3cecb29297
Jean Aurambault <aurambaj@users.noreply.github.com>
2019-06-12 13:36:31 -0700

    Add Phabricator client to add/remove reviewer and to add comment to PRs

---
+++ webapp/src/main/java/com/box/l10n/mojito/phabricator/PhabricatorClientException.java
@@ -0,0 +1,8 @@
+package com.box.l10n.mojito.phabricator;
+
+public class PhabricatorClientException extends Exception {
+
+    public PhabricatorClientException(String message) {
+        super(message);
+    }
+}
