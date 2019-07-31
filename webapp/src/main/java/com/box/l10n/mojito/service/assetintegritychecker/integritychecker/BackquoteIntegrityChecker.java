9b9fbce31e9ebc8d1c74e2f274f538d2a3205b2f
Jee Yi <jyi@box.com>
2019-07-31 15:48:54 -0700

    Implemented backquote integrity checker to preserve strings in

---
+++ webapp/src/main/java/com/box/l10n/mojito/service/assetintegritychecker/integritychecker/BackquoteIntegrityChecker.java
@@ -0,0 +1,33 @@
+package com.box.l10n.mojito.service.assetintegritychecker.integritychecker;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Checks that there are the same backquoted strings in the source and target
+ * content.
+ *
+ * @author jyi
+ */
+public class BackquoteIntegrityChecker extends RegexIntegrityChecker {
+
+    /**
+     * logger
+     */
+    static Logger logger = LoggerFactory.getLogger(BackquoteIntegrityChecker.class);
+
+    @Override
+    public String getRegex() {
+        return "`.*?`";
+    }
+
+    @Override
+    public void check(String sourceContent, String targetContent) throws BackquoteIntegrityCheckerException {
+
+        try {
+            super.check(sourceContent, targetContent);
+        } catch (RegexCheckerException rce) {
+            throw new BackquoteIntegrityCheckerException((rce.getMessage()));
+        }
+    }
+}
