b0351ba91b87683e6f1a9f0720447e74f89902dd
Jee Yi <jyi@box.com>
2019-08-08 10:51:33 -0700

    Escaping of GT and LineBreak in text unit id for xcode xliff

---
+++ webapp/src/main/java/com/box/l10n/mojito/okapi/XcodeXliffSkeletonWriter.java
@@ -0,0 +1,48 @@
+package com.box.l10n.mojito.okapi;
+
+import net.sf.okapi.common.LocaleId;
+import net.sf.okapi.common.encoder.EncoderContext;
+import net.sf.okapi.common.resource.ITextUnit;
+import net.sf.okapi.filters.xliff.Parameters;
+import net.sf.okapi.filters.xliff.XLIFFSkeletonWriter;
+
+/**
+ *
+ * @author jyi
+ */
+public class XcodeXliffSkeletonWriter extends XLIFFSkeletonWriter {
+
+    public XcodeXliffSkeletonWriter(Parameters params) {
+        super(params);
+    }
+
+    @Override
+    protected String getString(ITextUnit tu,
+            LocaleId locToUse,
+            EncoderContext context) {
+        String tuString = super.getString(tu, locToUse, context);
+        return encodeTransUnitId(tuString);
+    }
+
+    private String encodeTransUnitId(String tuString) {
+        String tuIdStart = "<trans-unit id=\"";
+        int start = tuString.indexOf(tuIdStart);
+        if (start < 0) {
+            return tuString;
+        } else {
+            start += tuIdStart.length();
+            int end = tuString.indexOf("\"", start);
+
+            String tuIdString = tuString.substring(start, end);
+            tuIdString = tuIdString.replaceAll(">", "&gt;");
+            tuIdString = tuIdString.replaceAll("\n", "&#10;");
+
+            StringBuilder encoded = new StringBuilder();
+            encoded.append(tuString.substring(0, start));
+            encoded.append(tuIdString);
+            encoded.append(tuString.substring(end, tuString.length()));
+
+            return encoded.toString();
+        }
+    }
+}
--- webapp/src/main/java/com/box/l10n/mojito/okapi/filters/XcodeXliffFilter.java
+++ webapp/src/main/java/com/box/l10n/mojito/okapi/filters/XcodeXliffFilter.java
@@ -1,14 +1,16 @@
 package com.box.l10n.mojito.okapi.filters;
 
+import com.box.l10n.mojito.okapi.XcodeXliffSkeletonWriter;
 import java.util.List;
 import net.sf.okapi.common.Event;
-import net.sf.okapi.common.IParameters;
 import net.sf.okapi.common.MimeTypeMapper;
-import net.sf.okapi.common.encoder.EncoderManager;
-import static net.sf.okapi.common.encoder.XMLEncoder.ESCAPEGT;
-import static net.sf.okapi.common.encoder.XMLEncoder.ESCAPELINEBREAK;
 import net.sf.okapi.common.filters.FilterConfiguration;
+import net.sf.okapi.common.resource.ITextUnit;
+import net.sf.okapi.common.skeleton.GenericSkeleton;
+import net.sf.okapi.common.skeleton.GenericSkeletonPart;
+import net.sf.okapi.common.skeleton.ISkeletonWriter;
 import net.sf.okapi.filters.xliff.XLIFFFilter;
+import net.sf.okapi.filters.xliff.XLIFFSkeletonWriter;
 import org.springframework.beans.factory.annotation.Configurable;
 
 /**
@@ -33,14 +35,15 @@ public class XcodeXliffFilter extends XLIFFFilter {
                 getClass().getName(),
                 "XCODEXLIFF",
                 "Configuration for XCODE XLIFF documents. Supports XCODE specific metadata",
-                "xcode_mojito.fprm",
+                null,
                 ".xliff"));
         return list;
     }
-    
-	@Override
-	public Event next () {
-            return super.next();
-        }
+
+    @Override
+    public ISkeletonWriter createSkeletonWriter() {
+        XLIFFSkeletonWriter writer = new XcodeXliffSkeletonWriter(getParameters());
+        return writer;
+    }
 
 }
