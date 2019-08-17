5040aa1da3f9b13f9ace5c44d730d54de2dc1760
Jee Yi <jyi@box.com>
2019-08-17 09:57:01 -0700

    Additional option --fully-translated for pull command (#478)
    This option is used to prevent partially translated localized files. When this option is specified, it will check the number of untranslated strings for the locale. If there are untranslated strings, the localized file is not generated.
    
    With this option, localized files are generated for partially translated locales (child locales that inherit translations from the parent locale) only if their parent locales are fully translated.

--- cli/src/test/java/com/box/l10n/mojito/cli/command/PullCommandTest.java
+++ cli/src/test/java/com/box/l10n/mojito/cli/command/PullCommandTest.java
@@ -694,4 +694,31 @@ public class PullCommandTest extends CLITestBase {
 
         checkExpectedGeneratedResources();
     }
+
+    @Test
+    public void pullFullyTranslated() throws Exception {
+
+        Repository repository = createTestRepoUsingRepoService();
+
+        getL10nJCommander().run("push", "-r", repository.getName(),
+                "-s", getInputResourcesTestDir("source").getAbsolutePath());
+
+        Asset asset = assetClient.getAssetByPathAndRepositoryId("demo.properties", repository.getId());
+        importTranslations(asset.getId(), "source-xliff_", "fr-FR");
+        importTranslations(asset.getId(), "source-xliff_", "ja-JP");
+
+        getL10nJCommander().run("pull", "-r", repository.getName(),
+                "-s", getInputResourcesTestDir("source").getAbsolutePath(),
+                "-t", getTargetTestDir("target").getAbsolutePath());
+
+        // allow time for repo stats to be updated
+        Thread.sleep(1000);
+
+        getL10nJCommander().run("pull", "-r", repository.getName(),
+                "-s", getInputResourcesTestDir("source").getAbsolutePath(),
+                "-t", getTargetTestDir("target_fully_translated").getAbsolutePath(),
+                "--fully-translated");
+
+        checkExpectedGeneratedResources();
+    }
 }
---
+++ restclient/src/main/java/com/box/l10n/mojito/rest/entity/RepositoryStatistic.java
@@ -0,0 +1,22 @@
+package com.box.l10n.mojito.rest.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author jyi
+ */
+public class RepositoryStatistic {
+
+    private Set<RepositoryLocaleStatistic> repositoryLocaleStatistics = new HashSet<>();
+
+    public Set<RepositoryLocaleStatistic> getRepositoryLocaleStatistics() {
+        return repositoryLocaleStatistics;
+    }
+
+    public void setRepositoryLocaleStatistics(Set<RepositoryLocaleStatistic> repositoryLocaleStatistics) {
+        this.repositoryLocaleStatistics = repositoryLocaleStatistics;
+    }
+
+}
