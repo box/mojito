418d8a3cb7b232b155045f024c6d68af6db95138
Edwin Hoogerbeets <ehoogerbeets@box.com>
2019-09-23 13:49:44 -0700

    Stub for History Service object methods

--- webapp/src/main/java/com/box/l10n/mojito/service/tm/TMTextUnitHistoryService.java
+++ webapp/src/main/java/com/box/l10n/mojito/service/tm/TMTextUnitHistoryService.java
@@ -3,7 +3,11 @@
  */
 package com.box.l10n.mojito.service.tm;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.box.l10n.mojito.entity.TMTextUnitVariant;
 
 /**
  * @author ehoogerbeets
@@ -11,5 +15,7 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class TMTextUnitHistoryService {
-
+    public List<TMTextUnitVariant> findHistory(Long localeId, Long tmId) {
+        return null;
+    }
 }
