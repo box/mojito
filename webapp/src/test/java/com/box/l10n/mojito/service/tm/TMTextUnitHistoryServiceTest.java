b17bc8282d7e1c0eccc5bb5b8750018265de3bb2
Edwin Hoogerbeets <ehoogerbeets@box.com>
2019-10-01 22:55:04 -0700

    Cleanup to address Jean's PR review comments

--- webapp/src/test/java/com/box/l10n/mojito/service/tm/TMTextUnitHistoryServiceTest.java
+++ webapp/src/test/java/com/box/l10n/mojito/service/tm/TMTextUnitHistoryServiceTest.java
@@ -46,11 +46,6 @@ public class TMTextUnitHistoryServiceTest extends ServiceTestBase {
     @Autowired
     TMTextUnitHistoryService tmHistoryService;
 
-    /*
-    @Autowired
-    TMTextUnitVariantRepository tmTextUnitVariantRepository;
-    */
-
     @Autowired
     TMTextUnitRepository tmTextUnitRepository;
 
