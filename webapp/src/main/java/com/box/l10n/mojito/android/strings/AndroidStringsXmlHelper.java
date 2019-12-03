e2795886e0c134568460bc0e5b7096ced4a275a5
Sergey Demyanenko <sdemyanenko@pinterest.com>
2019-12-03 13:53:13 -0800

    Added classes for handling Android XML

---
+++ webapp/src/main/java/com/box/l10n/mojito/android/strings/AndroidStringsXmlHelper.java
@@ -0,0 +1,103 @@
+package com.box.l10n.mojito.android.strings;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+public class AndroidStringsXmlHelper {
+
+    public static final String DEFAULT_PLURAL_SEPARATOR = " _";
+
+    public static final String ROOT_ELEMENT_NAME = "resources";
+    public static final String SINGULAR_ELEMENT_NAME = "string";
+    public static final String PLURAL_ELEMENT_NAME = "plurals";
+    public static final String PLURAL_ITEM_ELEMENT_NAME = "item";
+    public static final String NAME_ATTRIBUTE_NAME = "name";
+    public static final String QUANTITY_ATTRIBUTE_NAME = "quantity";
+    public static final String ID_ATTRIBUTE_NAME = "tmTextUnitId";
+
+    public enum PluralItem {
+        zero,
+        one,
+        two,
+        few,
+        many,
+        other
+    }
+
+    public static AndroidStringsTextUnit createTextUnit(String name, String content, String comment, String id, String pluralForm, String pluralFormOther) {
+        return new AndroidStringsTextUnit(name, content, comment, id, pluralForm, pluralFormOther);
+    }
+
+    public static AndroidStringsTextUnit createTextUnit(String name, String content, String comment, Long id, String pluralForm, String pluralFormOther) {
+        return createTextUnit(name, content, comment, id == null ? null : id.toString(), pluralForm, pluralFormOther);
+    }
+
+    public static AndroidStringsTextUnit createSingular(String name, String content, String comment, String id) {
+        return createTextUnit(name, content, comment, id, null, null);
+    }
+
+    public static AndroidStringsTextUnit createSingular(String name, String content, String comment) {
+        return createSingular(name, content, comment, null);
+    }
+
+    public static AndroidStringsTextUnit createPlural(String pluralName, PluralItem pluralItem, String content, String comment, String id, String pluralNameSeparator) {
+        return createTextUnit(pluralName + pluralNameSeparator + pluralItem.name(), content, comment, id, pluralItem.name(), pluralName + pluralNameSeparator + PluralItem.other.name());
+    }
+
+    public static AndroidStringsTextUnit createPlural(String pluralName, PluralItem pluralItem, String content, String comment) {
+        return createPlural(pluralName, pluralItem, content, comment, null, DEFAULT_PLURAL_SEPARATOR);
+    }
+
+    static Element createChild(Document document, Node node, String name) {
+        Element result = document.createElement(name);
+        node.appendChild(result);
+
+        return result;
+    }
+
+    static void addText(Element element, String text) {
+        if (text != null) {
+            element.setTextContent(text);
+        }
+    }
+
+    static void addComment(Document document, Node node, String comment) {
+        if (comment != null) {
+            node.appendChild(document.createComment(comment));
+        }
+    }
+
+    static void addAttribute(Element element, String name, String value) {
+        if (value != null) {
+            element.setAttribute(name, value);
+        }
+    }
+
+    static String getAttribute(Element element, String attributeName) {
+        String result = null;
+        if (element.hasAttribute(attributeName)) {
+            result = element.getAttribute(attributeName);
+        }
+
+        return result;
+    }
+
+    static String getContent(Node node) {
+        String result = null;
+        if (node != null) {
+            result = node.getTextContent();
+        }
+
+        return result;
+    }
+
+    static DocumentBuilder getDocumentBuilder() throws ParserConfigurationException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        return factory.newDocumentBuilder();
+    }
+}
---
+++ webapp/src/test/java/com/box/l10n/mojito/android/strings/AndroidStringsXmlWriterTest.java
@@ -0,0 +1,157 @@
+package com.box.l10n.mojito.android.strings;
+
+import org.junit.Test;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static com.box.l10n.mojito.android.strings.AndroidStringsXmlHelper.*;
+import static org.junit.Assert.*;
+
+public class AndroidStringsXmlWriterTest {
+
+    @Test
+    public void addStringTest() throws TransformerException, ParserConfigurationException {
+        List<AndroidStringsTextUnit> androidStringsTextUnitList = Collections.singletonList(
+                createSingular("name", "content", "comment"));
+        
+        assertEquals(
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?><resources>\n" +
+                        "<!--comment-->\n" +
+                        "<string name=\"name\">content</string>\n" +
+                        "</resources>\n",
+                AndroidStringsXmlWriter.toText(androidStringsTextUnitList));
+    }
+
+    @Test
+    public void addQuotesTest() throws TransformerException, ParserConfigurationException {
+        List<AndroidStringsTextUnit> androidStringsTextUnitList = Collections.singletonList(
+                createSingular("name\"", "content\"", "comment"));
+
+        assertEquals(
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?><resources>\n" +
+                        "<!--comment-->\n" +
+                        "<string name=\"name&quot;\">content\\\"</string>\n" +
+                        "</resources>\n",
+                AndroidStringsXmlWriter.toText(androidStringsTextUnitList));
+    }
+    
+    @Test
+    public void addPluralStringTest() throws TransformerException, ParserConfigurationException {
+
+        List<AndroidStringsTextUnit> androidStringsTextUnitList = Arrays.asList(
+                createPlural("name", PluralItem.one, "singular", "comment"),
+                createPlural("name", PluralItem.other, "plural", "comment"));
+
+        assertEquals(
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?><resources>\n" +
+                        "<!--comment-->\n" +
+                        "<plurals name=\"name\">\n" +
+                        "<item quantity=\"one\">singular</item>\n" +
+                        "<item quantity=\"other\">plural</item>\n" +
+                        "</plurals>\n" +
+                        "</resources>\n",
+                AndroidStringsXmlWriter.toText(androidStringsTextUnitList));
+    }
+    
+    @Test
+    public void addMultipleStringTest() throws TransformerException, ParserConfigurationException {
+
+        List<AndroidStringsTextUnit> androidStringsTextUnitList = Arrays.asList(
+                createSingular("name0", "content0", "comment0"),
+                createPlural("name1", PluralItem.one, "singular1", "comment1"),
+                createPlural("name1", PluralItem.other, "plural1", "comment1"),
+                createPlural("name3", PluralItem.one, "singular3", "comment3"),
+                createSingular("name2", "content2", "comment2"),
+                createPlural("name3", PluralItem.other, "plural3", "comment3"));
+
+        assertEquals(
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?><resources>\n" +
+                        "<!--comment0-->\n" +
+                        "<string name=\"name0\">content0</string>\n" +
+                        "<!--comment1-->\n" +
+                        "<plurals name=\"name1\">\n" +
+                        "<item quantity=\"one\">singular1</item>\n" +
+                        "<item quantity=\"other\">plural1</item>\n" +
+                        "</plurals>\n" +
+                        "<!--comment2-->\n" +
+                        "<string name=\"name2\">content2</string>\n" +
+                        "<!--comment3-->\n" +
+                        "<plurals name=\"name3\">\n" +
+                        "<item quantity=\"one\">singular3</item>\n" +
+                        "<item quantity=\"other\">plural3</item>\n" +
+                        "</plurals>\n" +
+                        "</resources>\n",
+                AndroidStringsXmlWriter.toText(androidStringsTextUnitList));
+    }
+
+    @Test
+    public void listIsEmptyTest() throws TransformerException, ParserConfigurationException {
+        assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?><resources/>\n",
+                AndroidStringsXmlWriter.toText(Collections.emptyList()));
+    }
+
+    @Test
+    public void listTest() throws TransformerException, ParserConfigurationException {
+        List<AndroidStringsTextUnit> androidStringsTextUnitList = Arrays.asList(
+                createSingular("name0", "content0", "comment0"),
+                createPlural("name1", PluralItem.other, "content1_plural", "comment1"),
+                createPlural("name1", PluralItem.one, "content1_singular", "comment1"),
+                createSingular("name2", "content2", "comment2"));
+
+        assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?><resources>\n" +
+                        "<!--comment0-->\n" +
+                        "<string name=\"name0\">content0</string>\n" +
+                        "<!--comment1-->\n" +
+                        "<plurals name=\"name1\">\n" +
+                        "<item quantity=\"one\">content1_singular</item>\n" +
+                        "<item quantity=\"other\">content1_plural</item>\n" +
+                        "</plurals>\n" +
+                        "<!--comment2-->\n" +
+                        "<string name=\"name2\">content2</string>\n" +
+                        "</resources>\n",
+                AndroidStringsXmlWriter.toText(androidStringsTextUnitList));
+    }
+
+    @Test
+    public void listWithIdTest() throws TransformerException, ParserConfigurationException {
+        List<AndroidStringsTextUnit> androidStringsTextUnitList = Arrays.asList(
+                createSingular("name0", "content0", "comment0","123"),
+                createPlural("name1", PluralItem.other, "content1_plural", "comment1","124", DEFAULT_PLURAL_SEPARATOR),
+                createPlural("name1", PluralItem.one, "content1_singular", "comment1","125", DEFAULT_PLURAL_SEPARATOR),
+                createSingular("name2", "content2", "comment2","126"));
+
+        assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?><resources>\n" +
+                        "<!--comment0-->\n" +
+                        "<string name=\"name0\" tmTextUnitId=\"123\">content0</string>\n" +
+                        "<!--comment1-->\n" +
+                        "<plurals name=\"name1\">\n" +
+                        "<item quantity=\"one\" tmTextUnitId=\"125\">content1_singular</item>\n" +
+                        "<item quantity=\"other\" tmTextUnitId=\"124\">content1_plural</item>\n" +
+                        "</plurals>\n" +
+                        "<!--comment2-->\n" +
+                        "<string name=\"name2\" tmTextUnitId=\"126\">content2</string>\n" +
+                        "</resources>\n",
+                AndroidStringsXmlWriter.toText(androidStringsTextUnitList));
+    }
+
+    @Test
+    public void listOtherTest() throws TransformerException, ParserConfigurationException, IOException {
+        List<AndroidStringsTextUnit> androidStringsTextUnitList = Collections.singletonList(
+                createPlural("name1", PluralItem.other, "content1", "comment1"));
+
+        assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?><resources>\n" +
+                        "<!--comment1-->\n" +
+                        "<plurals name=\"name1\">\n" +
+                        "<item quantity=\"other\">content1</item>\n" +
+                        "</plurals>\n" +
+                        "</resources>\n",
+                AndroidStringsXmlWriter.toText(androidStringsTextUnitList));
+    }
+
+}
