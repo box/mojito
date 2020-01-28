2f0b43536b6bbc1c368e122b161f056379169618
Jean Aurambault <aurambaj@users.noreply.github.com>
2020-01-28 09:12:42 -0800

    Add utility class to run shell commands (`sh` required: linux, OSX, etc)
    - Capture exit code, output and error stream into a Result object
    - Make sure the stream are properly consumed

---
+++ common/src/main/java/com/box/l10n/mojito/cli/shell/StreamGobbler.java
@@ -0,0 +1,21 @@
+package com.box.l10n.mojito.cli.shell;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.function.Consumer;
+
+class StreamGobbler implements Runnable {
+    InputStream inputStream;
+    Consumer<String> consumer;
+
+    public StreamGobbler(InputStream inputStream, Consumer<String> consumer) {
+        this.inputStream = inputStream;
+        this.consumer = consumer;
+    }
+
+    @Override
+    public void run() {
+        new BufferedReader(new InputStreamReader(inputStream)).lines().forEach(consumer);
+    }
+}
