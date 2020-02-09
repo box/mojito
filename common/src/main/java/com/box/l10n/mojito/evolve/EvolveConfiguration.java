221bbdb0321e5bad1dff84a34c885e1ec31a1092
Jean Aurambault <aurambaj@users.noreply.github.com>
2020-02-08 20:12:19 -0800

    Add Evolve client
    Allow to download courses to be translated and upload localized versions back

---
+++ common/src/main/java/com/box/l10n/mojito/evolve/EvolveConfiguration.java
@@ -0,0 +1,64 @@
+package com.box.l10n.mojito.evolve;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriTemplateHandler;
+
+import java.io.IOException;
+
+@ConditionalOnProperty("l10n.evolve.url")
+@Configuration
+public class EvolveConfiguration {
+
+    static Logger resttemplateLogger = LoggerFactory.getLogger(EvolveConfiguration.class.getPackage().getName() + ".Resttemplate");
+
+    @Autowired
+    EvolveConfigurationProperties evolveConfigurationProperties;
+
+    @Bean
+    Evolve evolveClient() {
+        return new Evolve(evolveRestTemplate());
+    }
+
+    RestTemplate evolveRestTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        DefaultUriTemplateHandler defaultUriTemplateHandler = new DefaultUriTemplateHandler();
+        defaultUriTemplateHandler.setBaseUrl(evolveConfigurationProperties.getUrl());
+        restTemplate.setUriTemplateHandler(defaultUriTemplateHandler);
+
+        restTemplate.getInterceptors().add(new ClientHttpRequestInterceptor() {
+            @Override
+            public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+                request.getHeaders().add(HttpHeaders.AUTHORIZATION, evolveConfigurationProperties.getToken());
+                return execution.execute(request, body);
+            }
+        });
+
+        restTemplate.setErrorHandler(new DefaultResponseErrorHandler() {
+            @Override
+            public void handleError(ClientHttpResponse response) throws IOException {
+                try {
+                    super.handleError(response);
+                } catch (HttpServerErrorException | HttpClientErrorException e) {
+                    resttemplateLogger.debug(e.getResponseBodyAsString());
+                    throw e;
+                }
+            }
+        });
+
+        return restTemplate;
+    }
+}
