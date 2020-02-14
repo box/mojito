9f0f02a6f4f10834237215e6ea89ebf17e28c029
Jean Aurambault <aurambaj@users.noreply.github.com>
2020-02-13 19:20:02 -0800

    Share extractor logic between webapp and cli

--- cli/src/main/java/com/box/l10n/mojito/cli/command/extraction/TextUnitComparators.java
+++ cli/src/main/java/com/box/l10n/mojito/cli/command/extraction/TextUnitComparators.java
@@ -7,9 +7,9 @@ public class TextUnitComparators {
     /**
      * Sort text units by filename, name, source and then comments
      */
-    public static final Comparator<TextUnitWithAssetPath> BY_FILENAME_NAME_SOURCE_COMMENTS = Comparator
-            .comparing(TextUnitWithAssetPath::getAssetPath, Comparator.nullsFirst(Comparator.naturalOrder()))
-            .thenComparing(TextUnitWithAssetPath::getName, Comparator.nullsFirst(Comparator.naturalOrder()))
-            .thenComparing(TextUnitWithAssetPath::getSource, Comparator.nullsFirst(Comparator.naturalOrder()))
-            .thenComparing(TextUnitWithAssetPath::getComments, Comparator.nullsFirst(Comparator.naturalOrder()));
+    public static final Comparator<AssetExtractorTextUnitWithAssetPath> BY_FILENAME_NAME_SOURCE_COMMENTS = Comparator
+            .comparing(AssetExtractorTextUnitWithAssetPath::getAssetPath, Comparator.nullsFirst(Comparator.naturalOrder()))
+            .thenComparing(AssetExtractorTextUnitWithAssetPath::getName, Comparator.nullsFirst(Comparator.naturalOrder()))
+            .thenComparing(AssetExtractorTextUnitWithAssetPath::getSource, Comparator.nullsFirst(Comparator.naturalOrder()))
+            .thenComparing(AssetExtractorTextUnitWithAssetPath::getComments, Comparator.nullsFirst(Comparator.naturalOrder()));
 }
---
+++ common/src/main/java/com/box/l10n/mojito/okapi/extractor/AssetExtractorTextUnit.java
@@ -0,0 +1,60 @@
+package com.box.l10n.mojito.okapi.extractor;
+
+import java.util.Set;
+
+public class AssetExtractorTextUnit {
+    String name;
+    String source;
+    String comments;
+    String pluralForm;
+    String pluralFormOther;
+    Set<String> usages;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public String getPluralForm() {
+        return pluralForm;
+    }
+
+    public void setPluralForm(String pluralForm) {
+        this.pluralForm = pluralForm;
+    }
+
+    public String getPluralFormOther() {
+        return pluralFormOther;
+    }
+
+    public void setPluralFormOther(String pluralFormOther) {
+        this.pluralFormOther = pluralFormOther;
+    }
+
+    public Set<String> getUsages() {
+        return usages;
+    }
+
+    public void setUsages(Set<String> usages) {
+        this.usages = usages;
+    }
+}
