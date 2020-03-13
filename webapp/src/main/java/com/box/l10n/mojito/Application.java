cf6e9935bb99bbdcee7b50261a4dd33a76e7b6a5
Jean Aurambault <aurambaj@users.noreply.github.com>
2020-03-12 18:25:46 -0700

    new @EntityScan

--- webapp/src/main/java/com/box/l10n/mojito/Application.java
+++ webapp/src/main/java/com/box/l10n/mojito/Application.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
