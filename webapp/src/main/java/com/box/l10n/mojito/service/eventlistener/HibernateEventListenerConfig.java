20952a36ce687fd33bd5e00643846d2cc03b69be
Jean Aurambault <aurambaj@users.noreply.github.com>
2020-03-13 11:20:49 -0700

    make hibernate compile - doens't work for now

--- webapp/src/main/java/com/box/l10n/mojito/service/eventlistener/HibernateEventListenerConfig.java
+++ webapp/src/main/java/com/box/l10n/mojito/service/eventlistener/HibernateEventListenerConfig.java
@@ -1,17 +1,18 @@
 package com.box.l10n.mojito.service.eventlistener;
 
-import javax.annotation.PostConstruct;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.event.service.spi.EventListenerRegistry;
 import org.hibernate.event.spi.EventType;
-import org.hibernate.internal.SessionFactoryImpl;
 import org.hibernate.jpa.internal.EntityManagerFactoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 /**
  * Registers hibernate event listeners.
- * 
+ *
  * @author jyi
  */
 @Component
@@ -19,14 +20,15 @@ public class HibernateEventListenerConfig {
 
     @Autowired
     LocalContainerEntityManagerFactoryBean lcemfb;
-    
+
     @Autowired
     EntityCrudEventListener entityCrudEventListener;
 
     @PostConstruct
     public void registerListeners() {
         EntityManagerFactoryImpl emf = (EntityManagerFactoryImpl) lcemfb.getNativeEntityManagerFactory();
-        SessionFactoryImpl sf = emf.getSessionFactory();
+        // TODO must test that
+        SessionFactoryImplementor sf = emf.getSessionFactory();
         EventListenerRegistry registry = (EventListenerRegistry)sf.getServiceRegistry().getService(EventListenerRegistry.class);
         registry.getEventListenerGroup(EventType.POST_COMMIT_INSERT).appendListener(entityCrudEventListener);
         registry.getEventListenerGroup(EventType.POST_COMMIT_UPDATE).appendListener(entityCrudEventListener);
