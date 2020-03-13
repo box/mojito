238f9c3642bfe428ff8e239d382d03cf05e6ad09
Jean Aurambault <aurambaj@users.noreply.github.com>
2020-03-13 11:20:49 -0700

    make hibernate compile - doens't work for now

--- webapp/src/test/java/com/box/l10n/mojito/db/DDLGenerator.java
+++ webapp/src/test/java/com/box/l10n/mojito/db/DDLGenerator.java
@@ -1,26 +1,27 @@
 package com.box.l10n.mojito.db;
 
 import com.box.l10n.mojito.service.assetExtraction.ServiceTestBase;
-import java.io.IOException;
-import java.lang.reflect.Field;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.internal.SessionFactoryImpl;
+import org.hibernate.boot.spi.MetadataImplementor;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.jpa.internal.EntityManagerFactoryImpl;
 import org.hibernate.service.internal.SessionFactoryServiceRegistryImpl;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
 import org.hibernate.tool.hbm2ddl.SchemaUpdate;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
  * Not a test, this generates db migration scripts, that can be used to write
  * actual migration script stored in db/migration
- * 
+ *
  * @author jaurambault
  */
 public class DDLGenerator extends ServiceTestBase {
@@ -32,35 +33,38 @@ public class DDLGenerator extends ServiceTestBase {
 
     @Autowired
     LocalContainerEntityManagerFactoryBean lcemfb;
-    
+
+    @Ignore("TODO see if we can still support that or if we remove it")
     @Test
     public void generateCreateAnUpdateDDL() throws IOException {
         logger.debug("Generate create and update DDL");
-        
+
         EntityManagerFactoryImpl emf = (EntityManagerFactoryImpl) lcemfb.getNativeEntityManagerFactory();
-        SessionFactoryImpl sf = emf.getSessionFactory();
+
+        SessionFactoryImplementor sf = emf.getSessionFactory();
         SessionFactoryServiceRegistryImpl serviceRegistry = (SessionFactoryServiceRegistryImpl) sf.getServiceRegistry();
-        Configuration cfg = null;
-        
-        try {
-            Field field = SessionFactoryServiceRegistryImpl.class.getDeclaredField("configuration");
-            field.setAccessible(true);
-            cfg = (Configuration) field.get(serviceRegistry);
-        } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
-        
+
+        MetadataImplementor metadata = null;
+
+//        try {
+//            Field field = SessionFactoryServiceRegistryImpl.class.getDeclaredField("configuration");
+//            field.setAccessible(true);
+//            metadata = (MetadataImplementor) field.get(serviceRegistry);
+//        } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+//            throw new RuntimeException(e);
+//        }
+
         Files.createDirectories(Paths.get("target/db/migration/"));
-        
-        SchemaUpdate update = new SchemaUpdate(serviceRegistry, cfg);
+
+        SchemaUpdate update = new SchemaUpdate(serviceRegistry, metadata);
         update.setDelimiter(";");
         update.setOutputFile("target/db/migration/Vx__yy_zz.sql");
         update.execute(false, false);
-        
-        SchemaExport export = new SchemaExport(serviceRegistry, cfg);
+
+        SchemaExport export = new SchemaExport(serviceRegistry, metadata);
         export.setDelimiter(";");
         export.setOutputFile("target/db/migration/create.sql");
-        export.execute(false, false, false, true);     
+        export.execute(false, false, false, true);
     }
-    
+
 }
