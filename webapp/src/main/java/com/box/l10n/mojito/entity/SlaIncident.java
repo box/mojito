ab8feb8efb1df4cdc4ddd4862f55f9df95f07a19
Jean Aurambault <aurambaj@users.noreply.github.com>
2020-03-19 09:49:25 -0700

    Implement DDLGenerator for Hibernate 5 and cleanup FK

--- webapp/src/main/java/com/box/l10n/mojito/entity/SlaIncident.java
+++ webapp/src/main/java/com/box/l10n/mojito/entity/SlaIncident.java
@@ -1,15 +1,17 @@
 package com.box.l10n.mojito.entity;
 
-import java.util.Set;
+import org.hibernate.annotations.Type;
+import org.joda.time.DateTime;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import org.hibernate.annotations.Type;
-import org.joda.time.DateTime;
+import java.util.Set;
 
 /**
  * @author jaurambault
@@ -22,11 +24,14 @@ public class SlaIncident extends AuditableEntity {
     @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDateTime")
     private DateTime closedDate;
 
+
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
-        name = "sla_incident_repositories", 
-        joinColumns = { @JoinColumn(name = "sla_incident_id") }, 
-        inverseJoinColumns = { @JoinColumn(name = "repository_id") }
+            name = "sla_incident_repositories",
+            joinColumns = {@JoinColumn(name = "sla_incident_id")},
+            inverseJoinColumns = {@JoinColumn(name = "repository_id")},
+            foreignKey = @ForeignKey(name = "FK__SLA_INCIDENT_REPOSITORIES__SLA_INCIDENT__ID"),
+            inverseForeignKey =  @ForeignKey(name = "FK__SLA_INCIDENT_REPOSITORIES__REPOSITORY__ID")
     )
     private Set<Repository> repositories;
 
@@ -47,3 +52,4 @@ public class SlaIncident extends AuditableEntity {
     }
 
 }
+
