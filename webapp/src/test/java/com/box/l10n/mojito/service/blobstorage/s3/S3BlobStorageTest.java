d720d28177922f194bbeca7be077ed658ffaf752
Jean Aurambault <aurambaj@users.noreply.github.com>
2020-03-20 08:59:06 -0700

    Fix more test
    - remove deprecated IntegrationTest, ect
    - spring.datasource.initialize=false, need to make sure that is not going to cause issues
    - Use TestWithEnableAutoConfiguration when needed

--- webapp/src/test/java/com/box/l10n/mojito/service/blobstorage/s3/S3BlobStorageTest.java
+++ webapp/src/test/java/com/box/l10n/mojito/service/blobstorage/s3/S3BlobStorageTest.java
@@ -11,18 +11,18 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = {
+@SpringBootTest(classes = {
         S3BlobStorageTest.class,
         S3BlobStorageConfigurationProperties.class,
         AmazonS3Configuration.class,
         AmazonS3ConfigurationProperties.class,
-        S3BlobStorageTest.TestConfig.class
+        S3BlobStorageTest.TestConfig.class,
 })
 @EnableConfigurationProperties
 public class S3BlobStorageTest implements BlobStorageTestShared {
--- webapp/src/test/java/com/box/l10n/mojito/service/branch/notification/slack/BranchNotificationMessageBuilderSlackTest.java
+++ webapp/src/test/java/com/box/l10n/mojito/service/branch/notification/slack/BranchNotificationMessageBuilderSlackTest.java
@@ -7,9 +7,6 @@ import com.box.l10n.mojito.utils.ServerConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -24,8 +21,7 @@ import static org.junit.Assert.assertEquals;
         BranchNotificationMessageBuilderSlack.class,
         BranchNotificationMessageBuilderSlackTest.class,
         BranchUrlBuilder.class,
-        ServerConfig.class},
-        properties = "spring.datasource.initialize=false")
+        ServerConfig.class})
 public class BranchNotificationMessageBuilderSlackTest {
 
     ObjectMapper objectMapper = new ObjectMapper();
