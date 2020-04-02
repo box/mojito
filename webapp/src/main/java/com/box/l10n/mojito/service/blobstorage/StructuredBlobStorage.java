c0ffa6a5d280becd29e1d94fc0946c9dab12cc07
Jean Aurambault <aurambaj@users.noreply.github.com>
2020-04-01 18:13:40 -0700

    Create a layer on top of BlobStorage to organize Mojito's data
    BlobStorage shouldn't be accessed directly instead this class should be used to make sure there is no naming conflicts

---
+++ webapp/src/main/java/com/box/l10n/mojito/service/blobstorage/StructuredBlobStorage.java
@@ -0,0 +1,35 @@
+package com.box.l10n.mojito.service.blobstorage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+/**
+ * Layer on top of {@link BlobStorage} to avoid naming clash between different Mojito services.
+ *
+ * Use this class instead of using directly {@link BlobStorage}
+ */
+@Component
+public class StructuredBlobStorage {
+
+    @Autowired
+    BlobStorage blobStorage;
+
+    public Optional<String> getString(Prefix prefix, String name) {
+        return blobStorage.getString(getFullName(prefix, name));
+    }
+
+    public void put(Prefix prefix, String name, String content, Retention retention) {
+        blobStorage.put(getFullName(prefix, name), content);
+    }
+
+    String getFullName(Prefix prefix, String name) {
+        return prefix.toString().toLowerCase() + "/" + name;
+    }
+
+    public enum Prefix {
+        POLLABLE_TASK,
+        IMAGE
+    }
+}
