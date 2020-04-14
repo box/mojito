47101d958f6708c8cc0ef2302cba1e9957f1f700
Jean Aurambault <aurambaj@users.noreply.github.com>
2020-04-14 11:02:45 -0700

    Add an option to Evolve command to write localized json course to file before sending them

--- cli/src/test/java/com/box/l10n/mojito/cli/command/EvolveCommandTest.java
+++ cli/src/test/java/com/box/l10n/mojito/cli/command/EvolveCommandTest.java
@@ -2,9 +2,10 @@ package com.box.l10n.mojito.cli.command;
 
 import com.box.l10n.mojito.cli.CLITestBase;
 import com.box.l10n.mojito.entity.Repository;
+import com.box.l10n.mojito.evolve.Course;
 import com.box.l10n.mojito.evolve.Evolve;
+import com.box.l10n.mojito.rest.entity.Locale;
 import org.junit.Assume;
-import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,15 +18,29 @@ public class EvolveCommandTest extends CLITestBase {
     @Autowired(required = false)
     Evolve evolve;
 
-    @Before
-    public void before() {
+    @Test
+    public void execute() throws Exception {
         Assume.assumeNotNull(evolve);
+        Repository repository = createTestRepoUsingRepoService();
+        getL10nJCommander().run("evolve-sync", "-r", repository.getName());
     }
 
     @Test
-    public void execute() throws Exception {
-        Repository repository = createTestRepoUsingRepoService();
-        getL10nJCommander().run("evolve-sync", "-r", repository.getName());
+    public void writeJsonTo() {
+        EvolveCommand evolveCommand = new EvolveCommand();
+        evolveCommand.writeJsonTo = getTargetTestDir().toString();
+
+        com.box.l10n.mojito.rest.entity.Repository repository = new com.box.l10n.mojito.rest.entity.Repository();
+        repository.setName("evolveRepository");
+
+        Course course = new Course();
+        course.setId("course1");
+
+        Locale locale = new Locale();
+        locale.setBcp47Tag("fr-FR");
+
+        evolveCommand.writeJsonToFile(repository, course, locale, "{\"key\" : \"value\"}");
+        checkExpectedGeneratedResources();
     }
 
 }
