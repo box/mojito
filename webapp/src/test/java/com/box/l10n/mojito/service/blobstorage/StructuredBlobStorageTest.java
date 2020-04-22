c18c0cbf20765e5b910e1003eaad0cbec57b7546
Jean Aurambault <aurambaj@users.noreply.github.com>
2020-04-22 16:06:42 -0700

    Fix rentention in StructuredBlobStorage

--- webapp/src/test/java/com/box/l10n/mojito/service/blobstorage/StructuredBlobStorageTest.java
+++ webapp/src/test/java/com/box/l10n/mojito/service/blobstorage/StructuredBlobStorageTest.java
@@ -1,20 +1,39 @@
 package com.box.l10n.mojito.service.blobstorage;
 
+import com.box.l10n.mojito.common.Mocks;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import static com.box.l10n.mojito.service.blobstorage.StructuredBlobStorage.Prefix.IMAGE;
+import static com.box.l10n.mojito.service.blobstorage.StructuredBlobStorage.Prefix.POLLABLE_TASK;
 import static org.junit.Assert.assertEquals;
 
+@RunWith(MockitoJUnitRunner.class)
 public class StructuredBlobStorageTest {
 
-    StructuredBlobStorage structuredBlobStorage = new StructuredBlobStorage();
+    @Mock
+    BlobStorage blobStorage;
+
+    @InjectMocks
+    StructuredBlobStorage structuredBlobStorage;
 
     @Test
     public void getPollableTask() {
-        assertEquals("pollable_task/test1", structuredBlobStorage.getFullName(StructuredBlobStorage.Prefix.POLLABLE_TASK, "test1"));
+        assertEquals("pollable_task/test1", structuredBlobStorage.getFullName(POLLABLE_TASK, "test1"));
     }
 
     @Test
     public void getImage() {
-        assertEquals("image/test1.jpg", structuredBlobStorage.getFullName(StructuredBlobStorage.Prefix.IMAGE, "test1.jpg"));
+        assertEquals("image/test1.jpg", structuredBlobStorage.getFullName(IMAGE, "test1.jpg"));
+    }
+
+    @Test
+    public void testRetention() {
+        structuredBlobStorage.put(POLLABLE_TASK, "testretention", "testrentention-content", Retention.MIN_1_DAY);
+        Mockito.verify(blobStorage).put("pollable_task/testretention", "testrentention-content", Retention.MIN_1_DAY);
     }
 }
