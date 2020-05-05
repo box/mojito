8c1347b78912adc8507e5a56860adb3dde83fe1d
Jean Aurambault <aurambaj@users.noreply.github.com>
2020-05-04 17:09:09 -0700

    move to 2.2.1
    2.2.x has aspectj issue

--- webapp/src/test/java/com/box/l10n/mojito/service/sla/SlaCheckerServiceTest.java
+++ webapp/src/test/java/com/box/l10n/mojito/service/sla/SlaCheckerServiceTest.java
@@ -13,7 +13,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.Spy;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 import java.util.Arrays;
