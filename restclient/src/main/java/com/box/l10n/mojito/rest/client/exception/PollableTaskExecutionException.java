9a24745d4a6dada69ea693cc6304a7202a56d9d1
Jean Aurambault <aurambaj@users.noreply.github.com>
2020-05-18 18:00:14 -0700

    Refactor error handling/display

--- restclient/src/main/java/com/box/l10n/mojito/rest/client/exception/PollableTaskExecutionException.java
+++ restclient/src/main/java/com/box/l10n/mojito/rest/client/exception/PollableTaskExecutionException.java
@@ -1,16 +1,11 @@
 package com.box.l10n.mojito.rest.client.exception;
 
-import com.box.l10n.mojito.rest.entity.ErrorMessage;
-
 /**
  * @author aloison
  */
 public class PollableTaskExecutionException extends PollableTaskException {
 
-    ErrorMessage errorMessage;
-
-    public PollableTaskExecutionException(ErrorMessage errorMessage) {
-        super(errorMessage.getMessage());
-        this.errorMessage = errorMessage;
+    public PollableTaskExecutionException(String message) {
+        super(message);
     }
 }
