e66d468f389bae16a91efa649c0d890b2c91b402
Jean Aurambault <aurambaj@users.noreply.github.com>
2020-05-21 22:24:41 -0700

    Migrate Flyway

--- webapp/src/main/java/com/box/l10n/mojito/FlyWayConfig.java
+++ webapp/src/main/java/com/box/l10n/mojito/FlyWayConfig.java
@@ -1,11 +1,19 @@
 package com.box.l10n.mojito;
 
-//import org.flywaydb.core.Flyway;
-
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.FlywayException;
+import org.flywaydb.core.api.configuration.FluentConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.util.Map;
 
 /**
  * Configure Flyway to provide an option to clean the database before starting
@@ -28,28 +36,74 @@ public class FlyWayConfig {
      */
     boolean clean = false;
 
-    //TODO(spring2)
-//    @Bean
-//    public FlywayMigrationStrategy cleanMigrateStrategy() {
-//
-//        FlywayMigrationStrategy strategy = new FlywayMigrationStrategy() {
-//
-//            @Override
-//            public void migrate(Flyway flyway) {
-//
-//                if (clean) {
-//                    logger.info("Clean DB with Flyway");
-//                    flyway.clean();
-//                } else {
-//                    logger.info("Don't clean DB with Flyway");
-//                }
-//
-//                flyway.migrate();
-//            }
-//        };
-//
-//        return strategy;
-//    }
+
+    @Autowired
+    DataSource dataSource;
+
+    @Bean
+    public FlywayMigrationStrategy cleanMigrateStrategy() {
+        return flyway -> {
+
+            if (clean) {
+                logger.info("Clean DB with Flyway");
+                flyway.clean();
+            } else {
+                logger.info("Don't clean DB with Flyway");
+            }
+
+            try {
+                logger.info("Flyway migrate() start");
+                flyway.migrate();
+                logger.info("Flyway migrate() finished");
+            } catch (FlywayException fe) {
+                tryToMigrateIfSpringMigration(flyway, fe);
+            }
+        };
+    }
+
+    void tryToMigrateIfSpringMigration(Flyway flyway, FlywayException fe) {
+        if (fe.getMessage().contains("without schema history")) {
+            logger.info("There is no schema history, we assume were migrating from Spring 1.x to 2.x");
+
+            String baselineVersion = getBaselineVersionFromOldFlywayTable();
+            logger.info("version: {}", baselineVersion);
+            if (baselineVersion == null) {
+                throw new RuntimeException("Can't read the version from the database, you must provide a Flyway baseline version to upgrade Mojito using spring.flyway.baseline-version");
+            }
+
+            flyway = rebuildFlywayInstanceWithBaselineFromOldTable(flyway, baselineVersion);
+
+            logger.info("Run baseline() with version: {}", flyway.getConfiguration().getBaselineVersion().getVersion());
+            flyway.baseline();
+
+            logger.info("Try Flyway migrate() again");
+            flyway.migrate();
+            logger.info("Flyway migrate() finished");
+        } else {
+            throw fe;
+        }
+    }
+
+    Flyway rebuildFlywayInstanceWithBaselineFromOldTable(Flyway flyway, String baselineVersion) {
+        FluentConfiguration fluentConfiguration = new FluentConfiguration();
+        fluentConfiguration.configuration(flyway.getConfiguration());
+        fluentConfiguration.baselineVersion(baselineVersion);
+        flyway = fluentConfiguration.load();
+        return flyway;
+    }
+
+    String getBaselineVersionFromOldFlywayTable() {
+        logger.info("Looking for a baseline version from the old Flyway version");
+        String baselineVersion = null;
+        try {
+            JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+            Map<String, Object> stringObjectMap = jdbcTemplate.queryForMap("select MAX(CAST(version as UNSIGNED)) as version from schema_version");
+            baselineVersion = String.valueOf(stringObjectMap.get("version"));
+        } catch (Exception e) {
+            logger.info("Couldn't get the old version");
+        }
+        return baselineVersion;
+    }
 
     public boolean isClean() {
         return clean;
