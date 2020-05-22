56b8ec61ab6682e7e32e7ab32ee795c6e3838e2a
Jean Aurambault <aurambaj@users.noreply.github.com>
2020-05-22 12:24:27 -0700

    Adjust Db/flyway config for unit tests

--- webapp/src/test/java/com/box/l10n/mojito/security/UserDetailsContextMapperImplTest.java
+++ webapp/src/test/java/com/box/l10n/mojito/security/UserDetailsContextMapperImplTest.java
@@ -1,7 +1,6 @@
 package com.box.l10n.mojito.security;
 
 import com.box.l10n.mojito.entity.security.user.User;
-import com.box.l10n.mojito.service.security.user.UserRepository;
 import com.box.l10n.mojito.service.security.user.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,10 +11,9 @@ import org.springframework.ldap.core.DirContextOperations;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.util.Assert;
 
-import static org.mockito.Mockito.anyObject;
 import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -32,15 +30,9 @@ public class UserDetailsContextMapperImplTest {
     @Mock
     UserService userService;
 
-    @Mock
-    UserRepository userRepository;
-
     @Test
     public void testMapUserFromContextWhenUserNameIsNotFound() throws Exception {
-        when(userRepository.findByUsername(anyString())).thenReturn(null);
-
-        when(userService.createOrUpdateBasicUser(anyObject(), anyString(), anyString(), anyString(),
-                anyString())).thenReturn(mock(User.class));
+        doReturn(mock(User.class)).when(userService).getOrCreateOrUpdateBasicUser("testUsername", "givename", "sn", "cn");
 
         DirContextOperations dirContextOperations = mock(DirContextOperations.class);
         when(dirContextOperations.getStringAttribute("givenname")).thenReturn("givename");
@@ -51,18 +43,5 @@ public class UserDetailsContextMapperImplTest {
 
         Assert.notNull(userDetails);
         verify(dirContextOperations, times(3)).getStringAttribute(anyString());
-    }
-
-    @Test
-    public void testMapUserFromContextWhenUserNameIsFound() throws Exception {
-        when(userRepository.findByUsername(anyString())).thenReturn(mock(User.class));
-
-        DirContextOperations dirContextOperations = mock(DirContextOperations.class);
-        UserDetails userDetails = userDetailsContextMapper.mapUserFromContext(dirContextOperations, "testUsername", null);
-
-        Assert.notNull(userDetails);
-        verify(dirContextOperations, never()).getStringAttribute(anyString());
-        verify(userService, never()).createOrUpdateBasicUser(anyObject(), anyString(),
-                        anyString(), anyString(), anyString());
     }
 }
