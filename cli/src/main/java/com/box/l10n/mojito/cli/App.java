54ea3255722b2a0241d48716f919b9004f013794
Jean Aurambault <aurambaj@users.noreply.github.com>
2020-05-29 16:44:33 -0700

    Use @SpringBootApplication for the CLI

--- cli/src/main/java/com/box/l10n/mojito/cli/App.java
+++ cli/src/main/java/com/box/l10n/mojito/cli/App.java
@@ -8,17 +8,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 
-@Configuration
-@EnableAutoConfiguration
 @EnableSpringConfigured
 @ComponentScan(basePackages = "com.box.l10n.mojito")
+@SpringBootApplication
 public class App implements CommandLineRunner {
 
     /**
