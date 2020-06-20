c221e3e4ef960de9b64f08059923e4fe1dbaa8e8
Patrick Connolly <patrick.c.connolly@gmail.com>
2020-06-20 01:31:25 -0300

    First pass for adding North American indigenous locales.

---
+++ webapp/src/main/resources/db/migration/V52__Add_Native_Locales.sql
@@ -0,0 +1,17 @@
+insert into locale (id, bcp47_tag) values (804, 'nv-003');
+insert into locale (id, bcp47_tag) values (805, 'oj-021');
+insert into locale (id, bcp47_tag) values (806, 'iu-021');
+insert into locale (id, bcp47_tag) values (807, 'cr-021');
+insert into locale (id, bcp47_tag) values (808, 'mus-021');
+insert into locale (id, bcp47_tag) values (809, 'dak-021');
+insert into locale (id, bcp47_tag) values (810, 'chr-021');
+insert into locale (id, bcp47_tag) values (811, 'ypk-021');
+
+insert into plural_form_for_locale (locale_id, plural_form_id) values (804, 0);
+insert into plural_form_for_locale (locale_id, plural_form_id) values (805, 0);
+insert into plural_form_for_locale (locale_id, plural_form_id) values (806, 0);
+insert into plural_form_for_locale (locale_id, plural_form_id) values (807, 0);
+insert into plural_form_for_locale (locale_id, plural_form_id) values (808, 0);
+insert into plural_form_for_locale (locale_id, plural_form_id) values (809, 0);
+insert into plural_form_for_locale (locale_id, plural_form_id) values (810, 0);
+insert into plural_form_for_locale (locale_id, plural_form_id) values (811, 0);
