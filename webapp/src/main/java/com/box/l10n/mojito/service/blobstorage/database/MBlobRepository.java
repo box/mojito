5d087631e536a279797d1677d1aa1f4c195a4193
Jean Aurambault <aurambaj@users.noreply.github.com>
2020-06-24 09:57:06 -0700

    Use batched delete to cleanup experied MBlobs
    - Use a find query to get the list of ids, since I can't find build in support for paginated delete and I don't want to write native queries.
    - Use more readable code for interval time.

--- webapp/src/main/java/com/box/l10n/mojito/service/blobstorage/database/MBlobRepository.java
+++ webapp/src/main/java/com/box/l10n/mojito/service/blobstorage/database/MBlobRepository.java
@@ -1,6 +1,7 @@
 package com.box.l10n.mojito.service.blobstorage.database;
 
 import com.box.l10n.mojito.entity.MBlob;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
@@ -8,6 +9,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * @author jaurambault
@@ -17,8 +20,14 @@ public interface MBlobRepository extends JpaRepository<MBlob, Long>, JpaSpecific
 
     MBlob findByName(@Param("name") String name);
 
+    @Query(
+            "select mb.id from #{#entityName} mb " +
+                    "where (unix_timestamp(mb.createdDate) + mb.expireAfterSeconds) < unix_timestamp()"
+    )
+    List<Long> findExpiredBlobIds(Pageable pageable);
+
     @Transactional
     @Modifying
-    @Query("delete from #{#entityName} mb where (unix_timestamp(mb.createdDate) + mb.expireAfterSeconds) < unix_timestamp() limit 100")
-    int deleteExpired();
+    @Query("delete from #{#entityName} mb where mb.id in ?1")
+    int deleteByIds(List<Long> ids);
 }
