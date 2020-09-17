86f7fb9e16a60edcbb433a8e5d6c15c375721d15
Jean Aurambault <aurambaj@users.noreply.github.com>
2020-09-17 15:57:00 -0700

    Retry component for DataIntegrityViolationException

---
+++ webapp/src/main/java/com/box/l10n/mojito/retry/DataIntegrityViolationExceptionRetryTemplate.java
@@ -0,0 +1,34 @@
+package com.box.l10n.mojito.retry;
+
+import com.google.common.collect.ImmutableMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.retry.RetryCallback;
+import org.springframework.retry.backoff.ExponentialRandomBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataIntegrityViolationExceptionRetryTemplate {
+
+    RetryTemplate retryTemplate;
+
+    public DataIntegrityViolationExceptionRetryTemplate() {
+        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy(5, ImmutableMap.of(DataIntegrityViolationException.class, true));
+
+        ExponentialRandomBackOffPolicy exponentialRandomBackOffPolicy = new ExponentialRandomBackOffPolicy();
+        exponentialRandomBackOffPolicy.setInitialInterval(10);
+        exponentialRandomBackOffPolicy.setMultiplier(3);
+        exponentialRandomBackOffPolicy.setMaxInterval(5000);
+
+        retryTemplate = new RetryTemplate();
+        retryTemplate.setRetryPolicy(retryPolicy);
+        retryTemplate.setBackOffPolicy(exponentialRandomBackOffPolicy);
+        retryTemplate.setThrowLastExceptionOnExhausted(true);
+    }
+
+    public <T, E extends Throwable> T execute(RetryCallback<T, E> retryCallback) throws E {
+        return retryTemplate.execute(retryCallback);
+    }
+}
