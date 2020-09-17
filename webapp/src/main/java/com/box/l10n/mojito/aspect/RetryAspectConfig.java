e86d8e94e8a744d0e126f20d54130fc1728e0631
Jean Aurambault <aurambaj@users.noreply.github.com>
2020-09-17 10:59:44 -0700

    Simple aspect to retry
    Note: @{@link org.springframework.retry.annotation.Retryable} doesn't seem to work with Aspectj CTW.
    This is over simplified to be really useful, would need more work and support similar to @Retryable. Keep it as a
    base for potential future work and as a reminder that @Retryable is not currently usable with AspectJ in this project.

---
+++ webapp/src/main/java/com/box/l10n/mojito/aspect/RetryAspectConfig.java
@@ -0,0 +1,17 @@
+package com.box.l10n.mojito.aspect;
+
+import org.aspectj.lang.Aspects;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author jaurambault
+ */
+@Configuration
+public class RetryAspectConfig {
+
+    @Bean
+    public RetryAspect getRetryAspect() {
+        return Aspects.aspectOf(RetryAspect.class);
+    }
+}
