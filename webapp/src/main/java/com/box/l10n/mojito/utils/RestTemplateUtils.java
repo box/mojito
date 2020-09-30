d7c325d7f9dcbd4540f25cdea3427e163fb72d3a
Jean Aurambault <aurambaj@users.noreply.github.com>
2020-09-30 16:35:38 -0700

    Import cleanup

--- webapp/src/main/java/com/box/l10n/mojito/utils/RestTemplateUtils.java
+++ webapp/src/main/java/com/box/l10n/mojito/utils/RestTemplateUtils.java
@@ -1,9 +1,7 @@
 package com.box.l10n.mojito.utils;
 
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 public class RestTemplateUtils {
