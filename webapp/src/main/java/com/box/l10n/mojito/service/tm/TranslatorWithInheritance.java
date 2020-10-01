3f6fe8ebe25b9a43abf9710e3b2cbf5187455f8d
Jean Aurambault <aurambaj@users.noreply.github.com>
2020-09-30 17:00:28 -0700

    Use TextUnitDTOsCacheService in TranslatorWithInheritance

--- webapp/src/main/java/com/box/l10n/mojito/service/tm/TranslatorWithInheritance.java
+++ webapp/src/main/java/com/box/l10n/mojito/service/tm/TranslatorWithInheritance.java
@@ -11,13 +11,16 @@ import com.box.l10n.mojito.service.tm.search.StatusFilter;
 import com.box.l10n.mojito.service.tm.search.TextUnitDTO;
 import com.box.l10n.mojito.service.tm.search.TextUnitSearcher;
 import com.box.l10n.mojito.service.tm.search.TextUnitSearcherParameters;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.box.l10n.mojito.service.tm.textunitdtocache.TextUnitDTOsCacheService;
+import com.box.l10n.mojito.service.tm.textunitdtocache.UpdateType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author jeanaurambault
@@ -45,18 +48,20 @@ public class TranslatorWithInheritance {
     @Autowired
     TextUnitUtils textUnitUtils;
 
+    @Autowired
+    TextUnitDTOsCacheService textUnitDTOsCacheService;
+
     Asset asset;
 
     InheritanceMode inheritanceMode;
 
     RepositoryLocale repositoryLocale;
-
-    private StatusFilter statusFilter;
-
     /**
      * Cache that contains the translations required to translate the asset.
      */
     Map<Long, Map<String, TextUnitDTO>> localeToTextUnitDTOsForLocaleMap = new HashMap<>();
+
+    private StatusFilter statusFilter;
 
     public TranslatorWithInheritance(Asset asset, RepositoryLocale repositoryLocale, InheritanceMode inheritanceMode) {
         this(asset, repositoryLocale, inheritanceMode, StatusFilter.TRANSLATED_AND_NOT_REJECTED);
@@ -104,7 +109,7 @@ public class TranslatorWithInheritance {
 
         return textUnitDTO;
     }
-    
+
     public boolean hasTranslationWithoutInheritance() {
         return !getTextUnitDTOsForLocaleMapFromCache(repositoryLocale.getLocale().getId()).isEmpty();
     }
@@ -118,7 +123,7 @@ public class TranslatorWithInheritance {
      * for optimization purpose, the translations fetched would be the same as
      * the source, hence returning the source directly is more efficient.
      *
-     * @param md5 MD5 to lookup the translation
+     * @param md5    MD5 to lookup the translation
      * @param source the source to fallback to if there is no translation
      * @return the translation from a parent locale if it exists else null
      */
@@ -151,8 +156,8 @@ public class TranslatorWithInheritance {
      * Gets a {@link TextUnitDTO} from the cache for a given locale and text
      * unit MD5.
      *
-     * @param md5 the MD5 of the text unit (see
-     * {@link TMService#computeTMTextUnitMD5(java.lang.String, java.lang.String, java.lang.String)})
+     * @param md5      the MD5 of the text unit (see
+     *                 {@link TMService#computeTMTextUnitMD5(java.lang.String, java.lang.String, java.lang.String)})
      * @param localeId the {@link Locale#id}
      * @return a {@link TextUnitDTO} or {@code null} if no translation is
      * available
@@ -192,25 +197,6 @@ public class TranslatorWithInheritance {
      * current translations for a given locale
      */
     private Map<String, TextUnitDTO> getTextUnitDTOsForLocaleByMD5(Long localeId) {
-
-        Map<String, TextUnitDTO> res = new HashMap<>();
-
-        logger.debug("Prepare TextUnitSearcherParameters to fetch translation for locale: {}", localeId);
-        TextUnitSearcherParameters textUnitSearcherParameters = new TextUnitSearcherParameters();
-        textUnitSearcherParameters.setLocaleId(localeId);
-        textUnitSearcherParameters.setAssetId(asset.getId());
-        textUnitSearcherParameters.setStatusFilter(statusFilter);
-
-        logger.debug("Getting TextUnitDTOs");
-        List<TextUnitDTO> textUnitDTOs = textUnitSearcher.search(textUnitSearcherParameters);
-
-        logger.debug("Transform TextUnitDTOs list (size={}) into map keyed by MD5", textUnitDTOs.size());
-        for (TextUnitDTO textUnitDTO : textUnitDTOs) {
-            String md5 = textUnitUtils.computeTextUnitMD5(textUnitDTO.getName(), textUnitDTO.getSource(), textUnitDTO.getComment());
-            res.put(md5, textUnitDTO);
-        }
-
-        return res;
+        return textUnitDTOsCacheService.getTextUnitDTOsForAssetAndLocaleByMD5(asset.getId(), localeId, statusFilter, false, UpdateType.ALWAYS);
     }
-
 }
