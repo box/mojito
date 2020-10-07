55c33003d1287f5837a78453b3d3281e25b63e0f
Jean Aurambault <aurambaj@users.noreply.github.com>
2020-10-07 11:16:51 -0700

    Add Immutable style for Jpa DTOs

---
+++ common/src/main/java/com/box/l10n/mojito/immutables/JpaDTO.java
@@ -0,0 +1,17 @@
+package com.box.l10n.mojito.immutables;
+
+import org.immutables.value.Value;
+
+@Value.Style(
+        allParameters = true,
+        of = "new",
+        defaults = @Value.Immutable(builder = false),
+        // Avoid annoying auto-complete and useless copies when working with Immutable collection
+        // Could be nice to have for test syntax
+        builtinContainerAttributes = false,
+        // No prefix or suffix for generated immutable type
+        typeImmutable = "*"
+)
+public @interface JpaDTO {
+}
+
