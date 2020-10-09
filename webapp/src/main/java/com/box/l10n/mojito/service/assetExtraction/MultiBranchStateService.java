7f556bc8c26b5c456560e3cad90ce9d58fcad1ec
Jean Aurambault <aurambaj@users.noreply.github.com>
2020-10-09 16:19:24 -0700

    Add MultiBranchStateService to re-create MultiBranchState from DB

---
+++ webapp/src/main/java/com/box/l10n/mojito/service/assetExtraction/MultiBranchStateService.java
@@ -0,0 +1,137 @@
+package com.box.l10n.mojito.service.assetExtraction;
+
+import com.box.l10n.mojito.entity.AssetTextUnit;
+import com.box.l10n.mojito.localtm.merger.Branch;
+import com.box.l10n.mojito.localtm.merger.BranchData;
+import com.box.l10n.mojito.localtm.merger.BranchStateTextUnit;
+import com.box.l10n.mojito.localtm.merger.MultiBranchState;
+import com.box.l10n.mojito.okapi.TextUnitUtils;
+import com.box.l10n.mojito.service.assetTextUnit.AssetTextUnitRepository;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class MultiBranchStateService {
+
+    /**
+     * logger
+     */
+    static Logger logger = LoggerFactory.getLogger(MultiBranchStateService.class);
+
+    MultiBranchStateBlobStorage multiBranchStateBlobStorage;
+
+    AssetTextUnitRepository assetTextUnitRepository;
+
+    AssetTextUnitToTMTextUnitRepository assetTextUnitToTMTextUnitRepository;
+
+    TextUnitUtils textUnitUtils;
+
+    LocalBranchToEntityBranchConverter localBranchToEntityBranchConverter;
+
+    public MultiBranchStateService(MultiBranchStateBlobStorage multiBranchStateBlobStorage,
+                                   AssetTextUnitRepository assetTextUnitRepository,
+                                   AssetTextUnitToTMTextUnitRepository assetTextUnitToTMTextUnitRepository,
+                                   TextUnitUtils textUnitUtils,
+                                   LocalBranchToEntityBranchConverter localBranchToEntityBranchConverter) {
+        this.multiBranchStateBlobStorage = Preconditions.checkNotNull(multiBranchStateBlobStorage);
+        this.assetTextUnitRepository = Preconditions.checkNotNull(assetTextUnitRepository);
+        this.assetTextUnitToTMTextUnitRepository = Preconditions.checkNotNull(assetTextUnitToTMTextUnitRepository);
+        this.textUnitUtils = Preconditions.checkNotNull(textUnitUtils);
+        this.localBranchToEntityBranchConverter = Preconditions.checkNotNull(localBranchToEntityBranchConverter);
+    }
+
+    public MultiBranchState getMultiBranchStateForAssetExtractionId(long assetExtractionId, long version) {
+        return multiBranchStateBlobStorage.getMultiBranchStateForAssetExtractionId(assetExtractionId, version)
+                .orElseGet(() -> {
+                    logger.debug("No MultiBranchState for asset extraction id: {}", assetExtractionId);
+                    return getAndSaveInitialMultiBranchStateFromDatabase(assetExtractionId, version);
+                });
+    }
+
+    public void deleteMultiBranchStateForAssetExtractionId(long assetExtractionId, long version) {
+        multiBranchStateBlobStorage.deleteMultiBranchStateForAssetExtractionId(assetExtractionId, version);
+    }
+
+    public void putMultiBranchStateForAssetExtractionId(MultiBranchState multiBranchState, long assetExtractionId, long version) {
+        multiBranchStateBlobStorage.putMultiBranchStateForAssetExtractionId(multiBranchState, assetExtractionId, version);
+    }
+
+    MultiBranchState getAndSaveInitialMultiBranchStateFromDatabase(long assetExtractionId, long version) {
+        MultiBranchState multiBranchStateForAssetExtractionId = getInitialMultiBranchStateFromDatabase(assetExtractionId, version);
+        logger.debug("Save the initial state for asset extraction id: {}", assetExtractionId);
+        multiBranchStateBlobStorage.putMultiBranchStateForAssetExtractionId(multiBranchStateForAssetExtractionId, assetExtractionId, 0);
+        return multiBranchStateForAssetExtractionId;
+    }
+
+    /**
+     * Build an initial MultiBranchState for an asset extraction from the database.
+     * <p>
+     * This is needed to migrate from the old extraction logic to the new one.
+     * <p>
+     * When working on new repositories (or assets) the data returned by the database will be empty, and the MultiBranchState
+     * will also be empty, so it should have minor impact later on the new implementation. In other words, if not
+     * a migrated repository, this code is not needed.
+     *
+     * @param assetExtractionId
+     * @param version
+     * @return
+     */
+    MultiBranchState getInitialMultiBranchStateFromDatabase(long assetExtractionId, long version) {
+
+        logger.debug("Get initial MultiBranchState for asset extraction id: {}", assetExtractionId);
+        List<AssetTextUnit> assetTextUnits = assetTextUnitRepository.findByAssetExtractionId(assetExtractionId);
+
+        ImmutableList<BranchStateTextUnit> branchStateTextUnits = assetTextUnits.stream()
+                .map(assetTextUnit -> {
+                    BranchData branchData = BranchData.of()
+                            .withUsages(assetTextUnit.getUsages() == null ?
+                                    ImmutableSet.of() :
+                                    ImmutableSet.copyOf(assetTextUnit.getUsages()));
+
+                    String branchName = localBranchToEntityBranchConverter.branchEntityToLocalBranchName(assetTextUnit.getBranch());
+
+                    ImmutableMap<String, BranchData> branchNameToBranchDatas = ImmutableMap.of(branchName, branchData);
+
+                    BranchStateTextUnit.Builder builder = BranchStateTextUnit.builder();
+
+                    // TODO(perf) should we batch the lookup: assetTextUnitToTMTextUnitRepository???
+                    builder
+                            .tmTextUnitId(assetTextUnitToTMTextUnitRepository.findTmTextUnitId(assetExtractionId, assetTextUnit.getId()).orElse(null))
+                            .assetTextUnitId(assetTextUnit.getId())
+                            .source(assetTextUnit.getContent())
+                            .name(assetTextUnit.getName())
+                            .comments(assetTextUnit.getComment())
+                            .createdDate(assetTextUnit.getCreatedDate())
+                            .branchNameToBranchDatas(branchNameToBranchDatas)
+                            .md5(textUnitUtils.computeTextUnitMD5(
+                                    assetTextUnit.getName(),
+                                    assetTextUnit.getContent(),
+                                    assetTextUnit.getComment()));
+
+                    if (assetTextUnit.getPluralForm() != null) {
+                        builder.pluralForm(assetTextUnit.getPluralForm().getName())
+                                .pluralFormOther(assetTextUnit.getPluralFormOther());
+                    }
+
+                    return builder.build();
+                })
+                .collect(ImmutableList.toImmutableList());
+
+        ImmutableSet<Branch> branches = assetTextUnits.stream()
+                .map(AssetTextUnit::getBranch)
+                .map(b -> localBranchToEntityBranchConverter.convertEntityBranchToLocaleBranch(b))
+                .collect(ImmutableSet.toImmutableSet());
+
+        return MultiBranchState.of()
+                .withBranchStateTextUnits(branchStateTextUnits)
+                .withBranches(branches);
+    }
+
+}
---
+++ webapp/src/test/java/com/box/l10n/mojito/service/assetExtraction/MultiBranchStateServiceTest.java
@@ -0,0 +1,117 @@
+package com.box.l10n.mojito.service.assetExtraction;
+
+import com.box.l10n.mojito.entity.AssetTextUnit;
+import com.box.l10n.mojito.localtm.merger.Branch;
+import com.box.l10n.mojito.localtm.merger.BranchData;
+import com.box.l10n.mojito.localtm.merger.BranchStateTextUnit;
+import com.box.l10n.mojito.localtm.merger.MultiBranchState;
+import com.box.l10n.mojito.service.DBUtils;
+import com.box.l10n.mojito.service.tm.TMTestData;
+import com.box.l10n.mojito.test.TestIdWatcher;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import org.assertj.core.api.Assertions;
+import org.joda.time.DateTime;
+import org.junit.Rule;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+import static com.box.l10n.mojito.service.assetExtraction.LocalBranchToEntityBranchConverter.NULL_BRANCH_DATE_PLACEHODLER;
+import static com.box.l10n.mojito.service.assetExtraction.LocalBranchToEntityBranchConverter.NULL_BRANCH_TEXT_PLACEHOLDER;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class MultiBranchStateServiceTest extends ServiceTestBase {
+
+    @Rule
+    public TestIdWatcher testIdWatcher = new TestIdWatcher();
+
+    @Autowired
+    MultiBranchStateService multiBranchStateService;
+
+    @Autowired
+    MultiBranchStateBlobStorage multiBranchStateBlobStorage;
+
+    @Autowired
+    DBUtils dbUtils;
+
+    @Test
+    public void getMultiBranchStateForAssetExtractionId() throws InterruptedException {
+        TMTestData tmTestData = new TMTestData(testIdWatcher);
+
+        Optional<MultiBranchState> multiBranchStateForAssetExtractionBefore = multiBranchStateBlobStorage.getMultiBranchStateForAssetExtractionId(tmTestData.assetExtraction.getId(), 0L);
+        assertFalse(multiBranchStateForAssetExtractionBefore.isPresent());
+
+        MultiBranchState multiBranchStateForAssetExtractionId = multiBranchStateService.getMultiBranchStateForAssetExtractionId(tmTestData.assetExtraction.getId(), 0L);
+
+        MultiBranchState expectedMultiBranchState = MultiBranchState.of()
+                .withBranches(ImmutableSet.of(Branch.builder()
+                        .name(NULL_BRANCH_TEXT_PLACEHOLDER)
+                        .createdAt(NULL_BRANCH_DATE_PLACEHODLER)
+                        .build()
+                ))
+                .withBranchStateTextUnits(
+                        ImmutableList.of(
+                                convertTmTextUnitToBranchStateTextUnit(tmTestData.createAssetTextUnit1, tmTestData.addTMTextUnit1.getId()),
+                                convertTmTextUnitToBranchStateTextUnit(tmTestData.createAssetTextUnit2, tmTestData.addTMTextUnit2.getId())
+                        ));
+        expectedMultiBranchState = roundDateTimesIfMysql(expectedMultiBranchState);
+
+        Assertions.assertThat(multiBranchStateForAssetExtractionId).usingRecursiveComparison().isEqualTo(expectedMultiBranchState);
+
+        Optional<MultiBranchState> multiBranchStateForAssetExtractionAfter = multiBranchStateBlobStorage.getMultiBranchStateForAssetExtractionId(tmTestData.assetExtraction.getId(), 0L);
+        assertTrue(multiBranchStateForAssetExtractionAfter.isPresent());
+    }
+
+    /**
+     * Round date times, to make the test work with Mysql.
+     * <p>
+     * On current Mysql schema as precision of second for dates, the test fails with date precision issue without rounding here
+     * - ignore for now
+     * - update the schema to have proper precision later
+     *
+     * @param expectedMultiBranchState
+     * @return
+     */
+    MultiBranchState roundDateTimesIfMysql(MultiBranchState expectedMultiBranchState) {
+
+        if (dbUtils.isMysql()) {
+            expectedMultiBranchState = expectedMultiBranchState.withBranchStateTextUnits(expectedMultiBranchState.getBranchStateTextUnits().stream()
+                    .map(bstu -> bstu.withCreatedDate(roundDateTimeToSecond(bstu.getCreatedDate())))
+                    .collect(ImmutableList.toImmutableList()));
+
+            expectedMultiBranchState = expectedMultiBranchState.withBranches(expectedMultiBranchState.getBranches().stream()
+                    .map(b -> b.withCreatedAt(roundDateTimeToSecond(b.getCreatedAt())))
+                    .collect(ImmutableSet.toImmutableSet()));
+
+        }
+        return expectedMultiBranchState;
+    }
+
+    DateTime roundDateTimeToSecond(DateTime dateTime) {
+
+        if (dateTime.getMillisOfSecond() > 500) {
+            dateTime = dateTime.withMillisOfSecond(0).plusSeconds(1);
+        } else {
+            dateTime = dateTime.withMillisOfSecond(0);
+        }
+
+        return dateTime;
+    }
+
+    BranchStateTextUnit convertTmTextUnitToBranchStateTextUnit(AssetTextUnit assetTextUnit, Long tmTextUnitId) {
+        return BranchStateTextUnit.builder()
+                .tmTextUnitId(tmTextUnitId)
+                .assetTextUnitId(assetTextUnit.getId())
+                .name(assetTextUnit.getName())
+                .md5(assetTextUnit.getMd5())
+                .source(assetTextUnit.getContent())
+                .comments(assetTextUnit.getComment())
+                .createdDate(assetTextUnit.getCreatedDate())
+                .branchNameToBranchDatas(ImmutableMap.of(NULL_BRANCH_TEXT_PLACEHOLDER, BranchData.of()))
+                .build();
+    }
+}
