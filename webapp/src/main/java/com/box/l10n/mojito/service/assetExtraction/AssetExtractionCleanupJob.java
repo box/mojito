8fe1615e2d66459d6dbb1dfc8af89092c3a45f6e
Jean Aurambault <aurambaj@users.noreply.github.com>
2020-10-15 16:48:14 -0700

    Make the AssetExtractionCleanupJob.java run only if asked in the config
    That job won't be need anymore with the asset extraction implementation. We're not removing it because it can be used during rollout with multiple server to keep cleaning up while the 2 implementations are running in parallel. It is opt-in with "l10n.asset-extraction.cleanup-job.enabled=true"

--- webapp/src/main/java/com/box/l10n/mojito/service/assetExtraction/AssetExtractionCleanupJob.java
+++ webapp/src/main/java/com/box/l10n/mojito/service/assetExtraction/AssetExtractionCleanupJob.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -26,6 +27,7 @@ import java.time.Duration;
 @Configuration
 @Component
 @DisallowConcurrentExecution
+@ConditionalOnProperty(value = "l10n.asset-extraction.cleanup-job.enabled", havingValue = "true")
 public class AssetExtractionCleanupJob implements Job {
 
     /**
