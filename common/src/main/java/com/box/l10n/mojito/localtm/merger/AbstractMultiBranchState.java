c94ac00da66bd06567220b7e11adaf425ff028d6
Jean Aurambault <aurambaj@users.noreply.github.com>
2020-11-04 20:57:54 -0800

    Models and classes to merge a simple local TM with branch support

---
+++ common/src/main/java/com/box/l10n/mojito/localtm/merger/AbstractMultiBranchState.java
@@ -0,0 +1,22 @@
+package com.box.l10n.mojito.localtm.merger;
+
+import com.box.l10n.mojito.immutables.NoPrefixNoBuiltinContainer;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import org.immutables.value.Value;
+
+@Value.Immutable(singleton = true)
+@NoPrefixNoBuiltinContainer
+public abstract class AbstractMultiBranchState {
+
+    @Value.Default
+    public ImmutableList<BranchStateTextUnit> getBranchStateTextUnits() {
+        return ImmutableList.of();
+    }
+
+    @Value.Default
+    public ImmutableSet<Branch> getBranches() {
+        return ImmutableSet.of();
+    }
+}
