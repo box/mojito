eb462023e0279022f8823c3d33e1b3e2288bdb6d
Mark Allen <3417310+maallen@users.noreply.github.com>
2021-09-14 11:33:27 +0100

    Added PrintfLikeIgnorePercentageAfterBracketsIntegrityChecker and PrintfLikeVariableTypeIntegrityChecker

---
+++ webapp/src/main/java/com/box/l10n/mojito/service/assetintegritychecker/integritychecker/PrintfLikeVariableTypeIntegrityCheckerException.java
@@ -0,0 +1,8 @@
+package com.box.l10n.mojito.service.assetintegritychecker.integritychecker;
+
+public class PrintfLikeVariableTypeIntegrityCheckerException extends IntegrityCheckException {
+
+    public PrintfLikeVariableTypeIntegrityCheckerException(String message) {
+        super(message);
+    }
+}
