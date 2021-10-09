9c72d2454d76af8fc87f703b5fff3946408ff2ee
Jean Aurambault <aurambaj@users.noreply.github.com>
2021-10-09 00:22:35 -0700

    Define strategies to remove untranslated text units
    Either drop the text unit event for filter that support that eg. properties or use a placeholder that will be removed by post processing of the document output.

---
+++ common/src/main/java/com/box/l10n/mojito/okapi/filters/RemoveUntranslatedStategyAnnotation.java
@@ -0,0 +1,27 @@
+package com.box.l10n.mojito.okapi.filters;
+
+import com.google.common.base.Preconditions;
+import net.sf.okapi.common.annotation.IAnnotation;
+
+/**
+ * Holds the strategy to remove untranslated text units.
+ *
+ * To be set by the filter depending on their implementation.
+ *
+ * Some filter may work by dropping the whole text unit (eg. properties) while other may generate a corrupted output
+ * (JSON filter)
+ *
+ * @author jaurambault
+ */
+public class RemoveUntranslatedStategyAnnotation implements IAnnotation {
+
+    RemoveUntranslatedStrategy removeUntranslatedStrategy;
+
+    public RemoveUntranslatedStategyAnnotation(RemoveUntranslatedStrategy removeUntranslatedStrategy) {
+        this.removeUntranslatedStrategy = Preconditions.checkNotNull(removeUntranslatedStrategy);
+    }
+
+    public RemoveUntranslatedStrategy getUntranslatedStrategy() {
+        return removeUntranslatedStrategy;
+    }
+}
