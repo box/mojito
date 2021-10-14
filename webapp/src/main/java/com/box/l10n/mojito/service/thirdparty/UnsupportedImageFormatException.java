d13a4541da502a6b15cefaac535b5180d8eef08f
Mark Allen <3417310+maallen@users.noreply.github.com>
2021-10-14 13:32:09 +0100

    Restrict image upload to specific formats

---
+++ webapp/src/main/java/com/box/l10n/mojito/service/thirdparty/UnsupportedImageFormatException.java
@@ -0,0 +1,8 @@
+package com.box.l10n.mojito.service.thirdparty;
+
+public class UnsupportedImageFormatException extends RuntimeException {
+
+    public UnsupportedImageFormatException(String message) {
+        super(message);
+    }
+}
