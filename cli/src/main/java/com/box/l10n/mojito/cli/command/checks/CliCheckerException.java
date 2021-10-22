bca7457eed63127f5d7e9819fc162ac13c751f5f
Mark Allen <3417310+maallen@users.noreply.github.com>
2021-10-22 16:35:19 +0100

    Deleted unused exception class

--- cli/src/main/java/com/box/l10n/mojito/cli/command/checks/CliCheckerException.java
+++
@@ -1,8 +0,0 @@
-package com.box.l10n.mojito.cli.command.checks;
-
-public class CliCheckerException extends RuntimeException {
-
-    public CliCheckerException(String message, Exception e) {
-        super(message, e);
-    }
-}
