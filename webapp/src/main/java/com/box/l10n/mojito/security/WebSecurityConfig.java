13deea463f93d7ae5efac12e28e3aeff338ac017
Jean Aurambault <aurambaj@users.noreply.github.com>
2021-12-08 17:09:02 -0800

    Add option to disable CSRF
     To make testing easier on localhost, add an option: l10n.security.csrf-disable to disable CSRF. (may have been present in springboot 1.5 but is not 2.x)

--- webapp/src/main/java/com/box/l10n/mojito/security/WebSecurityConfig.java
+++ webapp/src/main/java/com/box/l10n/mojito/security/WebSecurityConfig.java
@@ -148,8 +148,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // https://docs.spring.io/spring-security/site/docs/current/reference/html5/#headers-cache-control
         http.headers().cacheControl().disable();
 
-        // no csrf on rotation end point - they are accessible only locally
-        http.csrf().ignoringAntMatchers("/actuator/shutdown", "/actuator/loggers/**", "/api/rotation");
+        if (securityConfig.isCsrfDisable()) {
+            http.csrf().disable();
+        } else {
+            // no csrf on rotation end point - they are accessible only locally
+            http.csrf().ignoringAntMatchers("/actuator/shutdown", "/actuator/loggers/**", "/api/rotation");
+        }
 
         // matcher order matters - "everything else" mapping must be last
         http.authorizeRequests(authorizeRequests -> authorizeRequests.
