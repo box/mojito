b4a5d202426d66bf1d38095fe949403c5d9fa563
loliveiramontedonio <loliveiramontedonio@pinterest.com>
2022-05-13 14:36:34 -0300

    metricRegistry.send thirdPartySync

--- webapp/src/main/java/com/box/l10n/mojito/rest/thirdparty/ThirdPartyWS.java
+++ webapp/src/main/java/com/box/l10n/mojito/rest/thirdparty/ThirdPartyWS.java
@@ -2,6 +2,8 @@ package com.box.l10n.mojito.rest.thirdparty;
 
 import com.box.l10n.mojito.entity.PollableTask;
 import com.box.l10n.mojito.service.thirdparty.ThirdPartyService;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tags;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,9 +23,15 @@ public class ThirdPartyWS {
     @Autowired
     ThirdPartyService thirdPartyService;
 
+    @Autowired
+    MeterRegistry meterRegistry;
+
     @RequestMapping(value = "/api/thirdparty/sync", method = RequestMethod.POST)
     public PollableTask sync(@RequestBody ThirdPartySync thirdPartySync) {
         logger.debug("Sync repository: {}", thirdPartySync);
+
+        meterRegistry.counter("thirdPartyWS.sync", Tags.of("repositoryId", thirdPartySync.repositoryId.toString())).increment();
+
         return thirdPartyService.asyncSyncMojitoWithThirdPartyTMS(thirdPartySync)
                 .getPollableTask();
     }
