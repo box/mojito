5f3cb2e167346df272301eba891b38f3bb7db088
loliveiramontedonio <loliveiramontedonio@pinterest.com>
2022-06-01 08:21:43 -0300

    fix migration

--- webapp/src/main/resources/db/migration/V59__Alter_text_unit_usages.sql
+++ webapp/src/main/resources/db/migration/V59__Alter_text_unit_usages.sql
@@ -1,1 +1,1 @@
-ALTER TABLE mojitodb.asset_text_unit_usages MODIFY COLUMN usages varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL;
+ALTER TABLE asset_text_unit_usages MODIFY COLUMN usages varchar(512) NULL;
