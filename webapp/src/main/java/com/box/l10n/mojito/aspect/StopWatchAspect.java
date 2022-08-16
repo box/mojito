7261052aaf7342894ba8cb3f49b6d5b30784bc62
Jean Aurambault <aurambaj@users.noreply.github.com>
2022-08-15 21:10:55 -0700

    Make logging level configurable in @StopWatch

--- webapp/src/main/java/com/box/l10n/mojito/aspect/StopWatchAspect.java
+++ webapp/src/main/java/com/box/l10n/mojito/aspect/StopWatchAspect.java
@@ -1,12 +1,17 @@
 package com.box.l10n.mojito.aspect;
 
+import com.box.l10n.mojito.service.pollableTask.Pollable;
 import com.google.common.base.Stopwatch;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
+
+import java.lang.reflect.Method;
 
 /**
  * Simple aspect to log time spent in a function.
@@ -30,9 +35,36 @@ public class StopWatchAspect {
 
         stopwatch.stop();
        
-        logger.debug("{}#{} took: {}", pjp.getSignature().getDeclaringTypeName(), pjp.getSignature().getName(), stopwatch.toString());
-        
+        log(getLevel(pjp), "{}#{} took: {}", pjp.getSignature().getDeclaringTypeName(), pjp.getSignature().getName(), stopwatch.toString());
+
         return res;
+    }
+
+    private Level getLevel(ProceedingJoinPoint pjp) {
+        MethodSignature ms = (MethodSignature) pjp.getSignature();
+        Method m = ms.getMethod();
+        StopWatch annotation = m.getAnnotation(StopWatch.class);
+        return annotation.level();
+    }
+
+    private void log(Level level, String format, Object... args) {
+        switch (level) {
+            case TRACE:
+                logger.trace(format, args);
+                break;
+            case DEBUG:
+                logger.debug(format, args);
+                break;
+            case INFO:
+                logger.info(format, args);
+                break;
+            case WARN:
+                logger.warn(format, args);
+                break;
+            case ERROR:
+                logger.error(format, args);
+                break;
+        }
     }
     
     @Pointcut("execution(@com.box.l10n.mojito.aspect.StopWatch * *(..))")
