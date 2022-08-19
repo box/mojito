ef01f6b5b65964b32d7aa788af8381d16427cfe9
Jean Aurambault <aurambaj@users.noreply.github.com>
2022-08-19 14:14:54 -0700

    Refactor PullRunService and DeltaServiceTest to use optimized pull run recording

--- webapp/src/test/java/com/box/l10n/mojito/service/pullrun/PullRunServiceTest.java
+++ webapp/src/test/java/com/box/l10n/mojito/service/pullrun/PullRunServiceTest.java
@@ -8,6 +8,7 @@ import com.box.l10n.mojito.entity.TMTextUnitVariant;
 import com.box.l10n.mojito.service.asset.AssetService;
 import com.box.l10n.mojito.service.assetExtraction.ServiceTestBase;
 import com.box.l10n.mojito.service.repository.RepositoryNameAlreadyUsedException;
+import com.box.l10n.mojito.service.repository.RepositoryService;
 import com.box.l10n.mojito.service.tm.TMTestData;
 import com.box.l10n.mojito.test.TestIdWatcher;
 import org.junit.Assert;
@@ -30,109 +31,20 @@ public class PullRunServiceTest extends ServiceTestBase {
     public TestIdWatcher testIdWatcher = new TestIdWatcher();
 
     @Autowired
-    AssetService assetService;
-
-    @Autowired
     PullRunService pullRunService;
 
     @Autowired
-    PullRunAssetRepository pullRunAssetRepository;
-
-    @Autowired
-    PullRunAssetService pullRunAssetService;
-
-    @Autowired
-    PullRunTextUnitVariantRepository pullRunTextUnitVariantRepository;
-
-    @Autowired
-    PullRunTextUnitVariantService pullRunTextUnitVariantService;
-
-    TMTestData tmTestData;
-
-    Repository repository;
-
-    Asset asset;
-
-    @Before
-    public void before() throws RepositoryNameAlreadyUsedException {
-        if (tmTestData == null) {
-            tmTestData = new TMTestData(testIdWatcher);
-            asset = tmTestData.asset;
-            repository = tmTestData.repository;
-        }
-    }
+    RepositoryService repositoryService;
 
     @Test
-    public void testCreatePullRun() {
+    public void testGetOrCreate() throws RepositoryNameAlreadyUsedException {
+        Repository repository = repositoryService.createRepository(testIdWatcher.getEntityName("repository"));
+
         String pullRunName = "testCreatePullRun";
 
-        PullRun pullRun = pullRunService.createPullRun(repository, pullRunName);
+        PullRun pullRun = pullRunService.getOrCreate(pullRunName, repository);
 
         Assert.assertNotNull(pullRun);
         Assert.assertEquals(pullRunName, pullRun.getName());
-    }
-
-    @Test
-    public void testClearPullRunLinkedData() {
-        PullRun pullRun = pullRunService.createPullRun(repository, "testClearPullRunLinkedData");
-
-        PullRunAsset pullRunAsset = pullRunAssetService.createPullRunAsset(pullRun, asset);
-        Assert.assertFalse(pullRunAssetRepository.findByPullRun(pullRun).isEmpty());
-
-        pullRunTextUnitVariantService.createPullRunTextUnitVariant(pullRunAsset,
-                                                                   tmTestData.addCurrentTMTextUnitVariant1FrFR);
-        Assert.assertFalse(pullRunTextUnitVariantRepository.findByPullRunAsset(pullRunAsset, PageRequest.of(0,
-                                                                                                            Integer.MAX_VALUE))
-                                   .isEmpty());
-
-        pullRunService.clearPullRunLinkedData(pullRun);
-        Assert.assertTrue(pullRunTextUnitVariantRepository.findByPullRunAsset(pullRunAsset, PageRequest.of(0,
-                                                                                                           Integer.MAX_VALUE))
-                                  .isEmpty());
-    }
-
-    @Test
-    public void testAssociatePullRunToTextUnitVariants() {
-        PullRun pullRun = pullRunService.createPullRun(repository, "associatePullRunToTextUnitVariants");
-
-        pullRunService.associatePullRunToTextUnitVariants(pullRun, asset,
-                                                          Arrays.asList(tmTestData.addCurrentTMTextUnitVariant1FrFR,
-                                                                        tmTestData.addCurrentTMTextUnitVariant2FrCA));
-
-        List<TMTextUnitVariant> tmTextUnitVariants = pullRunService.getTextUnitVariants(pullRun, PageRequest.of(0,
-                                                                                                                Integer.MAX_VALUE));
-        Assert.assertEquals(2, tmTextUnitVariants.size());
-    }
-
-
-    @Test
-    public void testAssociatePullRunToTextUnitVariantsWithExistingData() {
-        PullRun pullRun = pullRunService.createPullRun(repository, "associatePullRunToTextUnitVariantsWithExistingData");
-
-        pullRunService.associatePullRunToTextUnitVariants(pullRun, asset,
-                                                          Arrays.asList(tmTestData.addCurrentTMTextUnitVariant1FrFR,
-                                                                        tmTestData.addCurrentTMTextUnitVariant2FrCA));
-
-        List<TMTextUnitVariant> tmTextUnitVariants = pullRunService.getTextUnitVariants(pullRun, PageRequest.of(0,
-                                                                                                                Integer.MAX_VALUE));
-        Assert.assertEquals(2, tmTextUnitVariants.size());
-
-        pullRunService.associatePullRunToTextUnitVariants(pullRun, asset,
-                                                          Collections.singletonList(tmTestData.addCurrentTMTextUnitVariant2FrCA));
-        tmTextUnitVariants = pullRunService.getTextUnitVariants(pullRun, PageRequest.of(0, Integer.MAX_VALUE));
-        Assert.assertEquals(1, tmTextUnitVariants.size());
-    }
-
-    @Test
-    public void testAssociatePullRunToTextUnitIds() {
-        PullRun pullRun = pullRunService.createPullRun(repository, "associatePullRunToTextUnitIds");
-
-        pullRunService.associatePullRunToTextUnitIds(pullRun, asset,
-                                                     Arrays.asList(tmTestData.addCurrentTMTextUnitVariant1FrFR.getId(),
-                                                                   tmTestData.addCurrentTMTextUnitVariant2FrCA.getId()));
-
-        List<TMTextUnitVariant> tmTextUnitVariants = pullRunService.getTextUnitVariants(pullRun, PageRequest.of(0,
-                                                                                                                Integer.MAX_VALUE));
-        Assert.assertEquals(2, tmTextUnitVariants.size());
     }
 }
