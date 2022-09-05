8a6f4cbb72de555171530d129f9ca335d3664878
loliveiramontedonio <loliveiramontedonio@pinterest.com>
2022-09-05 15:32:54 -0300

    WIP deleteScreenshot backend

--- webapp/src/main/java/com/box/l10n/mojito/service/thirdparty/ThirdPartyScreenshotRepository.java
+++ webapp/src/main/java/com/box/l10n/mojito/service/thirdparty/ThirdPartyScreenshotRepository.java
@@ -1,10 +1,10 @@
 package com.box.l10n.mojito.service.thirdparty;
 
 import com.box.l10n.mojito.entity.ThirdPartyScreenshot;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import java.util.List;
 
 /** @author jeanaurambault */
 @RepositoryRestResource(exported = false)
@@ -12,5 +12,5 @@ public interface ThirdPartyScreenshotRepository
     extends JpaRepository<ThirdPartyScreenshot, Long>,
         JpaSpecificationExecutor<ThirdPartyScreenshot> {
 
-    List<ThirdPartyScreenshot> findAllByScreenshotId(Long screenshotId);
+  List<ThirdPartyScreenshot> findAllByScreenshotId(Long screenshotId);
 }
