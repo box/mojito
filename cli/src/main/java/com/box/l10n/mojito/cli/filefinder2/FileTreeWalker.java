089fa88154ac28cd61317d29ed5fa33a7ca8030e
Jean Aurambault <aurambaj@users.noreply.github.com>
2022-10-12 14:21:59 -0700

    Add test with timing for some common file types

--- cli/src/main/java/com/box/l10n/mojito/cli/filefinder2/FileTreeWalker.java
+++ cli/src/main/java/com/box/l10n/mojito/cli/filefinder2/FileTreeWalker.java
@@ -11,19 +11,19 @@ import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-
+import java.util.stream.Stream;
 
 /**
  * FileTreeWalker with "include" and "exclude" regex filters
- * <p>
- * The FileTreeWalker calls onIncludeFile() for files that don't match any of the "exclude" patterns
- * and that match one of the "include" patterns. In other words "exclude" has precedence over
- * "include". The pattern matching is done on the "start" relativized path
- * <p>
- * The "include" regex pattern applies to file only. The "exclude" pattern is applied to both files
- * and directories to avoid walking unecessary part of the tree.
- * <p>
- * The file passed to onIncludeFile() is from the SimpleFileVisitor, left unchanged
+ *
+ * <p>The FileTreeWalker calls onIncludeFile() for files that don't match any of the "exclude"
+ * patterns and that match one of the "include" patterns. In other words "exclude" has precedence
+ * over "include". The pattern matching is done on the "start" relativized path
+ *
+ * <p>The "include" regex pattern applies to file only. The "exclude" pattern is applied to both
+ * files and directories to avoid walking unecessary part of the tree.
+ *
+ * <p>The file passed to onIncludeFile() is from the SimpleFileVisitor, left unchanged
  */
 public class FileTreeWalker {
 
@@ -32,58 +32,70 @@ public class FileTreeWalker {
   final List<Pattern> includePatterns;
   final List<Pattern> excludePatterns;
 
-  public FileTreeWalker(Path start, Consumer<Path> onIncludedFile, List<String> includeRegexes,
+  final List<Pattern> includeDirPatterns =
+      Stream.of("").map(Pattern::compile).collect(Collectors.toList());
+  final List<Pattern> excludeDirPatterns =
+      Stream.of(".git").map(Pattern::compile).collect(Collectors.toList());
+
+  public FileTreeWalker(
+      Path start,
+      Consumer<Path> onIncludedFile,
+      List<String> includeRegexes,
       List<String> excludeRegexes) {
     this.start = start;
     this.onIncludedFile = onIncludedFile;
-    this.includePatterns = includeRegexes.stream().map(Pattern::compile)
-        .collect(Collectors.toList());
-    this.excludePatterns = excludeRegexes.stream().map(Pattern::compile)
-        .collect(Collectors.toList());
+    this.includePatterns =
+        includeRegexes.stream().map(Pattern::compile).collect(Collectors.toList());
+    this.excludePatterns =
+        excludeRegexes.stream().map(Pattern::compile).collect(Collectors.toList());
   }
 
   public void walkFileTree() {
 
     try {
-      Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
+      Files.walkFileTree(
+          start,
+          new SimpleFileVisitor<Path>() {
 
-        @Override
-        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
-            throws IOException {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
+                throws IOException {
 
-          FileVisitResult fileVisitResult = FileVisitResult.CONTINUE;
+              FileVisitResult fileVisitResult = FileVisitResult.CONTINUE;
 
-          final Path relativeDirPath = start.relativize(dir);
+              final Path relativeDirPath = start.relativize(dir);
 
-          if (shouldExcludePath(relativeDirPath)) {
-            fileVisitResult = fileVisitResult.SKIP_SUBTREE;
-          }
+              if (shouldExcludePath(relativeDirPath)) {
+                fileVisitResult = fileVisitResult.SKIP_SUBTREE;
+              }
 
-          return fileVisitResult;
-        }
-
-        @Override
-        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-          final Path relativeFilePath = start.relativize(file);
-
-          if (!shouldExcludePath(relativeFilePath)) {
-            final boolean shouldIncludeFile = includePatterns.stream()
-                .map(pattern -> pattern.matcher(relativeFilePath.toString())).anyMatch(
-                    Matcher::matches);
-            if (shouldIncludeFile) {
-              onIncludedFile.accept(file);
+              return fileVisitResult;
             }
-          }
 
-          return FileVisitResult.CONTINUE;
-        }
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+                throws IOException {
+              final Path relativeFilePath = start.relativize(file);
 
-        private boolean shouldExcludePath(Path relativeDirPath) {
-          return excludePatterns.stream()
-              .map(pattern -> pattern.matcher(relativeDirPath.toString())).anyMatch(
-                  Matcher::matches);
-        }
-      });
+              if (!shouldExcludePath(relativeFilePath)) {
+                final boolean shouldIncludeFile =
+                    includePatterns.stream()
+                        .map(pattern -> pattern.matcher(relativeFilePath.toString()))
+                        .anyMatch(Matcher::matches);
+                if (shouldIncludeFile) {
+                  onIncludedFile.accept(file);
+                }
+              }
+
+              return FileVisitResult.CONTINUE;
+            }
+
+            private boolean shouldExcludePath(Path relativeDirPath) {
+              return excludePatterns.stream()
+                  .map(pattern -> pattern.matcher(relativeDirPath.toString()))
+                  .anyMatch(Matcher::matches);
+            }
+          });
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
--- cli/src/test/java/com/box/l10n/mojito/cli/filefinder2/FileTreeWalkerTest.java
+++ cli/src/test/java/com/box/l10n/mojito/cli/filefinder2/FileTreeWalkerTest.java
@@ -1,9 +1,9 @@
 package com.box.l10n.mojito.cli.filefinder2;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import com.google.common.base.Stopwatch;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
@@ -11,12 +11,41 @@ class FileTreeWalkerTest {
 
   @Test
   void test() {
-    final Path start = Paths.get("").toAbsolutePath();
+    final Stopwatch stopwatch = Stopwatch.createStarted();
+
+    final Path start = Paths.get("/Users/jeanaurambault/").toAbsolutePath();
     System.out.println(start);
-    new FileTreeWalker(start, path -> {
-      System.out.println(path);
-      System.out.println(start.relativize(path));
-    }, Arrays.asList(".*\\.resx"),
-        Arrays.asList(".*\\.fr\\.resx", "target")).walkFileTree();
+
+    final ArrayList<Path> paths = new ArrayList<>();
+
+    new FileTreeWalker(
+        start,
+        path -> {
+          //      System.out.println(path);
+          //        System.out.println(start.relativize(path));
+          //        try {
+          //          Thread.sleep(10);
+          //        } catch (InterruptedException e) {
+          //          throw new RuntimeException(e);
+          //        }
+          paths.add(path);
+        },
+        Arrays.asList(
+//                  "^(?<parentpath>(?:.+/)?)res/values/(?<basename>strings)\\.(?<extension>xml)$",
+//                  "^(?<parentpath>(?:.+/)?)(?<basename>[^.]+)\\.(?<extension>xlf)$",
+//                  "^(?<parentpath>(?:.+/)?)(?<locale>en)\\.lproj/(?<basename>.+)\\.(?<extension>strings)$",
+//                  "^(?<parentpath>(?:.+/)?)(?<locale>Base)\\.lproj/(?<basename>.+)\\.(?<extension>strings)$",
+            "^(?<parentpath>(?:.+/)?)LC_MESSAGES/(?<basename>.+)\\.(?<extension>pot)$"
+//                  "^(?<parentpath>(?:.+/)?)(?<basename>[^\\_]+)\\.(?<extension>properties)$"
+        ),
+        Arrays.asList(
+//            ".git"
+        ))
+        .walkFileTree();
+
+    System.out.println("source files count: " + paths.size());
+    paths.stream().forEach(System.out::println);
+
+    System.out.println("elapsed time: " + stopwatch);
   }
 }
