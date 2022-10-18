d43a68ab499057f73a7b31edcd9a640dd69363d7
Jean Aurambault <aurambaj@users.noreply.github.com>
2022-10-17 21:54:58 -0700

    Make DirectoryScanUtils a component to help figuring out if directories should be scanned or not

--- cli/src/main/java/com/box/l10n/mojito/cli/filefinder/DirectoryScanUtils.java
+++ cli/src/main/java/com/box/l10n/mojito/cli/filefinder/DirectoryScanUtils.java
@@ -1,6 +1,78 @@
 package com.box.l10n.mojito.cli.filefinder;
 
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * Helps to figure out which directories need to be scanned based on given lists of "include"
+ * patterns and "exclude" patterns.
+ *
+ * <p>Directory patterns are like directory paths but some subpaths can contain a wild card: "*",
+ * for example "modules/*&#47;src/main". The wild card will exactly match one directory name (no
+ * recursion in the directory).
+ */
 public class DirectoryScanUtils {
+
+  private final Path rootDirectory;
+
+  private final List<String[]> directoriesIncludePatterns;
+  private final List<String[]> directoriesExcludePatterns;
+
+  public DirectoryScanUtils(
+      Path rootDirectory,
+      List<String> directoriesIncludePatterns,
+      List<String> directoriesExcludePatterns) {
+    this.rootDirectory = Objects.requireNonNull(rootDirectory);
+    this.directoriesIncludePatterns = splitPatterns(directoriesIncludePatterns);
+    this.directoriesExcludePatterns = splitPatterns(directoriesExcludePatterns);
+  }
+
+  /**
+   * Indicates if a directory should be scanned to find files to process.
+   *
+   * <p>The root directory should always be scanned. Other directories should be scanned based on
+   * the "exclude" and "include" lists. The "exclude" list has precendence over the include list If
+   * the directoriesExcludePatterns list is non-null, the directory must not match any of the
+   * "exclude" patterns. If the directoriesIncludePatterns list is non-null, the directory will have
+   * to match at least one of the "include" patterns.
+   */
+  public boolean shouldScan(Path dir) {
+    return rootDirectory.equals(dir) || (!isExcludedDirectory(dir) && isIncludedDirectory(dir));
+  }
+
+  private boolean isExcludedDirectory(Path dir) {
+    boolean exclude = false;
+    if (directoriesExcludePatterns != null) {
+      String[] relativePathSplit = getRelativePathSplit(dir);
+      exclude =
+          directoriesExcludePatterns.stream()
+              .anyMatch(
+                  s ->
+                      s.length == relativePathSplit.length
+                          && DirectoryScanUtils.shouldScanDirectory(s, relativePathSplit));
+    }
+    return exclude;
+  }
+
+  private boolean isIncludedDirectory(Path dir) {
+    boolean include = true;
+    if (directoriesIncludePatterns != null) {
+      String[] relativePathSplit = getRelativePathSplit(dir);
+      include =
+          directoriesIncludePatterns.stream()
+              .anyMatch(s -> DirectoryScanUtils.shouldScanDirectory(s, relativePathSplit));
+    }
+    return include;
+  }
+
+  private String[] getRelativePathSplit(Path dir) {
+    Path relativePath = rootDirectory.relativize(dir);
+    String[] relativePathSplit = relativePath.toString().split("/");
+    return relativePathSplit;
+  }
+
   /**
    * Indicates if a directory should be scanned given a pattern. A directory should be scanned if
    * its sub paths matches the ones in the directory pattern. If the directory is shorter than the
@@ -37,5 +109,16 @@ public class DirectoryScanUtils {
     }
 
     return shouldScan;
+  }
+
+  static List<String[]> splitPatterns(List<String> directoriesIncludePatterns) {
+    List<String[]> splitted = null;
+
+    if (directoriesIncludePatterns != null) {
+      splitted =
+          directoriesIncludePatterns.stream().map(s -> s.split("/")).collect(Collectors.toList());
+    }
+
+    return splitted;
   }
 }
--- cli/src/test/java/com/box/l10n/mojito/cli/filefinder/DirectoryScanUtilsTest.java
+++ cli/src/test/java/com/box/l10n/mojito/cli/filefinder/DirectoryScanUtilsTest.java
@@ -1,11 +1,31 @@
 package com.box.l10n.mojito.cli.filefinder;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import java.nio.file.Paths;
+import java.util.Arrays;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 public class DirectoryScanUtilsTest {
+  @Test
+  public void testShouldScan() {
+    final DirectoryScanUtils directoryScanUtils =
+        new DirectoryScanUtils(
+            Paths.get("test"),
+            Arrays.asList("modules/*/src", "strings"),
+            Arrays.asList("modules/1"));
+    assertFalse(directoryScanUtils.shouldScan(Paths.get("test/.git")));
+    assertFalse(directoryScanUtils.shouldScan(Paths.get("test/modules/1")));
+    assertTrue(directoryScanUtils.shouldScan(Paths.get("test/")));
+    assertTrue(directoryScanUtils.shouldScan(Paths.get("test/modules/")));
+    assertTrue(directoryScanUtils.shouldScan(Paths.get("test/modules/2")));
+    assertTrue(directoryScanUtils.shouldScan(Paths.get("test/modules/2/src/")));
+    assertTrue(directoryScanUtils.shouldScan(Paths.get("test/modules/3/src/sub")));
+    assertTrue(directoryScanUtils.shouldScan(Paths.get("test/strings")));
+  }
 
   @Test
   public void testShouldScanDirectory() {
@@ -62,5 +82,16 @@ public class DirectoryScanUtilsTest {
         DirectoryScanUtils.shouldScanDirectory(
             new String[] {"modules", "*", "locale"},
             new String[] {"modules", "api2", "locale", "sub1"}));
+  }
+
+  @Test
+  public void testSplitPatternNonNull() {
+    Assertions.assertThat(DirectoryScanUtils.splitPatterns(Arrays.asList("p1", "p2/p3")))
+        .containsExactly(new String[] {"p1"}, new String[] {"p2", "p3"});
+  }
+
+  @Test
+  public void testSplitPatterNull() {
+    assertNull(DirectoryScanUtils.splitPatterns(null));
   }
 }
