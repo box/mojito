53e0ec557e63349986c899d9c4274f79c1bd481c
Jean Aurambault <aurambaj@users.noreply.github.com>
2022-10-19 16:06:44 -0700

    Improve the error/exception when there is a dupplicate in android
    before it would show only one id, now it will show both entries conflicting

--- webapp/src/main/java/com/box/l10n/mojito/android/strings/AndroidPluralDuplicateKeyException.java
+++ webapp/src/main/java/com/box/l10n/mojito/android/strings/AndroidPluralDuplicateKeyException.java
@@ -2,7 +2,7 @@ package com.box.l10n.mojito.android.strings;
 
 public class AndroidPluralDuplicateKeyException extends IllegalStateException {
 
-  public AndroidPluralDuplicateKeyException(String message, Throwable cause) {
-    super(message, cause);
+  public AndroidPluralDuplicateKeyException(String message) {
+    super(message);
   }
 }
