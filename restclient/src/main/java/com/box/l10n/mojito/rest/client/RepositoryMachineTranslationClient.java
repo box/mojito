e928348900e28d5a1b17019606614d5adc189146
Jean Aurambault <aurambaj@users.noreply.github.com>
2022-11-18 15:01:36 -0800

    Add web service and CLI to machine translate untranslated strings in a repository
    a quick but working implementation: one locale at the time, get untranslated strings, send them to the machine translation engine and then import the translations in the repository

---
+++ restclient/src/main/java/com/box/l10n/mojito/rest/client/RepositoryMachineTranslationClient.java
@@ -0,0 +1,40 @@
+package com.box.l10n.mojito.rest.client;
+
+import com.box.l10n.mojito.rest.entity.RepositoryMachineTranslationBody;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+/** @author jaurambault */
+@Component
+public class RepositoryMachineTranslationClient extends BaseClient {
+
+  /** logger */
+  static Logger logger = LoggerFactory.getLogger(RepositoryMachineTranslationClient.class);
+
+  @Override
+  public String getEntityName() {
+    return "machine-translation";
+  }
+
+  /**
+   * Machine translate untranslated strings in a repository for a given list of locales
+   *
+   * @param repositoryMachineTranslationBody
+   * @return {@link RepositoryMachineTranslationBody}
+   */
+  public RepositoryMachineTranslationBody translateRepository(
+      RepositoryMachineTranslationBody repositoryMachineTranslationBody) {
+
+    String translateRepositoryPath =
+        UriComponentsBuilder.fromPath(getBasePathForEntity())
+            .pathSegment("repository")
+            .toUriString();
+
+    return authenticatedRestTemplate.postForObject(
+        translateRepositoryPath,
+        repositoryMachineTranslationBody,
+        RepositoryMachineTranslationBody.class);
+  }
+}
---
+++ webapp/src/main/java/com/box/l10n/mojito/service/machinetranslation/RepositoryMachineTranslationService.java
@@ -0,0 +1,127 @@
+package com.box.l10n.mojito.service.machinetranslation;
+
+import com.box.l10n.mojito.entity.Repository;
+import com.box.l10n.mojito.service.pollableTask.Pollable;
+import com.box.l10n.mojito.service.pollableTask.PollableFuture;
+import com.box.l10n.mojito.service.pollableTask.PollableFutureTaskResult;
+import com.box.l10n.mojito.service.repository.RepositoryRepository;
+import com.box.l10n.mojito.service.tm.importer.TextUnitBatchImporterService;
+import com.box.l10n.mojito.service.tm.search.StatusFilter;
+import com.box.l10n.mojito.service.tm.search.TextUnitDTO;
+import com.box.l10n.mojito.service.tm.search.TextUnitSearcher;
+import com.box.l10n.mojito.service.tm.search.TextUnitSearcherParameters;
+import com.box.l10n.mojito.service.tm.search.UsedFilter;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RepositoryMachineTranslationService {
+
+  static Logger logger = LoggerFactory.getLogger(RepositoryMachineTranslationService.class);
+
+  @Autowired MachineTranslationService machineTranslationService;
+  @Autowired RepositoryRepository repositoryRepository;
+
+  @Autowired TextUnitSearcher textUnitSearcher;
+
+  @Autowired TextUnitBatchImporterService textUnitBatchImporterService;
+
+  /**
+   * Machine translate untranslated strings in a repository.
+   *
+   * <p>one locale at the time, get untranslated strings, send them to the machine translation
+   * engine and then import the translations in the repository
+   *
+   * @param repositoryName
+   * @param targetBcp47Tags
+   * @return
+   */
+  @Pollable(async = true, message = "Start machine translating repository")
+  public PollableFuture<Void> translateRepository(
+      String repositoryName, List<String> targetBcp47Tags) {
+
+    logger.info(
+        "Start Machine Translating repository: {} and target locales: {}",
+        repositoryName,
+        targetBcp47Tags);
+    Repository repository = repositoryRepository.findByName(repositoryName);
+
+    if (repository == null) {
+      throw new RuntimeException("Must have a valid repository name to machine translate");
+    }
+
+    for (String targetBcp47Tag : targetBcp47Tags) {
+      final TextUnitSearcherParameters searchParameters = new TextUnitSearcherParameters();
+      searchParameters.setRepositoryIds(repository.getId());
+      searchParameters.setStatusFilter(StatusFilter.UNTRANSLATED);
+      searchParameters.setUsedFilter(UsedFilter.USED);
+      searchParameters.setLocaleTags(Arrays.asList(targetBcp47Tag));
+
+      final List<TextUnitDTO> textUnitDTOS = textUnitSearcher.search(searchParameters);
+
+      final List<String> sourceTexts =
+          textUnitDTOS.stream().map(TextUnitDTO::getSource).collect(Collectors.toList());
+
+      final TranslationsResponseDTO translationsResponseDTO =
+          machineTranslationService.getTranslations(
+              sourceTexts,
+              repository.getSourceLocale().getBcp47Tag(),
+              Arrays.asList(targetBcp47Tag),
+              false,
+              true,
+              null,
+              null);
+
+      final Map<String, List<TextUnitDTO>> sourceToTextUnitDTOs =
+          textUnitDTOS.stream().collect(Collectors.groupingBy(TextUnitDTO::getSource));
+
+      logger.info("Number of text unit to machine translate: {}", sourceToTextUnitDTOs.size());
+
+      if (sourceToTextUnitDTOs.size() > 300) {
+        throw new RuntimeException("Too many text unit, fail safe for now");
+      }
+
+      final List<TextUnitDTO> machineTranslatedTextUnitDTOs =
+          translationsResponseDTO.getTextUnitTranslations().stream()
+              .flatMap(
+                  textUnitTranslationGroupDTO -> {
+                    final String sourceText = textUnitTranslationGroupDTO.getSourceText();
+
+                    final Stream<TextUnitDTO> textUnitDTOStreamFor1Translation;
+
+                    if (!textUnitTranslationGroupDTO.getTranslations().isEmpty()) {
+                      // since we're doing 1 locale we just read the first entry
+                      final TranslationDTO translationDTO =
+                          textUnitTranslationGroupDTO.getTranslations().get(0);
+
+                      textUnitDTOStreamFor1Translation =
+                          sourceToTextUnitDTOs.getOrDefault(sourceText, Collections.emptyList())
+                              .stream()
+                              .map(
+                                  textUnitDTO -> {
+                                    textUnitDTO.setTarget(translationDTO.getText());
+                                    return textUnitDTO;
+                                  });
+                    } else {
+                      logger.error("There must be a translation available but if not, just skip");
+                      textUnitDTOStreamFor1Translation = Stream.empty();
+                    }
+
+                    return textUnitDTOStreamFor1Translation;
+                  })
+              .collect(Collectors.toList());
+
+      textUnitBatchImporterService.importTextUnits(machineTranslatedTextUnitDTOs, false, true);
+    }
+
+    return new PollableFutureTaskResult<>();
+  }
+}
