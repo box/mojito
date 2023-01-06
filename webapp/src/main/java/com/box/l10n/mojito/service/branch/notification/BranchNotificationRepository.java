48f869b90536561a84af3003c78a7861aac2eb73
Jean Aurambault <aurambaj@users.noreply.github.com>
2023-01-06 14:18:25 +0100

    Find BranchNotification by notifier id

--- webapp/src/main/java/com/box/l10n/mojito/service/branch/notification/BranchNotificationRepository.java
+++ webapp/src/main/java/com/box/l10n/mojito/service/branch/notification/BranchNotificationRepository.java
@@ -11,5 +11,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 public interface BranchNotificationRepository
     extends JpaRepository<BranchNotification, Long>, JpaSpecificationExecutor<BranchNotification> {
 
+  BranchNotification findByBranchAndNotifierId(Branch branch, String notifierId);
+
   BranchNotification findByBranchAndSenderType(Branch branch, String senderType);
 }
