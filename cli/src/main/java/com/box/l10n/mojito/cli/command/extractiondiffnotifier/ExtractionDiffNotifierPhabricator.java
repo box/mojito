113f08df269d61904e73709a59adb0e731a9a4b9
Jean Aurambault <aurambaj@users.noreply.github.com>
2023-01-16 17:16:32 -0800

    Add Phabricator implementation of ExtractionDiffNotifier

---
+++ cli/src/main/java/com/box/l10n/mojito/cli/command/extractiondiffnotifier/ExtractionDiffNotifierPhabricator.java
@@ -0,0 +1,31 @@
+package com.box.l10n.mojito.cli.command.extractiondiffnotifier;
+
+import com.box.l10n.mojito.cli.command.extraction.ExtractionDiffStatistics;
+import com.box.l10n.mojito.phabricator.DifferentialRevision;
+import com.google.common.base.Preconditions;
+
+public class ExtractionDiffNotifierPhabricator implements ExtractionDiffNotifier {
+
+  ExtractionDiffNotifierMessageBuilder extractionDiffNotifierMessageBuilder;
+
+  DifferentialRevision differentialRevision;
+
+  String objectIdentifier;
+
+  public ExtractionDiffNotifierPhabricator(
+      ExtractionDiffNotifierMessageBuilder extractionDiffNotifierMessageBuilder,
+      DifferentialRevision differentialRevision,
+      String objectIdentifier) {
+    this.extractionDiffNotifierMessageBuilder =
+        Preconditions.checkNotNull(extractionDiffNotifierMessageBuilder);
+    this.differentialRevision = Preconditions.checkNotNull(differentialRevision);
+    this.objectIdentifier = Preconditions.checkNotNull(objectIdentifier);
+  }
+
+  @Override
+  public String sendDiffStatistics(ExtractionDiffStatistics extractionDiffStatistics) {
+    String message = extractionDiffNotifierMessageBuilder.getMessage(extractionDiffStatistics);
+    differentialRevision.addComment(objectIdentifier, message);
+    return message;
+  }
+}
