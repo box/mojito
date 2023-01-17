4c3c914332d93393bc300877d81673c49a30390a
Jean Aurambault <aurambaj@users.noreply.github.com>
2023-01-16 20:35:36 -0800

    Add springboot based configuration for ExtractionDiffNotifier
    ExtractionDiffNotificationCommand can get notifiers specified by CLI arguments. But it might be preferable later on to share the configuration via springboot configuration. Keeps to possibility for now, and see what turns out to be the best option.

---
+++ cli/src/main/java/com/box/l10n/mojito/cli/command/extractiondiffnotifier/ExtractionDiffNotifiersConfigurationProperties.java
@@ -0,0 +1,168 @@
+package com.box.l10n.mojito.cli.command.extractiondiffnotifier;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("l10n.extraction-diff.notifiers")
+public class ExtractionDiffNotifiersConfigurationProperties {
+
+  Map<String, GithubConfigurationProperties> githubs = new HashMap<>();
+
+  Map<String, SlackConfigurationProperties> slacks = new HashMap<>();
+
+  Map<String, PhabricatorConfigurationProperties> phabricators = new HashMap<>();
+
+  public Map<String, GithubConfigurationProperties> getGithub() {
+    return githubs;
+  }
+
+  public Map<String, SlackConfigurationProperties> getSlack() {
+    return slacks;
+  }
+
+  public Map<String, PhabricatorConfigurationProperties> getPhabricator() {
+    return phabricators;
+  }
+
+  static class GithubConfigurationProperties {
+
+    String owner;
+
+    String repository;
+
+    String messageTemplate = "{baseMessage}";
+
+    int prNumber;
+
+    public String getOwner() {
+      return owner;
+    }
+
+    public void setOwner(String owner) {
+      this.owner = owner;
+    }
+
+    public String getRepository() {
+      return repository;
+    }
+
+    public void setRepository(String repository) {
+      this.repository = repository;
+    }
+
+    public String getMessageTemplate() {
+      return messageTemplate;
+    }
+
+    public void setMessageTemplate(String messageTemplate) {
+      this.messageTemplate = messageTemplate;
+    }
+
+    public int getPrNumber() {
+      return prNumber;
+    }
+
+    public void setPrNumber(int prNumber) {
+      this.prNumber = prNumber;
+    }
+  }
+
+  static class SlackConfigurationProperties {
+
+    String slackClientId;
+
+    String userEmailPattern;
+
+    boolean useDirectMessage = false;
+
+    String messageTemplate = "{baseMessage}";
+
+    String username;
+
+    public String getSlackClientId() {
+      return slackClientId;
+    }
+
+    public void setSlackClientId(String slackClientId) {
+      this.slackClientId = slackClientId;
+    }
+
+    public String getUserEmailPattern() {
+      return userEmailPattern;
+    }
+
+    public void setUserEmailPattern(String userEmailPattern) {
+      this.userEmailPattern = userEmailPattern;
+    }
+
+    public boolean isUseDirectMessage() {
+      return useDirectMessage;
+    }
+
+    public void setUseDirectMessage(boolean useDirectMessage) {
+      this.useDirectMessage = useDirectMessage;
+    }
+
+    public String getMessageTemplate() {
+      return messageTemplate;
+    }
+
+    public void setMessageTemplate(String messageTemplate) {
+      this.messageTemplate = messageTemplate;
+    }
+
+    public String getUsername() {
+      return username;
+    }
+
+    public void setUsername(String username) {
+      this.username = username;
+    }
+  }
+
+  static class PhabricatorConfigurationProperties {
+
+    String url;
+
+    String token;
+
+    String messageTemplate = "{baseMessage}";
+
+    String objectIdentifier;
+
+    public String getUrl() {
+      return url;
+    }
+
+    public void setUrl(String url) {
+      this.url = url;
+    }
+
+    public String getToken() {
+      return token;
+    }
+
+    public void setToken(String token) {
+      this.token = token;
+    }
+
+    public String getMessageTemplate() {
+      return messageTemplate;
+    }
+
+    public void setMessageTemplate(String messageTemplate) {
+      this.messageTemplate = messageTemplate;
+    }
+
+    public String getObjectIdentifier() {
+      return objectIdentifier;
+    }
+
+    public void setObjectIdentifier(String objectIdentifier) {
+      this.objectIdentifier = objectIdentifier;
+    }
+  }
+}
