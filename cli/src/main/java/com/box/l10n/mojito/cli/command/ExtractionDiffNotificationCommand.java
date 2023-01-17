b10fd5ceacb2845b77fbaeda29d8bb8d4be9e50f
Jean Aurambault <aurambaj@users.noreply.github.com>
2023-01-16 18:50:13 -0800

    Add ExtractionDiffNotificationCommand to send extraction diff statistics to multiple system
    It is a refactor of PhabricatorExtractionDiffNotificationCommand to support multiple notifiers (Slack, Github, Phabricator). The other command is kept for backward compatibility. GitExtractionDiffNotificationCommand that was recently will be dropped since it should not be used yet.

---
+++ cli/src/main/java/com/box/l10n/mojito/cli/command/ExtractionDiffNotificationCommand.java
@@ -0,0 +1,257 @@
+package com.box.l10n.mojito.cli.command;
+
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+import com.box.l10n.mojito.cli.command.extraction.ExtractionDiffPaths;
+import com.box.l10n.mojito.cli.command.extraction.ExtractionDiffService;
+import com.box.l10n.mojito.cli.command.extraction.ExtractionDiffStatistics;
+import com.box.l10n.mojito.cli.command.extraction.ExtractionPaths;
+import com.box.l10n.mojito.cli.command.extraction.MissingExtractionDirectoryExcpetion;
+import com.box.l10n.mojito.cli.command.extractiondiffnotifier.ExtractionDiffNotifier;
+import com.box.l10n.mojito.cli.command.extractiondiffnotifier.ExtractionDiffNotifierGithub;
+import com.box.l10n.mojito.cli.command.extractiondiffnotifier.ExtractionDiffNotifierMessageBuilder;
+import com.box.l10n.mojito.cli.command.extractiondiffnotifier.ExtractionDiffNotifierPhabricator;
+import com.box.l10n.mojito.cli.command.extractiondiffnotifier.ExtractionDiffNotifierSlack;
+import com.box.l10n.mojito.cli.console.ConsoleWriter;
+import com.box.l10n.mojito.github.GithubClient;
+import com.box.l10n.mojito.github.GithubClients;
+import com.box.l10n.mojito.phabricator.DifferentialRevision;
+import com.box.l10n.mojito.slack.SlackClient;
+import com.google.common.base.Preconditions;
+import java.util.HashSet;
+import java.util.Set;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+/**
+ * Command to compute extraction diff statistics and send notifications. Supported notifiers are
+ * Github, Phabricator, Slack.
+ *
+ * <p>A notification is sent if the Extraction contains any added or removed text units. The level
+ * (icon: info, warning, danger) of the notification will depend on the number of removed text
+ * units.
+ *
+ * <p>Supersede {@link PhabricatorExtractionDiffNotificationCommand }
+ */
+@Component
+@Scope("prototype")
+@Parameters(
+    commandNames = {"extraction-diff-notif"},
+    commandDescription = "Compute extraction diff statistics and optionally notify Phabricator")
+public class ExtractionDiffNotificationCommand extends Command {
+
+  /** logger */
+  static Logger logger = LoggerFactory.getLogger(ExtractionDiffNotificationCommand.class);
+
+  @Qualifier("ansiCodeEnabledFalse")
+  @Autowired
+  ConsoleWriter consoleWriterAnsiCodeEnabledFalse;
+
+  @Autowired(required = false)
+  DifferentialRevision differentialRevision;
+
+  @Autowired GithubClients githubClients;
+
+  @Autowired SlackClient slackClient;
+
+  @Autowired ExtractionDiffService extractionDiffService;
+
+  @Parameter(
+      names = {"--current", "-c"},
+      arity = 1,
+      required = true,
+      description = ExtractionDiffCommand.CURRENT_EXTRACTION_NAME_DESCRIPTION)
+  String currentExtractionName;
+
+  @Parameter(
+      names = {"--base", "-b"},
+      arity = 1,
+      required = true,
+      description = ExtractionDiffCommand.BASE_EXTRACTION_NAME_DESCRIPTION)
+  String baseExtractionName;
+
+  @Parameter(
+      names = {"--name", "-n"},
+      arity = 1,
+      description = ExtractionDiffCommand.EXCTRACTION_DIFF_NAME_DESCRIPTION)
+  String extractionDiffName = null;
+
+  @Parameter(
+      names = {"--output-directory", "-o"},
+      arity = 1,
+      description = ExtractionDiffCommand.OUTPUT_DIRECTORY_DESCRIPTION)
+  String outputDirectoryParam = ExtractionDiffPaths.DEFAULT_OUTPUT_DIRECTORY;
+
+  @Parameter(
+      names = {"--input-directory", "-i"},
+      arity = 1,
+      description = ExtractionDiffCommand.INPUT_DIRECTORY_DESCRIPTION)
+  String inputDirectoryParam = ExtractionPaths.DEFAULT_OUTPUT_DIRECTORY;
+
+  @Parameter(
+      names = "--github-owner",
+      arity = 1,
+      description =
+          "The owner to send Github notifications (assume GitClients are configured). If set, it will setup the Github notifier.")
+  String githubOwner;
+
+  @Parameter(
+      names = "--github-message-template",
+      arity = 1,
+      description = "The template to format the message sent to Github")
+  String githubMessageTemplate = "{baseMessage}";
+
+  @Parameter(
+      names = "--github-repository",
+      arity = 1,
+      description = "The Github repository used to send the notification")
+  String githubRepository;
+
+  @Parameter(
+      names = "--github-pr-number",
+      arity = 1,
+      description = "The Github Pull Request number used to send the notification")
+  int githubPrNumber;
+
+  @Parameter(
+      names = "--slack-username",
+      arity = 1,
+      description =
+          "To username, recepient of the Slack message (assume SlackClients are configured). If set, it will setup the Slack notifier.")
+  String slackUsername;
+
+  @Parameter(
+      names = "--slack-user-email-pattern",
+      arity = 1,
+      description = "The user email pattern used to send the Slack notification")
+  String slackUserEmailPattern;
+
+  @Parameter(
+      names = "--slack-use-direct-message",
+      arity = 1,
+      description = "To send a direct Slack message")
+  boolean slackUseDirectMessage;
+
+  @Parameter(
+      names = "--slack-message-template",
+      arity = 1,
+      description = "The template to format the message sent to Slack")
+  String slackMessageTemplate = "{baseMessage}";
+
+  @Parameter(
+      names = "--phabricator-object-identifier",
+      arity = 1,
+      description =
+          "The Phabricator 'object identifier' where to send notifications. If set, it will setup the Phabricator notifier (requires a Phabricator client to be configured).")
+  String phabricatorObjectIdentifier;
+
+  @Parameter(
+      names = "--phabricator-message-template",
+      arity = 1,
+      description = "The template to format the message sent to Phabricator")
+  String phabricatorMessageTemplate = "{baseMessage}";
+
+  Set<ExtractionDiffNotifier> extractionDiffNotifiers;
+
+  @Override
+  public boolean shouldShowInCommandList() {
+    return false;
+  }
+
+  @Override
+  public void execute() throws CommandException {
+
+    initNotifiers();
+
+    ExtractionPaths baseExtractionPaths =
+        new ExtractionPaths(inputDirectoryParam, baseExtractionName);
+    ExtractionPaths currentExtractionPaths =
+        new ExtractionPaths(inputDirectoryParam, currentExtractionName);
+    ExtractionDiffPaths extractionDiffPaths =
+        ExtractionDiffPaths.builder()
+            .outputDirectory(outputDirectoryParam)
+            .diffExtractionName(extractionDiffName)
+            .baseExtractorPaths(baseExtractionPaths)
+            .currentExtractorPaths(currentExtractionPaths)
+            .build();
+
+    ExtractionDiffStatistics extractionDiffStatistics = null;
+    try {
+      extractionDiffStatistics =
+          extractionDiffService.computeExtractionDiffStatistics(extractionDiffPaths);
+    } catch (MissingExtractionDirectoryExcpetion missingExtractionDirectoryExcpetion) {
+      throw new CommandException(
+          "Can't compute extraction diff statistics", missingExtractionDirectoryExcpetion);
+    }
+
+    if (shouldSendNotification(extractionDiffStatistics)) {
+      for (ExtractionDiffNotifier extractionDiffNotifier : extractionDiffNotifiers) {
+        String message = extractionDiffNotifier.sendDiffStatistics(extractionDiffStatistics);
+        // TODO(jean) this will be duplicating messages in the output if multiple notifiers are
+        // used.
+        // We could log a generic message but then we may be missing build urls, etc
+        consoleWriterAnsiCodeEnabledFalse.a(message).println();
+      }
+    } else {
+      consoleWriterAnsiCodeEnabledFalse.a("No need to send notification").println();
+    }
+  }
+
+  boolean shouldSendNotification(ExtractionDiffStatistics extractionDiffStatistics) {
+    return extractionDiffStatistics.getRemoved() > 0 || extractionDiffStatistics.getAdded() > 0;
+  }
+
+  private void initNotifiers() {
+    extractionDiffNotifiers = new HashSet<>();
+    if (githubOwner != null) {
+      extractionDiffNotifiers.add(createGihubNotifier());
+    }
+
+    if (phabricatorObjectIdentifier != null) {
+      extractionDiffNotifiers.add(createPhabricatorNotifier());
+    }
+
+    if (slackUsername != null) {
+      extractionDiffNotifiers.add(createSlackNotifier());
+    }
+  }
+
+  private ExtractionDiffNotifierSlack createSlackNotifier() {
+    return new ExtractionDiffNotifierSlack(
+        slackClient,
+        slackUserEmailPattern,
+        slackUseDirectMessage,
+        new ExtractionDiffNotifierMessageBuilder(slackMessageTemplate),
+        slackUsername);
+  }
+
+  private ExtractionDiffNotifierPhabricator createPhabricatorNotifier() {
+    Preconditions.checkNotNull(phabricatorObjectIdentifier);
+    PhabricatorPreconditions.checkNotNull(differentialRevision);
+
+    return new ExtractionDiffNotifierPhabricator(
+        new ExtractionDiffNotifierMessageBuilder(phabricatorMessageTemplate),
+        differentialRevision,
+        phabricatorObjectIdentifier);
+  }
+
+  private ExtractionDiffNotifierGithub createGihubNotifier() {
+    Preconditions.checkNotNull(githubOwner);
+
+    GithubClient githubClient = githubClients.getClient(githubOwner);
+
+    if (githubClient == null) {
+      throw new CommandException("Github client missing for owner: " + githubOwner);
+    }
+
+    return new ExtractionDiffNotifierGithub(
+        new ExtractionDiffNotifierMessageBuilder(githubMessageTemplate),
+        githubClient,
+        githubRepository,
+        githubPrNumber);
+  }
+}
---
+++ cli/src/test/java/com/box/l10n/mojito/cli/command/ExtractionDiffNotificationCommandTest.java
@@ -0,0 +1,182 @@
+package com.box.l10n.mojito.cli.command;
+
+import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.box.l10n.mojito.cli.CLITestBase;
+import com.box.l10n.mojito.cli.command.extraction.ExtractionDiffService;
+import com.box.l10n.mojito.cli.command.extraction.ExtractionDiffStatistics;
+import com.box.l10n.mojito.github.GithubClient;
+import com.box.l10n.mojito.github.GithubClients;
+import com.box.l10n.mojito.phabricator.DifferentialRevision;
+import com.box.l10n.mojito.slack.SlackClient;
+import com.box.l10n.mojito.slack.request.Message;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+public class ExtractionDiffNotificationCommandTest extends CLITestBase {
+
+  @Test
+  public void shouldSendNotification() {
+    ExtractionDiffNotificationCommand extractionDiffNotificationCommand =
+        new ExtractionDiffNotificationCommand();
+
+    assertTrue(
+        extractionDiffNotificationCommand.shouldSendNotification(
+            ExtractionDiffStatistics.builder()
+                .added(0)
+                .removed(200)
+                .base(500)
+                .current(300)
+                .build()));
+  }
+
+  @Test
+  public void shouldNotSendNotification() {
+    ExtractionDiffNotificationCommand extractionDiffNotificationCommand =
+        new ExtractionDiffNotificationCommand();
+
+    assertFalse(
+        extractionDiffNotificationCommand.shouldSendNotification(
+            ExtractionDiffStatistics.builder().added(0).removed(0).base(500).current(300).build()));
+  }
+
+  @Test
+  public void sendNotification() throws Exception {
+
+    getL10nJCommander()
+        .run(
+            "extract",
+            "-s",
+            getInputResourcesTestDir("source1").getAbsolutePath(),
+            "-o",
+            getTargetTestDir("extractions").getAbsolutePath(),
+            "-n",
+            "source1",
+            "-fo",
+            "sometestoption=value1");
+
+    getL10nJCommander()
+        .run(
+            "extract",
+            "-s",
+            getInputResourcesTestDir("source2").getAbsolutePath(),
+            "-o",
+            getTargetTestDir("extractions").getAbsolutePath(),
+            "-n",
+            "source2",
+            "-fo",
+            "sometestoption=value1");
+
+    getL10nJCommander()
+        .run(
+            "extract-diff",
+            "-i",
+            getTargetTestDir("extractions").getAbsolutePath(),
+            "-o",
+            getTargetTestDir("extraction-diffs").getAbsolutePath(),
+            "-c",
+            "source2",
+            "-b",
+            "source1");
+
+    L10nJCommander l10nJCommander = getL10nJCommander();
+    ExtractionDiffNotificationCommand command =
+        l10nJCommander.getCommand(ExtractionDiffNotificationCommand.class);
+
+    DifferentialRevision mockDifferentialRevision = mock(DifferentialRevision.class);
+    command.differentialRevision = mockDifferentialRevision;
+
+    SlackClient mockSlackClient = mock(SlackClient.class);
+    command.slackClient = mockSlackClient;
+
+    command.githubClients = mock(GithubClients.class);
+    GithubClient mockGithubClient = mock(GithubClient.class);
+    when(command.githubClients.getClient("testowner1")).thenReturn(mockGithubClient);
+
+    l10nJCommander.run(
+        "extraction-diff-notif",
+        "-i",
+        getTargetTestDir("extractions").getAbsolutePath(),
+        "-o",
+        getTargetTestDir("extraction-diffs").getAbsolutePath(),
+        "-c",
+        "source2",
+        "-b",
+        "source1",
+        "--phabricator-message-template",
+        "{baseMessage} in diff: ${DIFF_ID}. Check [[https://build.org/${BUILD_NUMBER}|build]] for extraction details.",
+        "--phabricator-object-identifier",
+        "{objectId}",
+        "--slack-username",
+        "testusername",
+        "--slack-user-email-pattern",
+        "{0}@mojito.org",
+        "--github-owner",
+        "testowner1",
+        "--github-repository",
+        "testrepository",
+        "--github-pr-number",
+        "123",
+        "--github-message-template",
+        "Github -- {baseMessage}");
+
+    verify(mockDifferentialRevision, times(1))
+        .addComment(
+            "{objectId}",
+            "⚠️ 5 strings removed and 2 strings added (from 10 to 7) in diff: ${DIFF_ID}. Check [[https://build.org/${BUILD_NUMBER}|build]] for extraction details.");
+    assertTrue(
+        outputCapture
+            .toString()
+            .contains(
+                "⚠️ 5 strings removed and 2 strings added (from 10 to 7) in diff: ${DIFF_ID}. Check [[https://build.org/${BUILD_NUMBER}|build]] for extraction details."));
+    checkExpectedGeneratedResources();
+
+    ArgumentCaptor<Message> slackMessageArgumentCaptor = ArgumentCaptor.forClass(Message.class);
+    verify(mockSlackClient).sendInstantMessage(slackMessageArgumentCaptor.capture());
+    Assertions.assertThat(slackMessageArgumentCaptor.getValue().getAttachments().get(0).getText())
+        .isEqualTo("⚠️ 5 strings removed and 2 strings added (from 10 to 7)");
+
+    verify(mockGithubClient)
+        .addCommentToPR(
+            "testrepository",
+            123,
+            "Github -- ⚠️ 5 strings removed and 2 strings added (from 10 to 7)");
+  }
+
+  @Test
+  public void noNotifications() throws Exception {
+
+    L10nJCommander l10nJCommander = getL10nJCommander();
+    ExtractionDiffNotificationCommand command =
+        l10nJCommander.getCommand(ExtractionDiffNotificationCommand.class);
+
+    DifferentialRevision mock = mock(DifferentialRevision.class);
+    command.differentialRevision = mock;
+    command.extractionDiffService = mock(ExtractionDiffService.class);
+    Mockito.when(command.extractionDiffService.computeExtractionDiffStatistics(any()))
+        .thenReturn(ExtractionDiffStatistics.builder().build());
+
+    l10nJCommander.run(
+        "extraction-diff-notif",
+        "-i",
+        getTargetTestDir("extractions").getAbsolutePath(),
+        "-o",
+        getTargetTestDir("extraction-diffs").getAbsolutePath(),
+        "-c",
+        "source2",
+        "-b",
+        "source1");
+
+    verify(mock, Mockito.never()).addComment(anyString(), anyString());
+    assertTrue(outputCapture.toString().contains("No need to send notification"));
+  }
+}
