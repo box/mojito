87e9741d014c57495181d3897a411951d20bd70d
Edwin Hoogerbeets <ehoogerbeets@box.com>
2023-02-01 23:34:02 -0800

    Revert changes

--- cli/src/main/java/com/box/l10n/mojito/cli/command/CommandWaitForPollableTaskListener.java
+++ cli/src/main/java/com/box/l10n/mojito/cli/command/CommandWaitForPollableTaskListener.java
@@ -32,18 +32,6 @@ public class CommandWaitForPollableTaskListener implements WaitForPollableTaskLi
      */
     int numberAfterPollCallback = 0;
 
-    /**
-     * When true, do not generate output to the console.
-     */
-    boolean quiet;
-
-    /**
-     * @param quiet whether to send progress output to the console
-     */
-    CommandWaitForPollableTaskListener(boolean quiet) {
-        this.quiet = quiet;
-    }
-
     @Override
     public void afterPoll(PollableTask pollableTask) {
 
