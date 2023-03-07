440723e79ce3ad2276f4cd58214e06b7f25929e7
Jean Aurambault <aurambaj@users.noreply.github.com>
2023-03-07 09:28:44 -0800

    Change how the extract diff notifier sets the translation-required label
    For the initial string addition, the CLI will put the label, but for updates
    that creates new strings and if the initial string set has been translated,
    the server will have to change the label since this command won't have enough
    information to know the current state. The "translation-required" label
    would eventually be set by the server, but we set it here earlier here to
    be more reactive and not have to wait for the server to process the branch
    (which can lag a bit)

--- cli/src/main/java/com/box/l10n/mojito/cli/command/extractiondiffnotifier/ExtractionDiffNotifierGithub.java
+++ cli/src/main/java/com/box/l10n/mojito/cli/command/extractiondiffnotifier/ExtractionDiffNotifierGithub.java
@@ -1,13 +1,19 @@
 package com.box.l10n.mojito.cli.command.extractiondiffnotifier;
 
+import static com.box.l10n.mojito.github.PRLabel.TRANSLATIONS_READY;
 import static com.box.l10n.mojito.github.PRLabel.TRANSLATIONS_REQUIRED;
 import static com.box.l10n.mojito.github.PRLabel.updatePRLabel;
+import static org.slf4j.LoggerFactory.getLogger;
 
 import com.box.l10n.mojito.cli.command.extraction.ExtractionDiffStatistics;
 import com.box.l10n.mojito.github.GithubClient;
 import com.google.common.base.Preconditions;
+import org.slf4j.Logger;
 
 public class ExtractionDiffNotifierGithub implements ExtractionDiffNotifier {
+
+  /** logger */
+  static Logger logger = getLogger(ExtractionDiffNotifierGithub.class);
 
   ExtractionDiffNotifierMessageBuilder extractionDiffNotifierMessageBuilder;
 
@@ -36,8 +42,24 @@ public class ExtractionDiffNotifierGithub implements ExtractionDiffNotifier {
 
     String message = extractionDiffNotifierMessageBuilder.getMessage(extractionDiffStatistics);
     githubClient.addCommentToPR(repository, prNumber, message);
+
     if (extractionDiffStatistics.getAdded() > 0) {
-      updatePRLabel(githubClient, repository, prNumber, TRANSLATIONS_REQUIRED);
+      // For the initial string addition, the CLI will put the label, but for updates that creates
+      // new strings and if the initial string set has been translated, the server will have to
+      // change the label since this command won't have enough information to know the current
+      // state.
+      //
+      // The "translation-required" label would eventually be set by the server, but we set it here
+      // earlier here to be more reactive and not have to wait for the server to process the branch
+      // (which can lag a bit)
+      if (githubClient.isLabelAppliedToPR(repository, prNumber, TRANSLATIONS_READY.toString())) {
+        logger.debug(
+            "'translation-ready' is present, skip setting the 'translation-required' to avoid"
+                + " being in an invalid state where the translations are actually ready");
+      } else {
+        logger.debug("Set 'translation-required' early to avoid lag in the server setting it");
+        updatePRLabel(githubClient, repository, prNumber, TRANSLATIONS_REQUIRED);
+      }
     }
     return message;
   }
