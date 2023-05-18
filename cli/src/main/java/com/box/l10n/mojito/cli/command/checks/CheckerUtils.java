221c7702eed1693313c5966209a926334cd8d6f2
hbarbosa <hbarbosa@pinterest.com>
2023-05-18 18:26:07 -0300

    google-java-format

--- cli/src/main/java/com/box/l10n/mojito/cli/command/checks/CheckerUtils.java
+++ cli/src/main/java/com/box/l10n/mojito/cli/command/checks/CheckerUtils.java
@@ -11,30 +11,30 @@ import org.apache.commons.lang3.StringUtils;
 
 public class CheckerUtils {
 
-    /**
-     * Pattern matching for html tags.
-     *
-     * <p>"<" matches an opening angle bracket, "[^>]+" matches one or more characters that are not a
-     * closing angle bracket and ">" matches a closing angle bracket
-     */
-    private static final Pattern HTML_TAGS_PATTERN = Pattern.compile("<[^>]+>");
+  /**
+   * Pattern matching for html tags.
+   *
+   * <p>"<" matches an opening angle bracket, "[^>]+" matches one or more characters that are not a
+   * closing angle bracket and ">" matches a closing angle bracket
+   */
+  private static final Pattern HTML_TAGS_PATTERN = Pattern.compile("<[^>]+>");
 
-    public static List<String> getWordsInString(String str) {
-        if (StringUtils.isEmpty(str)) {
-            return new ArrayList<>();
-        }
-        List<String> words = new ArrayList<>();
-        BreakIterator wordBreakIterator = BreakIterator.getWordInstance(Locale.ENGLISH);
-        str = str.replaceAll(HTML_TAGS_PATTERN.pattern(), "");
-        wordBreakIterator.setText(str);
-        int start = wordBreakIterator.first();
-        for (int end = wordBreakIterator.next();
-             end != BreakIterator.DONE;
-             start = end, end = wordBreakIterator.next()) {
-            if (wordBreakIterator.getRuleStatus() != BreakIterator.WORD_NONE) {
-                words.add(str.substring(start, end));
-            }
-        }
-        return words;
+  public static List<String> getWordsInString(String str) {
+    if (StringUtils.isEmpty(str)) {
+      return new ArrayList<>();
     }
+    List<String> words = new ArrayList<>();
+    BreakIterator wordBreakIterator = BreakIterator.getWordInstance(Locale.ENGLISH);
+    str = str.replaceAll(HTML_TAGS_PATTERN.pattern(), "");
+    wordBreakIterator.setText(str);
+    int start = wordBreakIterator.first();
+    for (int end = wordBreakIterator.next();
+        end != BreakIterator.DONE;
+        start = end, end = wordBreakIterator.next()) {
+      if (wordBreakIterator.getRuleStatus() != BreakIterator.WORD_NONE) {
+        words.add(str.substring(start, end));
+      }
+    }
+    return words;
+  }
 }
