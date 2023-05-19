f6d8f74a51a4a336cbf24c696a9f17d7c1f9156a
hbarbosa <hbarbosa@pinterest.com>
2023-05-19 16:22:30 -0300

    fixing imports format

--- cli/src/main/java/com/box/l10n/mojito/cli/command/checks/CheckerUtils.java
+++ cli/src/main/java/com/box/l10n/mojito/cli/command/checks/CheckerUtils.java
@@ -1,12 +1,10 @@
 package com.box.l10n.mojito.cli.command.checks;
 
 import com.ibm.icu.text.BreakIterator;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.regex.Pattern;
-
 import org.apache.commons.lang3.StringUtils;
 
 public class CheckerUtils {
