4b111d1f463fb7966019a020edb6452b076b9deb
Jean Aurambault <aurambaj@users.noreply.github.com>
2023-05-30 20:05:30 -0700

    Add integration test skeleton for server side notification

--- webapp/src/test/java/com/box/l10n/mojito/service/branch/notification/BranchNotificationMessageSendersITest.java
+++ webapp/src/test/java/com/box/l10n/mojito/service/branch/notification/BranchNotificationMessageSendersITest.java
@@ -1,7 +1,9 @@
 package com.box.l10n.mojito.service.branch.notification;
 
 import com.box.l10n.mojito.service.assetExtraction.ServiceTestBase;
+import java.util.Arrays;
 import org.assertj.core.api.Assertions;
+import org.junit.Assume;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -14,5 +16,48 @@ public class BranchNotificationMessageSendersITest extends ServiceTestBase {
     // noop-1 is the default test notifier, defined in application-test.properties
     BranchNotificationMessageSender noop = branchNotificationMessageSenders.getById("noop-1");
     Assertions.assertThat(noop).isNotNull();
+  }
+
+  @Test
+  public void github() throws Exception, BranchNotificationMessageSenderException {
+
+    String notifierId = "github-1";
+    String branchName = "org/repo/pull/1234";
+    String username = "username";
+
+    BranchNotificationMessageSender github = branchNotificationMessageSenders.getById(notifierId);
+
+    Assume.assumeNotNull(github);
+
+    String messageId =
+        github.sendNewMessage(
+            branchName, username, Arrays.asList("test 1 string", "test 2 string"));
+
+    github.sendUpdatedMessage(
+        branchName, username, messageId, Arrays.asList("test 1 string", "test 2 string"));
+
+    github.sendScreenshotMissingMessage(branchName, messageId, username);
+    github.sendTranslatedMessage(branchName, username, messageId);
+  }
+
+  @Test
+  public void slack() throws Exception, BranchNotificationMessageSenderException {
+
+    String notifierId = "slack-1";
+    String branchName = "org/repo/pull/1234";
+    String username = "username";
+
+    BranchNotificationMessageSender slack = branchNotificationMessageSenders.getById(notifierId);
+
+    Assume.assumeNotNull(slack);
+
+    String messageId =
+        slack.sendNewMessage(branchName, username, Arrays.asList("test 1 string", "test 2 string"));
+
+    slack.sendUpdatedMessage(
+        branchName, username, messageId, Arrays.asList("test 1 string", "test 2 string"));
+
+    slack.sendScreenshotMissingMessage(branchName, messageId, username);
+    slack.sendTranslatedMessage(branchName, username, messageId);
   }
 }
