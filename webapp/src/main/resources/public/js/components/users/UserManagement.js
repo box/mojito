120010300667a3344da69571c1ad751ae9826f63
Daniel Mensinger <d.mensinger@tup.com>
2024-02-23 12:03:35 +0100

    Show a message if the user does not have permissions for the user management

--- webapp/src/main/resources/public/js/components/users/UserManagement.js
+++ webapp/src/main/resources/public/js/components/users/UserManagement.js
@@ -1,11 +1,20 @@
 import React from "react";
-import {injectIntl, __esModule} from "react-intl";
+import {injectIntl, __esModule, FormattedMessage} from "react-intl";
 import UserMainPage from "./UserMainPage";
 import AltContainer from "alt-container";
 import UserStore from "../../stores/users/UserStore";
+import AuthorityService from "../../utils/AuthorityService";
 
 class UserManagement extends React.Component {
     render() {
+        if (!AuthorityService.hasPermissionsForUserManagement()) {
+            return (
+                <div className="ptl">
+                    <h3 className="text-center mtl"><FormattedMessage id="users.forbidden"/></h3>
+                </div>
+            )
+        }
+
         return (
             <AltContainer store={UserStore}>
                 <UserMainPage />
