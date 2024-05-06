f1af38ebe141f24fc6365bc43490055333a86f22
Jean Aurambault <aurambaj@users.noreply.github.com>
2024-05-06 13:37:10 -0700

    Add bn-BD locale

---
+++ webapp/src/main/resources/db/migration/V66__Add_bn_BD.sql
@@ -0,0 +1,4 @@
+insert into locale (id, bcp47_tag) values (1641, 'bn-BD');
+
+insert into plural_form_for_locale (locale_id, plural_form_id) values (1641, 1);
+insert into plural_form_for_locale (locale_id, plural_form_id) values (1641, 5);
