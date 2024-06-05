bda0f1f9bccffcf35d02dab76d23b0f1aeee69fb
Jean Aurambault <aurambaj@users.noreply.github.com>
2024-06-05 16:12:43 -0700

    Fix bug in Project Request UI
    This must have been broken since the spring 3.x/Hibernate migration.

--- webapp/src/main/java/com/box/l10n/mojito/service/drop/DropRepository.java
+++ webapp/src/main/java/com/box/l10n/mojito/service/drop/DropRepository.java
@@ -2,6 +2,9 @@ package com.box.l10n.mojito.service.drop;
 
 import com.box.l10n.mojito.entity.Drop;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,6 +16,10 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  */
 @RepositoryRestResource(exported = false)
 public interface DropRepository extends JpaRepository<Drop, Long>, JpaSpecificationExecutor<Drop> {
+
+  @Override
+  @EntityGraph(value = "Drop.legacy", type = EntityGraphType.FETCH)
+  Page<Drop> findAll(Specification<Drop> spec, Pageable pageable);
 
   @Override
   @EntityGraph(value = "Drop.legacy", type = EntityGraphType.FETCH)
