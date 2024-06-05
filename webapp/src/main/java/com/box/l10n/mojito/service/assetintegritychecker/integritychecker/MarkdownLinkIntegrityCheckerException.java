a5bf2b1ca8f4da93f6becf5c09a47b03d3ed478f
Jean Aurambault <aurambaj@users.noreply.github.com>
2024-06-05 13:34:48 -0700

    Add an integrity check for Markdown links
    Checks for Markdown links only, no other Markdown formatting is checked.

---
+++ webapp/src/main/java/com/box/l10n/mojito/service/assetintegritychecker/integritychecker/MarkdownLinkIntegrityCheckerException.java
@@ -0,0 +1,7 @@
+package com.box.l10n.mojito.service.assetintegritychecker.integritychecker;
+
+public class MarkdownLinkIntegrityCheckerException extends IntegrityCheckException {
+  public MarkdownLinkIntegrityCheckerException(String message) {
+    super(message);
+  }
+}
