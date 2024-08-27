4533304337b4641d29f9de258f56de2ed359144d
Jean Aurambault <ja@openai.com>
2024-08-26 22:28:53 -0700

    Introduce generic OptionsParser

--- common/src/main/java/com/box/l10n/mojito/okapi/filters/FilterOptions.java
+++ common/src/main/java/com/box/l10n/mojito/okapi/filters/FilterOptions.java
@@ -1,42 +1,12 @@
 package com.box.l10n.mojito.okapi.filters;
 
-import com.google.common.base.Splitter;
-import java.util.LinkedHashMap;
+import com.box.l10n.mojito.utils.OptionsParser;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Consumer;
 import net.sf.okapi.common.annotation.IAnnotation;
 
-public class FilterOptions implements IAnnotation {
-
-  Map<String, String> options = new LinkedHashMap<>();
+public class FilterOptions extends OptionsParser implements IAnnotation {
 
   public FilterOptions(List<String> options) {
-    if (options != null) {
-      for (String option : options) {
-        List<String> optionKeyAndValue = Splitter.on("=").limit(2).splitToList(option);
-        if (optionKeyAndValue.size() == 2) {
-          this.options.put(optionKeyAndValue.get(0), optionKeyAndValue.get(1));
-        }
-      }
-    }
-  }
-
-  public void getString(String key, Consumer<String> consumer) {
-    if (this.options.containsKey(key)) {
-      consumer.accept(this.options.get(key));
-    }
-  }
-
-  public void getBoolean(String key, Consumer<Boolean> consumer) {
-    if (this.options.containsKey(key)) {
-      consumer.accept(Boolean.valueOf(this.options.get(key)));
-    }
-  }
-
-  public void getInteger(String key, Consumer<Integer> consumer) {
-    if (this.options.containsKey(key)) {
-      consumer.accept(Integer.valueOf(this.options.get(key)));
-    }
+    super(options);
   }
 }
---
+++ common/src/main/java/com/box/l10n/mojito/utils/OptionsParser.java
@@ -0,0 +1,51 @@
+package com.box.l10n.mojito.utils;
+
+import com.google.common.base.Splitter;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public class OptionsParser {
+
+  Map<String, String> options = new LinkedHashMap<>();
+
+  public OptionsParser(List<String> options) {
+    if (options != null) {
+      for (String option : options) {
+        List<String> optionKeyAndValue = Splitter.on("=").limit(2).splitToList(option);
+        if (optionKeyAndValue.size() == 2) {
+          this.options.put(optionKeyAndValue.get(0), optionKeyAndValue.get(1));
+        }
+      }
+    }
+  }
+
+  public void getString(String key, Consumer<String> consumer) {
+    if (this.options.containsKey(key)) {
+      consumer.accept(this.options.get(key));
+    }
+  }
+
+  public void getBoolean(String key, Consumer<Boolean> consumer) {
+    if (this.options.containsKey(key)) {
+      consumer.accept(Boolean.valueOf(this.options.get(key)));
+    }
+  }
+
+  public Boolean getBoolean(String key) {
+    Boolean value = null;
+
+    if (this.options.containsKey(key)) {
+      value = Boolean.valueOf(this.options.get(key));
+    }
+
+    return value;
+  }
+
+  public void getInteger(String key, Consumer<Integer> consumer) {
+    if (this.options.containsKey(key)) {
+      consumer.accept(Integer.valueOf(this.options.get(key)));
+    }
+  }
+}
