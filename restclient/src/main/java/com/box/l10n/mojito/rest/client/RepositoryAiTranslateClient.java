660ae666ced649b430c8975ae4af2cdc89d0ccd7
Jean Aurambault <ja@openai.com>
2024-10-25 11:02:33 -0700

    Add command line to translate repository using the AiTranslateService

---
+++ restclient/src/main/java/com/box/l10n/mojito/rest/client/RepositoryAiTranslateClient.java
@@ -0,0 +1,35 @@
+package com.box.l10n.mojito.rest.client;
+
+import com.box.l10n.mojito.rest.entity.PollableTask;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author jaurambault
+ */
+@Component
+public class RepositoryAiTranslateClient extends BaseClient {
+
+  /** logger */
+  static Logger logger = LoggerFactory.getLogger(RepositoryAiTranslateClient.class);
+
+  @Override
+  public String getEntityName() {
+    return "proto-ai-translate";
+  }
+
+  /** Ai translate untranslated and rejected strings in a repository for a given list of locales */
+  public ProtoAiTranslateResponse translateRepository(
+      ProtoAiTranslateRequest protoAiTranslateRequest) {
+
+    return authenticatedRestTemplate.postForObject(
+        getBasePathForEntity(), protoAiTranslateRequest, ProtoAiTranslateResponse.class);
+  }
+
+  public record ProtoAiTranslateRequest(
+      String repositoryName, List<String> targetBcp47tags, int sourceTextMaxCountPerLocale) {}
+
+  public record ProtoAiTranslateResponse(PollableTask pollableTask) {}
+}
