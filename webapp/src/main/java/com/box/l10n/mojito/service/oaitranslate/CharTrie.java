b8d8791a14dfd3c104191309ea5818780c9b1683
Jean Aurambault <ja@openai.com>
2025-07-21 14:33:38 -0700

    Switch to character-based trie for partial word matching
    - Use a character trie instead of a word trie to enable matching glossary terms within words.
    - Now terms like "chat" in the glossary will match "chat", "chats", and "chatting" in the source.

---
+++ webapp/src/main/java/com/box/l10n/mojito/service/oaitranslate/CharTrie.java
@@ -0,0 +1,44 @@
+package com.box.l10n.mojito.service.oaitranslate;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class CharTrie<T extends CharTrie.Term> {
+
+  private final Node<T> root = new Node<>(null, new ArrayList<>(), new HashMap<>());
+
+  public void addTerm(T term) {
+    String source = term.text();
+    Node<T> cur = root;
+    for (char c : source.toCharArray()) {
+      cur = cur.child().computeIfAbsent(c, k -> new Node<>(c, new ArrayList<>(), new HashMap<>()));
+    }
+    cur.terms().add(term);
+  }
+
+  public Set<T> findTerms(String text) {
+    Set<T> terms = new HashSet<>();
+
+    for (int i = 0; i < text.length(); i++) {
+      Node<T> cur = root;
+      for (int j = i; j < text.length(); j++) {
+        cur = cur.child().get(text.charAt(j));
+        if (cur == null) break;
+        if (!cur.terms().isEmpty()) {
+          terms.addAll(cur.terms());
+        }
+      }
+    }
+    return terms;
+  }
+
+  public interface Term {
+    String text();
+  }
+
+  private record Node<T>(Character c, List<T> terms, Map<Character, Node<T>> child) {}
+}
