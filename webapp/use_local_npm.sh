1a3b1ef4df3f03dbc96fe404571c586c93a42d3d
Jean Aurambault <ja@openai.com>
2025-08-04 08:13:27 -0700

    Add maven npx to dev path

--- webapp/use_local_npm.sh
+++ webapp/use_local_npm.sh
@@ -2,4 +2,4 @@
 
 # usage "source use_local_npm.sh"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export PATH=$DIR/node:$PATH
+export PATH="$DIR/node:$DIR/node/node_modules/npm/bin:$PATH"
