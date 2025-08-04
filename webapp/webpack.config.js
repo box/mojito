ccaa6574c78be88be15f1330ec0f2661312438d3
Jean Aurambault <ja@openai.com>
2025-08-04 08:18:07 -0700

    Upgrade Webpack and refactoring
    - Upgrade to Webpack5 and Babel 7.
    - Refactor webpack.config.js to share the code between the core and the ict extension

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -1,130 +1,73 @@
-var path = require('path');
-var webpack = require("webpack");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = function (env) {
+function makeCommonRules(argv) {
+    const isDev = argv.mode !== 'production';
+    return [{
+        test: /\.jsx?$/, exclude: /node_modules/, use: {
+            loader: 'babel-loader', options: {
+                presets: [['@babel/preset-env', {modules: false}], '@babel/preset-react'], cacheDirectory: true
+            }
+        }
+    }, {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        type: 'asset',
+        parser: {dataUrlCondition: {maxSize: 8 * 1024}},
+        generator: {filename: 'img/[name]-[contenthash][ext]'}
+    }, {
+        test: /\.properties$/,
+        exclude: /node_modules/,
+        use: [{loader: path.resolve('src/main/webpackloader/properties.js')}]
+    }, {
+        test: /\.(eot|ttf|woff|woff2)$/i, type: 'asset/inline'
+    }, {
+        test: /\.scss$/, use: [{loader: 'style-loader'}, {loader: 'css-loader'}, {
+            loader: 'sass-loader', options: {sourceMap: isDev}
+        }]
+    }];
+}
 
-    env = env || {};
-
-    var config = {
-        entry: {
-            'app': path.resolve(__dirname, './src/main/resources/public/js/app.js'),
-            'css': path.resolve(__dirname, './src/main/resources/sass/mojito.scss')
-        },
-        output: {
+function makeBaseConfig(env, argv) {
+    const isProd = env.production || argv.mode === 'production';
+    return {
+        name: 'base', mode: isProd ? 'production' : 'development', entry: {
+            app: path.resolve(__dirname, './src/main/resources/public/js/app.js'),
+            css: path.resolve(__dirname, './src/main/resources/sass/mojito.scss')
+        }, output: {
             path: path.resolve(__dirname, './target/classes/public'),
             publicPath: '/',
-            filename: 'js/[name]-[hash].js',
-            chunkFilename: 'js/[name]-[chunkhash].js'
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.jsx?$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['es2015', 'react', 'stage-0']
-                        }
-                    }
-                },
-                {
-                    test: /\.(gif|png|jpe?g|svg)$/i,
-                    loaders: [
-                        {
-                            loader: 'file-loader',
-                            options: {
-                                name: 'img/[name]-[hash].[ext]',
-                            }
-                        },
-                        {
-                            loader: 'image-webpack-loader',
-                            query: {
-                                name: 'img/[name]-[hash].[ext]',
-                                query: {
-                                    mozjpeg: {
-                                        progressive: true
-                                    },
-                                    gifsicle: {
-                                        interlaced: false
-                                    },
-                                    optipng: {
-                                        optimizationLevel: 4
-                                    },
-                                    pngquant: {
-                                        quality: '75-90',
-                                        speed: 3
-                                    },
-                                },
-                            }
-                        }
-                    ]
-                },
-                {
-                    test: /\.properties$/,
-                    exclude: /node_modules/,
-                    use: [
-                        {
-                            loader: path.resolve('src/main/webpackloader/properties.js')
-                        }
-                    ],
-                },
-                {
-                    // __webpack_public_path__ is not supported by ExtractTextPlugin
-                    // so we inline all the fonts here. If not inlined, references
-                    // to the font are invalid if mojito is deployed with a
-                    // specific deploy path.
-                    // hardcoded for deploy path for test -->
-                    //    name: '{deployPath}/fonts/[name]-[hash].[ext]'
-
-                    test: /\.(eot|ttf|woff|woff2)$/,
-                    loader: 'url-loader',
-                    options: {
-                        name: 'fonts/[name]-[hash].[ext]'
-                    }
-                },
-
-                {
-                    test: /\.scss$/,
-                    use: [{
-                            loader: "style-loader" // creates style nodes from JS strings
-                        }, {
-                            loader: "css-loader" // translates CSS into CommonJS
-                        }, {
-                            loader: "sass-loader",
-                            options: {
-                                precision: 8,
-                                sourceMap: true
-                            }
-                        }]
-                },
-            ]
-        },
-        plugins: []
+            filename: 'js/[name]-[contenthash].js',
+            chunkFilename: 'js/[name]-[contenthash].js'
+        }, module: {rules: makeCommonRules(argv)}, plugins: [new HtmlWebpackPlugin({
+            filename: path.resolve(__dirname, './target/classes/templates/index.html'),
+            template: 'src/main/resources/templates/index.html',
+            inject: false
+        })], devtool: env.inlineSourceMap ? 'inline-source-map' : false
     };
+}
 
+function makeIctConfig(env, argv) {
+    const isProd = env.production || argv.mode === 'production';
+    return {
+        name: 'ict',
+        mode: isProd ? 'production' : 'development',
+        entry: {
+            'ict-popup': path.resolve(__dirname, './src/main/resources/public/js/ict/chrome-ict-popup.js'),
+            'ict': path.resolve(__dirname, './src/main/resources/public/js/ict/chrome-ict.js')
+        },
+        output: {
+            path: path.resolve(__dirname, '../chromeextension'), filename: '[name]-bundle.js', publicPath: 'auto'
+        },
+        module: {rules: makeCommonRules(argv)},
+        plugins: [],
+        devtool: env.inlineSourceMap ? 'inline-source-map' : false
+    };
+}
 
-    var HtmlWebpackPlugin = require('html-webpack-plugin');
-    config.plugins.push(new HtmlWebpackPlugin({
-        filename: path.resolve(__dirname, './target/classes/templates/index.html'),
-        template: 'src/main/resources/templates/index.html',
-        inject: false
-    }));
-
-    if (env.production) {
-        config.plugins.push(
-                new webpack.DefinePlugin({
-                    'process.env': {
-                        'NODE_ENV': JSON.stringify('production')
-                    }
-                }));
-
-        config.plugins.push(new webpack.optimize.UglifyJsPlugin());
-    }
-
-    if (env.inlineSourceMap) {
-        config.devtool = "inline-source-map";
-    }
-
-    return config;
+module.exports = (env = {}, argv = {}) => {
+    const target = env.target; // "base" | "ict" | undefined
+    if (target === 'base') return makeBaseConfig(env, argv);
+    if (target === 'ict') return makeIctConfig(env, argv);
+    // no target => build both
+    return [makeBaseConfig(env, argv), makeIctConfig(env, argv)];
 };
