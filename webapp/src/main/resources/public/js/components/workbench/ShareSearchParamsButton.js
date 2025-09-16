8670731889a8278c3bea93cb1b20ae75cb296280
Jean Aurambault <ja@openai.com>
2025-09-16 01:12:30 -0700

    New copy, overlay trigger and fix class name in ShareSearchParamsButton.js

--- webapp/src/main/resources/public/js/components/workbench/ShareSearchParamsButton.js
+++ webapp/src/main/resources/public/js/components/workbench/ShareSearchParamsButton.js
@@ -1,23 +1,29 @@
 import PropTypes from 'prop-types';
 import React from "react";
 import {injectIntl} from 'react-intl';
-import {Button, Glyphicon} from "react-bootstrap";
+import {Button, Glyphicon, OverlayTrigger, Tooltip} from "react-bootstrap";
 
-class StatusGlyph extends React.Component {
+class ShareSearchParamsButton extends React.Component {
     static propTypes = {
-        "onClick": PropTypes.func.isRequired
+        "onClick": PropTypes.func.isRequired,
+        "intl": PropTypes.object.isRequired,
     };
 
     /**
      * @return {JSX}
      */
     render() {
+        const {intl} = this.props;
+        const label = intl.formatMessage({id: "workbench.shareSearchParams.button"});
+
         return (
-            <Button onClick={this.props.onClick} className="mlm">
-                <Glyphicon glyph='glyphicon glyphicon-link'/>
-            </Button>
+            <OverlayTrigger trigger={['hover']} placement="bottom" overlay={<Tooltip id="workbench-share-tooltip">{label}</Tooltip>}>
+                <Button onClick={this.props.onClick} className="mlm" title={label}>
+                    <Glyphicon glyph='glyphicon glyphicon-link'/>
+                </Button>
+            </OverlayTrigger>
         );
     }
 }
 
-export default injectIntl(StatusGlyph);
+export default injectIntl(ShareSearchParamsButton);
