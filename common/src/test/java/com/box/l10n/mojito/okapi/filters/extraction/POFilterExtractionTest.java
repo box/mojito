918835d715025a7cd411245df933907e2adfa91f
Wadim Wawrzenczak <wwawrzenczak@box.com>
2026-03-12 13:45:33 +0100

    Add tests to pin down current PO file extraction behavior

---
+++ common/src/test/java/com/box/l10n/mojito/okapi/filters/extraction/POFilterExtractionTest.java
@@ -0,0 +1,167 @@
+package com.box.l10n.mojito.okapi.filters.extraction;
+
+import static org.assertj.core.groups.Tuple.tuple;
+
+import com.box.l10n.mojito.okapi.asset.UnsupportedAssetFilterTypeException;
+import com.box.l10n.mojito.okapi.extractor.AssetExtractorTextUnit;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class POFilterExtractionTest extends FilterExtractionTestBase {
+
+  @Test
+  public void extractSimple() throws UnsupportedAssetFilterTypeException {
+    List<AssetExtractorTextUnit> units =
+        extract(
+            "test.pot",
+            """
+                #: src/main.py:10
+                msgid "Hello"
+                msgstr ""
+
+                #: src/main.py:11
+                msgid "World"
+                msgstr ""
+                """,
+            null,
+            null);
+
+    Assertions.assertThat(units)
+        .extracting(AssetExtractorTextUnit::getName, AssetExtractorTextUnit::getSource)
+        .containsExactly(tuple("Hello", "Hello"), tuple("World", "World"));
+  }
+
+  @Test
+  public void extractWithContext() throws UnsupportedAssetFilterTypeException {
+    List<AssetExtractorTextUnit> units =
+        extract(
+            "test.pot",
+            """
+                msgctxt "menu"
+                msgid "File"
+                msgstr ""
+                """,
+            null,
+            null);
+
+    Assertions.assertThat(units)
+        .extracting(AssetExtractorTextUnit::getName, AssetExtractorTextUnit::getSource)
+        .containsExactly(tuple("File --- menu", "File"));
+  }
+
+  @Test
+  public void extractWithEscapedDoubleQuotes() throws UnsupportedAssetFilterTypeException {
+    List<AssetExtractorTextUnit> units =
+        extract(
+            "test.pot",
+            """
+                msgid "He said \\"hello\\""
+                msgstr ""
+                """,
+            null,
+            null);
+
+    Assertions.assertThat(units)
+        .extracting(AssetExtractorTextUnit::getName, AssetExtractorTextUnit::getSource)
+        .containsExactly(tuple("He said \\\"hello\\\"", "He said \"hello\""));
+  }
+
+  @Test
+  public void extractWithEscapedSingleQuotes() throws UnsupportedAssetFilterTypeException {
+    List<AssetExtractorTextUnit> units =
+        extract(
+            "test.pot",
+            """
+                msgid "it\\'s a test"
+                msgstr ""
+                """,
+            null,
+            null);
+
+    Assertions.assertThat(units)
+        .extracting(AssetExtractorTextUnit::getName, AssetExtractorTextUnit::getSource)
+        .containsExactly(tuple("it\\'s a test", "it's a test"));
+  }
+
+  @Test
+  public void extractWithComment() throws UnsupportedAssetFilterTypeException {
+    List<AssetExtractorTextUnit> units =
+        extract(
+            "test.pot",
+            """
+                #. Translator comment
+                #: src/app.py:5
+                msgid "Save"
+                msgstr ""
+                """,
+            null,
+            null);
+
+    Assertions.assertThat(units)
+        .extracting(
+            AssetExtractorTextUnit::getName,
+            AssetExtractorTextUnit::getSource,
+            AssetExtractorTextUnit::getComments)
+        .containsExactly(tuple("Save", "Save", "Translator comment"));
+  }
+
+  @Test
+  public void extractPlurals() throws UnsupportedAssetFilterTypeException {
+    List<AssetExtractorTextUnit> units =
+        extract(
+            "test.pot",
+            """
+                #: src/main.py:10
+                msgid "item"
+                msgid_plural "items"
+                msgstr[0] ""
+                msgstr[1] ""
+                """,
+            null,
+            null);
+
+    Assertions.assertThat(units)
+        .extracting(
+            AssetExtractorTextUnit::getName,
+            AssetExtractorTextUnit::getSource,
+            AssetExtractorTextUnit::getPluralForm,
+            AssetExtractorTextUnit::getPluralFormOther)
+        .containsExactly(
+            tuple("item _zero", "items", "zero", "item _other"),
+            tuple("item _one", "item", "one", "item _other"),
+            tuple("item _two", "items", "two", "item _other"),
+            tuple("item _few", "items", "few", "item _other"),
+            tuple("item _many", "items", "many", "item _other"),
+            tuple("item _other", "items", "other", "item _other"));
+  }
+
+  @Test
+  public void extractPluralsWithEscapedQuotes() throws UnsupportedAssetFilterTypeException {
+    List<AssetExtractorTextUnit> units =
+        extract(
+            "test.pot",
+            """
+                msgid "the \\"item\\""
+                msgid_plural "the \\"items\\""
+                msgstr[0] ""
+                msgstr[1] ""
+                """,
+            null,
+            null);
+
+    Assertions.assertThat(units)
+        .extracting(
+            AssetExtractorTextUnit::getName,
+            AssetExtractorTextUnit::getSource,
+            AssetExtractorTextUnit::getPluralForm,
+            AssetExtractorTextUnit::getPluralFormOther)
+        .containsExactly(
+            tuple("the \\\"item\\\" _zero", "the \"items\"", "zero", "the \\\"item\\\" _other"),
+            tuple("the \\\"item\\\" _one", "the \"item\"", "one", "the \\\"item\\\" _other"),
+            tuple("the \\\"item\\\" _two", "the \"items\"", "two", "the \\\"item\\\" _other"),
+            tuple("the \\\"item\\\" _few", "the \"items\"", "few", "the \\\"item\\\" _other"),
+            tuple("the \\\"item\\\" _many", "the \"items\"", "many", "the \\\"item\\\" _other"),
+            tuple("the \\\"item\\\" _other", "the \"items\"", "other", "the \\\"item\\\" _other"));
+  }
+}
